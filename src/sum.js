3254ece8330b6dc9769d0460c7c0a61b7ac65569
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-15 17:57:25 +0300

    fixed function name

---
+++ src/sum.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * Implement sum function:
+ *
+ * Function works with next pattern: sum(a)(b) === a + b. Double braces is not a
+ * mistake. The implementation requires solution with closures.
+ *
+ * sum(1)(2) === 3
+ * sum(1)(11) === 12
+ * sum(1)(-1) === 0
+ *
+ * @param {number} a
+ *
+ * @return {function}
+ */
+function sum(a) {
+  // write code here
+}
+
+module.exports = sum;
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const sum = require('./getSumWithClosure');
+const sum = require('./sum');
 
 test('add two numbers: 1 + 2 = 3', () => {
   expect(sum(1)(2))
