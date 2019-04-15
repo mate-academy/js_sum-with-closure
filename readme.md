681bd3a6141ff6467e9618155d7cac532bc96ae2
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-15 16:23:53 +0300

    implemented task & tests

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Boilerplate for JS tasks
+# Sum with a closure
 
 **Read the guideline before start**
 
---
+++ src/getSumWithClosure.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * Implement getSumWithClosure function:
+ *
+ * Function works with next pattern: getSumWithClosure(a)(b) === a + b. Double
+ * braces is not a mistake.
+ *
+ * getSumWithClosure(1)(2) === 3
+ * getSumWithClosure(1)(11) === 12
+ * getSumWithClosure(1)(-1) === 0
+ *
+ * @param {number} a
+ *
+ * @return {function}
+ */
+function getSumWithClosure(a) {
+  // write code here
+}
+
+module.exports = getSumWithClosure;
