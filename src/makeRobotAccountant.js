1d93d45def2fc387aa9c7bd0acfef2fd0a3ac609
Andrew Nickyforov <nik_andrey@ukr.net>
2020-06-04 23:33:12 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return function(num1) {
+    counter += 1;
+
+    return function(num2) {
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return num1 + num2;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
