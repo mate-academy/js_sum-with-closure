8f8e52b7cbcb7728243b6486a993c1463e7f0a89
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-06-05 09:37:03 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let index = 0;
+
+  return a => {
+    index++;
+
+    return b => {
+      if (index > 3 && index % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
