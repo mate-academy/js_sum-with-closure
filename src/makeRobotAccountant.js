3d3e67c41ed0cf782032387cb4adbd11616400c9
Slava Polishchuk <dizerllc@gmail.com>
2020-06-06 13:05:55 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,32 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (a) => {
+    counter++;
+
+    return (b) => {
+      if (counter > 3 && !(counter % 2)) {
+        return 'Bzzz... Error!';
+      } else {
+        return a + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
+
+// const getSum = makeRobotAccountant();
+
+// console.log(getSum(3)(5));
+// console.log(getSum(3)(5));
+// console.log(getSum(3)(5));
+// console.log(getSum(3)(5));
+// console.log(getSum(3)(5));
+// console.log(getSum(3)(5));
+// console.log(getSum(4)(3));
+// console.log(getSum(4)(3));
+// console.log(getSum(4)(3));
+// console.log(getSum(4)(3));
