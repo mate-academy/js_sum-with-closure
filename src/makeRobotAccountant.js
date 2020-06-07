2ba52db7bf4fcc900c6ad92cabe47b153a961a7d
tyooma <flying.alien.godzilla@gmail.com>
2020-06-07 16:16:14 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (firstNumber) => {
+    counter++;
+
+    return (secondNumber) => {
+      return counter > 3 && counter % 2 === 0
+        ? 'Bzzz... Error!'
+        : firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
