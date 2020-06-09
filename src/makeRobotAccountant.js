a234d3f9d6417b1f16f488922f8b251e6b187f52
Yakiv <yakov.sivukha@gmail.com>
2020-06-09 09:27:53 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let calls = 0;
+
+  return function(arg1) {
+    return function(arg2) {
+      calls++;
+
+      if (calls > 3 && !(calls % 2)) {
+        return 'Bzzz... Error!';
+      } else {
+        return arg1 + arg2;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
