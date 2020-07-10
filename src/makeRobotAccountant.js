9f428d82a936f40d5aecab623a9604dbe1599ecc
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-07-10 19:23:24 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countNumber = 0;
+
+  return function(x) {
+    return function(y) {
+      countNumber++;
+
+      if (countNumber <= 3) {
+        return x + y;
+      } else {
+        if ((countNumber % 2) === 0) {
+          return 'Bzzz... Error!';
+        } else {
+          return x + y;
+        }
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
