3bb3da1035f0c6cdfe164cdb03812e55b1f3bcd5
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-11 19:44:09 +0300

    +

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let counts = 0;
+
+  return function(first) {
+    return function(second) {
+      counts++;
+
+      if (counts < 4 || counts % 2 === 1) {
+        return first + second;
+      } else {
+        return 'Bzzz... Error!';
+      };
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
