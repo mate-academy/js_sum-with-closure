058cca9812e9c26c42be2cec98fd7b65f1b54251
Yurii Rysyn <zerocool_cs@ukr.net>
2020-08-14 22:26:49 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,20 @@
 
 function makeRobotAccountant() {
   // write code here
+  // eslint-disable-next-line no-unused-vars
+  let count = 0;
+
+  return function(a) {
+    count++;
+
+    return b => {
+      if (count < 3 || count % 2 !== 0) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
