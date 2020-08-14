1321406809847c19628ceeb155cd7950442dc086
mpone <m.parphenjuk@gmail.com>
2020-08-14 23:15:07 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let operationCall = 0;
+
+  return (firstValue) => {
+    operationCall++;
+
+    return (secondValue) => {
+      switch (true) {
+        case (operationCall <= 3):
+          return firstValue + secondValue;
+
+        case (operationCall % 2 !== 0):
+          return firstValue + secondValue;
+
+        default:
+          return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
