bee4aafe7c112f10a00c363344604ab2d95cc76f
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-08-14 14:50:13 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (a) => { // getSum(5)
+    count++;
+
+    return (b) => { // ( getSum(5) )(6)
+      if (count <= 3) { // first three works everytime
+        return a + b;
+      } else if (count % 2) { // odd (НЕпарні) works correct
+        return a + b;
+      } else { // >3 and even works not correct
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
