cff3cf3cbbd9b97c823c970adabf4d0c155f3ca3
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-08-14 10:15:06 +0300

    fixed again

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,9 +9,10 @@ function makeRobotAccountant() {
   let startCount = 0;
 
   return (x) => {
-    startCount++;
 
     return (y) => {
+      startCount++;
+
       if (startCount <= 3 || startCount % 2 !== 0) {
         return x + y;
       } else {
