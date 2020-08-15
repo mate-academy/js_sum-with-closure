49f72de711f1fe099dc3fb58c5ad261903c2f6ed
tykhan <tykhanvasyl@gmail.com>
2020-08-15 16:00:43 +0300

    fixed code style

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,19 +8,15 @@
 function makeRobotAccountant() {
   let counter = 0;
 
-  function getSum(first) {
+  return function(first) {
     return function(second) {
       counter++;
 
-      if (counter <= 3 || counter % 2 !== 0) {
-        return first + second;
-      } else {
-        return 'Bzzz... Error!';
-      }
+      return (counter <= 3 || counter % 2 !== 0)
+        ? first + second
+        : 'Bzzz... Error!';
     };
   };
-
-  return getSum;
 };
 
 module.exports = makeRobotAccountant;
