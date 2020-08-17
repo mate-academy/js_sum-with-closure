2c594be0ebc915a6ec3a356842bd70c7e4be85cd
Zarva <zarva6596@gmail.com>
2020-08-17 19:10:55 +0300

    first commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let i = 0;
+
+  return (a) => {
+    return (b) => {
+      if (i < 3 || i / 2 === Math.floor(i / 2)) {
+        i++;
+
+        return a + b;
+      } else {
+        i++;
+
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
