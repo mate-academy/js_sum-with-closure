e71a163cc58ef1fbfb0069a99e32ec31890ba32c
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-17 17:47:48 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let i = 0;
+
+  const takeValue1 = (x) => {
+    const takeValue2 = (y) => {
+      i++;
+
+      if (i < 4 || i % 2 !== 0) {
+        return x + y;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+
+    return takeValue2;
+  };
+
+  return takeValue1;
 }
 
 module.exports = makeRobotAccountant;
