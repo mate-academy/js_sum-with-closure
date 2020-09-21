88ccdc04e482d9aa53fb53ae0b4bb7ad406ff5f4
dmitry <akulinind@gmail.com>
2020-09-21 19:08:51 +0300

    add sum function

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,28 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  function sum(num1) {
+    return function(num2) {
+      if (counter === 3) {
+        counter++;
+
+        return 'Bzzz... Error!';
+      }
+
+      if (counter > 3 && counter % 2 !== 0) {
+        counter++;
+
+        return 'Bzzz... Error!';
+      }
+      counter++;
+
+      return num1 + num2;
+    };
+  }
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
