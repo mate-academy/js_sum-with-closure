f3509f32b498f02d43c14e99df11e0c68bc7aeb5
Artem Lavreniuk <artemlavrenyuk@gmail.com>
2020-09-29 17:02:48 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  const sumOfNums = (num1) => {
+    return (num2) => {
+      counter++;
+
+      if (counter > 2 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return num1 + num2;
+      }
+    };
+  };
+
+  return sumOfNums;
 }
 
 module.exports = makeRobotAccountant;
