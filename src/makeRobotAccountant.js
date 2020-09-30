4e02bcfea63669f5be914f77e492b8079a4d773d
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-09-30 20:12:28 +0300

    solution make robot

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const sumOfNums = (num1) => {
+    return (num2) => {
+      count++;
+
+      return (count > 2 && count % 2 === 0) ? 'Bzzz... Error!' : num1 + num2;
+    };
+  };
+
+  return sumOfNums;
 }
 
 module.exports = makeRobotAccountant;
