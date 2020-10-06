517be15a339216edb63a55d92975f44201bcc95a
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-06 19:02:41 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getTotal = (number1) => {
+    return (number2) => {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return number1 + number2;
+      }
+    };
+  };
+
+  return getTotal;
 }
 
 module.exports = makeRobotAccountant;
