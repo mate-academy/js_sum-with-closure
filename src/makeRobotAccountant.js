2c09313fba20c7cb92b067c63c06e24b95ca2fa1
Yuriy Putsan <putsan57@gmail.com>
2020-10-07 14:16:16 +0300

    delete not nidded scopes

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,8 +8,8 @@
 function makeRobotAccountant() {
   let callsCount = 0;
 
-  return (numberA) => {
-    return (numberB) => {
+  return numberA => {
+    return numberB => {
       callsCount++;
 
       return callsCount < 3 || callsCount % 2
