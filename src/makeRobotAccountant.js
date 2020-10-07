2dbd2b6b0b4ec3b7a2ecc1f76603287e8d9050ce
Lancaelot <Lancaelot@gmail.com>
2020-10-07 09:40:01 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,21 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+  const nomralWorkCycles = 3;
+  const errorMessage = 'Bzzz... Error!';
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      count++;
+
+      if (count > nomralWorkCycles && count % 2 === 0) {
+        return errorMessage;
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
