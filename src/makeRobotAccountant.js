3c2617cb94a74db638c5240a0033ed7bd4854c9d
KirillLutsenko <mnrovski68@gmail.com>
2020-10-07 22:06:17 +0300

    Make Robot Accountant solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,26 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  const firstAdd = (firstValue) => {
+    let result = firstValue;
+
+    const secondAdd = (secondValue) => {
+      result += secondValue;
+      counter++;
+
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return result;
+    };
+
+    return secondAdd;
+  };
+
+  return firstAdd;
 }
 
 module.exports = makeRobotAccountant;
