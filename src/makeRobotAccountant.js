1cc39ae8ec919d053e446d3a3aa73da11c121afa
kerpichonka <dimontregyb@email.ua>
2020-10-08 15:40:22 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const getNum = (numA) => {
+    count++;
+
+    const getSum = (numB) => {
+      if (count < 3) {
+        return numA + numB;
+      } else if (count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return numA + numB;
+    };
+
+    return getSum;
+  };
+
+  return getNum;
 }
 
 module.exports = makeRobotAccountant;
