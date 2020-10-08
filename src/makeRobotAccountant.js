398cc4f06dde001cbcd05e441750a9d32759063b
Yurii <yurastt1@gmail.com>
2020-10-08 14:40:40 +0300

    sol

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (firstNum) => {
+    count++;
+
+    return function(secondNum) {
+      if (count > 3 && count % 2 === 0) {
+        return `Bzzz... Error!`;
+      } else {
+        return firstNum + secondNum;
+      };
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
