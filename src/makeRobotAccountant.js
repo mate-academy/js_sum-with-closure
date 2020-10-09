24eed46a04e45e971d60bf4cea36dcd5e86ebba7
Atteroot <yuora556@gmail.com>
2020-10-09 15:13:13 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (numberFirst) => {
+    return (secondNumber) => {
+      count++;
+
+      if (count <= 3 || count % 2 !== 0) {
+        return numberFirst + secondNumber;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
