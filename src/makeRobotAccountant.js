61a62d91bedf92a5d6e61df7b437fbcdf6b484cf
Dmitrii <dmitrii.zinchuk@dstarlab.com>
2020-10-12 16:04:11 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 1;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      if (counter <= 3) {
+        counter++;
+
+        return firstNumber + secondNumber;
+      } else {
+        if (counter % 2 === 0) {
+          counter++;
+
+          return 'Bzzz... Error!';
+        } else {
+          counter++;
+
+          return firstNumber + secondNumber;
+        }
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
