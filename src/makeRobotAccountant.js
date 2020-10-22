6676f8330f24447f69763def174c1d13a1849a3d
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2020-10-22 21:20:49 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,21 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  const addDigits = (x) => {
+    return function(y) {
+      count++;
+
+      if (count >= 4 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
+
+  return addDigits;
 }
 
 module.exports = makeRobotAccountant;
