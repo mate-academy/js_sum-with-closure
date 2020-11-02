5b072b90457ff42af86d8257564603d147342505
Olga Rubalska <olga.anikeieva@gmail.com>
2020-11-02 14:21:10 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let numOfCalls_ = 0;
+  const errorMessage_ = 'Bzzz... Error!';
+
+  const getSum_ = (param1) => {
+    numOfCalls_++;
+
+    return function(param2) {
+      if ((numOfCalls_ % 2) > 0 || numOfCalls_ <= 3) {
+        return param1 + param2;
+      }
+
+      return errorMessage_;
+    };
+  };
+
+  return getSum_;
 }
 
 module.exports = makeRobotAccountant;
