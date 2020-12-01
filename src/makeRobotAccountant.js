e4cc96f1a3a5782b3d210109f00d44eb6039b955
Inna Golovina <igigig978@gmail.com>
2020-12-01 17:01:31 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,18 @@
 
 function makeRobotAccountant() {
   // write code here
-}
+  let callCounter = 0;
 
+  return function(firstValue) {
+    return function(secondValue) {
+      callCounter++;
+
+      if (callCounter > 3 && callCounter % 2 === 0) {
+        return `Bzzz... Error!`;
+      }
+
+      return firstValue + secondValue;
+    };
+  };
+}
 module.exports = makeRobotAccountant;
