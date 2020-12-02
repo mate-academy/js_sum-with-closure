64773167a08d366f30379bb45e7bcba4545f51ed
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2020-12-02 19:35:00 +0200

    Add solution makeRobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,18 @@
 
 function makeRobotAccountant() {
   // write code here
+
+  let count = 0;
+
+  function getSum(x) {
+    count++;
+
+    return (y) => {
+      return count <= 3 ? x + y : count % 2 === 0 ? 'Bzzz... Error!' : x + y;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
