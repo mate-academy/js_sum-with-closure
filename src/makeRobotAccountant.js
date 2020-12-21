37ce3591c2bed582ffc5246778f3801bbf78ba7d
troospieler <troospieler1990@gmail.com>
2020-12-22 01:24:14 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return x => {
+    return y => {
+      counter++;
+
+      if (counter <= 3) {
+        return x + y;
+      } else if (counter % 2 !== 0) {
+        return x + y;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
