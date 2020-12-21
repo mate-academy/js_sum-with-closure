a7d5c8bfe1a97c0a01ddeded37dee342053a42b4
Lora Levitska <larakad94@gmail.com>
2020-12-21 20:36:43 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(x) {
+    return function(b) {
+      count++;
+
+      if (count > 2 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else if (count > 3 && count % 2 !== 0) {
+        return x + b;
+      } else {
+        return x + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
