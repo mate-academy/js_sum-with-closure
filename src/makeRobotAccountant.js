da3a3ac9040de43acea9c0cc49c2f27784a63a5f
SofiiaAsieieva <sofiia.asieieva@gmail.com>
2020-12-23 12:08:07 +0200

    Added function if sum with closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => {
+    return (y) => {
+      count++;
+
+      if (count >= 4 && x % 3 !== 0 && y % 3 !== 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return (x) + (y);
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
