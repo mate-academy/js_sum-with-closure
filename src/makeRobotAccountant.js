48fd38e9eba0969232de261637bff1aa111dd0b7
Roman Matsuk <the.show.of.ice.man@gmail.com>
2020-12-23 21:56:50 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 3;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      if (count > 0) {
+        count--;
+
+        return firstNumber + secondNumber;
+      }
+
+      count = 2;
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
