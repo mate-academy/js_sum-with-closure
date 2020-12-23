d00f761d0fed0a2eba3ceb0be6be463e425fd8f8
Olesia <olesia.tk@gmail.com>
2020-12-23 14:07:01 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,29 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let step = 0;
+  let stepAllowed = 3;
+
+  const getFirstArguments = function(a) {
+    const getNewArguments = function(b) {
+      step++;
+
+      const sum = a + b;
+
+      if (step <= stepAllowed) {
+        return sum;
+      } else {
+        step = 0;
+        stepAllowed = 1;
+
+        return 'Bzzz... Error!';
+      }
+    };
+
+    return getNewArguments;
+  };
+
+  return getFirstArguments;
 }
 
 module.exports = makeRobotAccountant;
