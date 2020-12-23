046ac5d4529dbce85ef52b89de9f0d8c3efb1deb
Maryna Zinchenko <pandoz16@gmail.com>
2020-12-23 19:42:43 +0200

    fixed after review

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,9 +9,9 @@ function makeRobotAccountant() {
   let count = 0;
 
   return function(a) {
-    count++;
-
     return function(b) {
+      count++;
+
       if (count <= 3 || count % 2 !== 0) {
         return a + b;
       }
