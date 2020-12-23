fe4d5955f26b5bda03f088a9aca56a0636943569
Oleksii Spalylo <white.rh42@gmail.com>
2020-12-23 22:45:26 +0200

    have made robot accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return (firstNumber) => {
+    count++;
+
+    return (secondNumber) => {
+      if (count <= 3 || count % 2 !== 0) {
+        return firstNumber + secondNumber;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
