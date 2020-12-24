a6a9053bc2da36820935778d34e38c8f7994afe5
dinexpod <work.toxa@gmail.com>
2020-12-24 23:08:11 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,26 @@
 'use strict';
 
 /**
- *
+ * Function need return function into function which have one param.
+ * This function result need to be sum of params these functions if function
+ * calling count is less than 3 and odd. Else return 'Bzzz... Error!'.
  * @return {function}
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (a) => {
+    count++;
+
+    return (b) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
