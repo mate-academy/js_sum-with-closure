9e0e8930a4de3f6ec214420b42bf136ac463748c
DominoSl <artemenko.mik@gmail.com>
2021-01-15 20:10:46 +0200

    SolutionX2

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,33 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  let sum = 0;
   let call = 1;
 
   return (number1) => {
     return (number2) => {
-      sum += number1 + number2;
-
       if (call <= 3) {
-        const ans = sum;
-
         ++call;
-        sum = 0;
 
-        return ans;
-      } else if (call % 2 === 1) {
-        const ans = sum;
+        return number1 + number2;
+      };
 
+      if (call % 2 === 1) {
         ++call;
-        sum = 0;
 
-        return ans;
-      } else {
-        ++call;
-        sum = 0;
-
-        return 'Bzzz... Error!';
+        return number1 + number2;
       }
+      ++call;
+
+      return 'Bzzz... Error!';
     };
   };
 }
