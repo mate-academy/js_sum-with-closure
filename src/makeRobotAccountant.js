cfbcba936919357ae9b3cea896ef522220f6c0c4
Georgy Dzumenko <erterj05@gmail.com>
2021-01-18 19:18:57 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  return (value1) => {
+    return (value2) => {
+      if (calls < 3) {
+        calls++;
+
+        return value1 + value2;
+      }
+
+      if (calls % 2 === 0) {
+        calls++;
+
+        return value1 + value2;
+      }
+
+      calls++;
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
