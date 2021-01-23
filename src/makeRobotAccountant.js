d24ac7f7b61130531eff25adc886650fff937158
Andriy Ivanyk <andriyivanyk@Air-Andriy.lan>
2021-01-23 22:54:46 +0000

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let calls = 0;
+
+  return (x) => {
+    calls++;
+
+    return (y) => {
+      if (calls > 3 && calls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
