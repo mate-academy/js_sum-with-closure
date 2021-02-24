3b705feb918c64ba3fce35f278e38771e4fbc12a
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-02-24 16:13:12 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(b) {
+    count++;
+
+    return function(a) {
+      if ((count > 3) && (count % 2 === 0)) {
+        return 'Bzzz... Error!';
+      }
+
+      return +(a + b);
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
