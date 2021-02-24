9740bbb4397519965322d5ddd29ce3cbfeb9e917
anastasiia <anastasiia.aliinyk@gmail.com>
2021-02-24 13:30:43 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callFunction = 0;
+
+  return (firstNumber) => {
+    callFunction++;
+
+    return (secondNumber) => {
+      const isEven = callFunction % 2 === 0;
+
+      if (callFunction > 3 && isEven) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
