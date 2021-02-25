0ca14f17610abe17d70cdc443a1ffd21172c5828
forestforgeron <forestforgeron@gmail.com>
2021-02-25 21:40:34 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let numberOfCalls = 0;
+
+  return (firstNumber) => {
+    numberOfCalls++;
+
+    return (secondNumber) => {
+      if (numberOfCalls > 3 && numberOfCalls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
