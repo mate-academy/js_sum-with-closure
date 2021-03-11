7971ff3bf1ff4b8b0e12346404c3a7fdee1ae1ee
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-03-11 19:29:36 +0200

    deleted isProblematic var

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,18 +7,13 @@
 
 function makeRobotAccountant() {
   let counter = 0;
-  let isProblematic = false;
   const ERROR_MESSAGE = 'Bzzz... Error!';
 
   const getSumFunction = (x) => {
     return function(y) {
       counter++;
 
-      if (counter >= 3) {
-        isProblematic = true;
-      }
-
-      if (isProblematic && counter % 2 === 0) {
+      if (counter > 3 && counter % 2 === 0) {
         return ERROR_MESSAGE;
       } else {
         return x + y;
