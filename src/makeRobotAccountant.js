03f8b53ee519e45077f4242b98ba81d63b995146
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-03-13 13:16:33 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,34 @@
 'use strict';
 
+// const getSum = makeRobotAccountant();
+// getSum(5)(6) === 11;
+// getSum(1)(3) === 4;
+// getSum(6)(6) === 12;
+// getSum(55)(16) === 'Bzzz... Error!';
+// getSum(55)(16) === 71;
+// getSum(55)(17) === 'Bzzz... Error!';
+// getSum(5)(17) === 22;
+// getSum(1)(1) === 'Bzzz... Error!';
+
 /**
  *
  * @return {function}
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (firsNumber) => {
+    return (secondNumber) => {
+      counter++;
+
+      if (counter === 4) {
+        return 'Bzzz... Error!';
+      }
+
+      return firsNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
