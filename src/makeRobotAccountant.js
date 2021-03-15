c569b9e88e43ac7340bbf8fbfa400c90c7bf6a52
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-15 18:57:53 +0200

    test

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
+  let count = 0;
   // write code here
+  const getSum = (firstArgument) => {
+    count++;
+
+    const getSumSecond = (secondArgument) => {
+      const sum = firstArgument + secondArgument;
+
+      if (count <= 3 || count % 2 !== 0) {
+        return sum;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+
+    return getSumSecond;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
