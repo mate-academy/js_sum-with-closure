e8ac03b2238baa4f80f9cb577fb91810ef49cbc1
Alex Fedorchenko <mralex.fed@gmail.com>
2021-03-25 17:00:11 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+  const errorMessage = 'Bzzz... Error!';
+
+  return (a) => {
+    return (b) => {
+      if (count === 4) {
+        count++;
+
+        return errorMessage;
+      } else if ((4 + (count % 2) > 4 || count < 4)) {
+        count++;
+
+        return a + b;
+      };
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
