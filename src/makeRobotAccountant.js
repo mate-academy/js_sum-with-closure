a7f2986f52f16fabe224552524d4c3cc902bd3de
Ivan Major <tarstarcas@gmail.com>
2021-04-15 14:49:14 +0300

    Make Robot Accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
+
+  const getSum = (add1) => {
+    const sum = (add2) => {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      };
+
+      return add1 + add2;
+    };
+
+    return sum;
+  };
+
+  return getSum;
+};
 
 module.exports = makeRobotAccountant;
