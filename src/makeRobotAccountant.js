bf62799cf9be542b2aa90123075634ae46d30da7
herkoss <dmytroherko@icloud.com>
2021-05-05 13:21:10 +0300

    Block if changed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -12,13 +12,11 @@ function makeRobotAccountant() {
     counter++;
 
     const insideSum = (digit2) => {
-      if (counter < 3) {
-        return digit + digit2;
-      } else if (counter % 2 === 1) {
-        return digit + digit2;
-      } else if (counter % 2 === 0) {
+      if (counter > 3 && (counter % 2) === 0) {
         return 'Bzzz... Error!';
       }
+
+      return digit + digit2;
     };
 
     return insideSum;
