c2b42fcdbf8a3534ec62f9f65cde7b47b972f86d
Vadym Liubych <v.liubych@ukr.net>
2021-05-06 17:02:14 +0300

    add solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(firstNum) {
+    return function(secondNum) {
+      count++;
+
+      if (count <= 3) {
+        return firstNum + secondNum;
+      }
+
+      if (count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNum + secondNum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
