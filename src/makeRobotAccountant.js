d6686c658ba1acd91641de99a64e65ad7b7aed4b
Sergey Zub <vook1996@gmail.com>
2021-05-06 15:08:03 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => {
+    return function(y) {
+      if (count > 2 && count % 2 !== 0) {
+        count++;
+
+        return 'Bzzz... Error!';
+      } else {
+        count++;
+
+        return x + y;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
