b57c0b780371dbee6fd404a3d0f295c9b466a998
Provolovskyi Illia <provolovskyi.illia@gmail.com>
2021-05-06 19:22:30 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countCall = 0;
+
+  return (numberFirst) => {
+    return (numberSecond) => {
+      countCall++;
+
+      if (countCall > 3 && countCall % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return numberFirst + numberSecond;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
