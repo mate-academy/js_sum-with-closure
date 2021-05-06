74e0a0c02f506b0f999beb4dc2895bee4fad37e5
Igor Lisogor <illisogori@gmail.com>
2021-05-06 17:58:29 +0300

    robot accountant implemented

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCount = 0;
+  const errorMessage = 'Bzzz... Error!';
+
+  return function(firstNumber = 0) {
+    callsCount++;
+
+    return (secondNumber = 0) => {
+      return (callsCount > 3 && callsCount % 2 === 0)
+        ? errorMessage : firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
