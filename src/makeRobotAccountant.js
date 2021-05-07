47e932e8ae0fd3317ee0884e4d6cb6487c3d6efb
Yurii Lisovenko <yriyliso@gmail.com>
2021-05-07 13:27:58 +0300

    created accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (x) => {
+    count++;
+
+    return function(y) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
