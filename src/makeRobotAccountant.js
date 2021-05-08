5bea3767f9917b91a8e600b2f5f00b464b04d687
victor <victorarytionowmail@gmail.com>
2021-05-08 15:51:57 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const nums = [];
+
+  const additor = function(num1) {
+    const innerAdditor = function(num2) {
+      const sum = num1 + num2;
+
+      nums.push(sum);
+
+      if (nums.length >= 4 && nums.length % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return sum;
+      }
+    };
+
+    return innerAdditor;
+  };
+
+  return additor;
 }
 
 module.exports = makeRobotAccountant;
