4131d9d132e1b52189d27999eaf24386ebde9bf2
Taras Kozak <kozaktarasofficial@gmail.com>
2021-05-10 22:11:12 +0300

    v1.00

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,15 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
+
+  return (num1) => {
+    count++;
+
+    return (num2) => (count < 4 || count % 2 !== 0)
+      ? num1 + num2
+      : 'Bzzz... Error!';
+  };
+};
 
 module.exports = makeRobotAccountant;
