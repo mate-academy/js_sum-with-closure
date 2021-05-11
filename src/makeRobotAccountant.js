06a00634a37ac1280d1484bcf11fa34feb63df11
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-05-11 22:31:48 +0300

    ready for check

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (x) => {
+    return function(y) {
+      count += 1;
+
+      const result = count > 3 && count % 2 === 0
+        ? `Bzzz... Error!` : x + y;
+
+      return result;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
