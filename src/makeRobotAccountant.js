daa7235169b4c3d94fd2ba5ba7682c9bafdf8d9b
Sasha Chornyi <sasha-202@inbox.ru>
2021-05-11 14:24:47 +0300

    fix

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,25 +9,17 @@ function makeRobotAccountant() {
   // write code here
   let count = 0;
 
-  const getSum = (arg1) => {
+  return (argOne) => {
     count++;
 
-    const res = (arg2) => {
-      if (count > 3) {
-        if (count % 2 === 0) {
-          return 'Bzzz... Error!';
-        } else {
-          return arg1 + arg2;
-        }
+    return (argTwo) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return argOne + argTwo;
       }
-
-      return arg1 + arg2;
     };
-
-    return res;
   };
-
-  return getSum;
 }
 
 module.exports = makeRobotAccountant;
