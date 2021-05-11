be5396a907b84229cb3668afb57c5b1fef7a1622
Artem Bazhenov <artem.bazhenov.325@mail.com>
2021-05-11 12:08:11 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,26 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+
+  const sum = (a) => (b) => {
+    if (count < 4) {
+      count++;
+
+      return a + b;
+    }
+
+    if ((count % 2) === 0) {
+      count++;
+
+      return 'Bzzz... Error!';
+    }
+    count++;
+
+    return a + b;
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
