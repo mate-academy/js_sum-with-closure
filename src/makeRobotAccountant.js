b811ff285f6f3a2d9bccd03be0f1d7299cecd1b8
Ivan Shpynda <ivanshpynda@gmail.com>
2021-05-11 16:56:31 +0300

    fixed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,23 +7,18 @@
 
 function makeRobotAccountant() {
   let sum = 0;
-  let isEven = 0;
 
   return function getSum(num1) {
-    // debugger;
-    isEven++;
     sum++;
 
-    if (isEven > 3 && isEven % 2 === 0) {
+    if (sum > 3 && sum % 2 === 0) {
       return () => {
         return 'Bzzz... Error!';
       };
     };
 
     return (num2) => {
-      sum = num1 + num2;
-
-      return sum;
+      return num1 + num2;
     };
   };
 };
