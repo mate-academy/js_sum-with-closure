959e9dfd1cbe0f750047727042652cc85844fdb7
Ivan Shpynda <ivanshpynda@gmail.com>
2021-05-11 12:15:41 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,26 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let sum = 0;
+  let isEven = 0;
+
+  return function getSum(num1) {
+    // debugger;
+    isEven++;
+    sum++;
+
+    if (isEven > 3 && isEven % 2 === 0) {
+      return () => {
+        return 'Bzzz... Error!';
+      };
+    };
+
+    return (num2) => {
+      sum = num1 + num2;
+
+      return sum;
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
