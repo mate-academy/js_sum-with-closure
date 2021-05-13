1acfc3a443bfd8fa1ea7905d061c76c853c066f8
amithos <alex.theutos@gmail.com>
2021-05-13 15:49:12 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,31 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let previousValue;
+  let counter = 0;
+
+  const getSum = (inputNumber) => {
+    if (!previousValue) {
+      previousValue = inputNumber;
+      counter++;
+
+      return getSum;
+    }
+
+    if (counter > 3 && counter % 2 === 0) {
+      previousValue = undefined;
+
+      return 'Bzzz... Error!';
+    }
+
+    const temporaryPreviousValue = previousValue;
+
+    previousValue = undefined;
+
+    return temporaryPreviousValue + inputNumber;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
