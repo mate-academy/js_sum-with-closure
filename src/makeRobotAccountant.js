6d2a136b79dac66101f8bc980e379760a1d2446d
VasylPerepichai <vperepichay@gmail.com>
2021-05-16 14:56:23 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  const getSum = function(num1) {
+    calls++;
+
+    if (calls <= 3 || calls % 2 !== 0) {
+      return (num2) => {
+        return num1 + num2;
+      };
+    } else {
+      return () => {
+        return 'Bzzz... Error!';
+      };
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
