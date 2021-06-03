4f7a6802a803ca25c2940e26cd17c805235bc783
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-06-03 11:22:40 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,23 @@
 
 function makeRobotAccountant() {
   // write code here
+  let limit = 0;
+
+  return function(x) {
+    limit++;
+
+    if (limit <= 3) {
+      return function(y) {
+        return x + y;
+      };
+    } else if (limit % 2 !== 0) {
+      return function(y) {
+        return x + y;
+      };
+    } else {
+      return () => ('Bzzz... Error!');
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
