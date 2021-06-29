3a80193e11e8b0ac32548e1c15affc96b568a365
korzh <korzh.alexey@yahoo.com>
2021-06-29 15:30:26 +0300

    solved

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,20 @@
 'use strict';
 
-/**
- *
- * @return {function}
- */
-
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const getSum = (a) => {
+    return (b) => {
+      count++;
+
+      if (count <= 3 || count % 2 === 1) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
