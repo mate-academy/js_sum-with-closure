3004f48c7aee1603d809f014ba6b3cce2cc5e904
Sviatoslav Kishka <sviatoslavkishka@gmail.com>
2021-06-30 17:02:01 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCount = 0;
+
+  function getSum(a) {
+    return function(b) {
+      callsCount++;
+
+      if (callsCount > 3 && callsCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
