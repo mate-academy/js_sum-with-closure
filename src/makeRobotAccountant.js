fad7e7fe99a2486c6d7791ca61c8987487936f1e
David Dohru <tronsmartmy@gmail.com>
2021-07-01 23:51:14 +0300

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (operand1) => (operand2) => {
+    count++;
+
+    if (count >= 4 && count % 2 === 0) {
+      return 'Bzzz... Error!';
+    }
+
+    return (operand1 + operand2);
+  };
 }
 
 module.exports = makeRobotAccountant;
