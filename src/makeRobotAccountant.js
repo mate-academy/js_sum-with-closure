639f028864bfb811cec585f234bf75719d47c691
Stas Pechenyi <spechenuy@gmail.com>
2021-07-06 13:33:38 +0300

    Homework was done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCount = 0;
+
+  const getSum = (valueA) => (valueB) => {
+    callsCount++;
+
+    switch (true) {
+      case callsCount < 4:
+      case (callsCount >= 4
+        && callsCount % 2 === 1):
+        return valueA + valueB;
+      case callsCount % 2 === 0:
+        return 'Bzzz... Error!';
+    }
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
