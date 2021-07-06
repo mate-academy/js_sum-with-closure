9c13f72a48faab1a49236404a1ab9340ac2d58b9
Stas Pechenyi <spechenuy@gmail.com>
2021-07-06 16:30:18 +0300

    Issues were fixxed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -11,13 +11,11 @@ function makeRobotAccountant() {
   const getSum = (valueA) => (valueB) => {
     callsCount++;
 
-    switch (true) {
-      case callsCount < 4:
-      case (callsCount >= 4
-        && callsCount % 2 === 1):
-        return valueA + valueB;
-      case callsCount % 2 === 0:
-        return 'Bzzz... Error!';
+    if (callsCount % 2 === 0
+      && callsCount >= 4) {
+      return 'Bzzz... Error!';
+    } else {
+      return valueA + valueB;
     }
   };
 
