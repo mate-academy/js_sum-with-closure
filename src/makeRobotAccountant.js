27ddc3444a3beac56fedd9d6615d48c5aab805e3
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-07-07 23:34:28 +0300

    message

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (first) => {
+    return (second) => {
+      count++;
+
+      if (count > 3) {
+        if (count % 2 === 0) {
+          return 'Bzzz... Error!';
+        } else {
+          return first + second;
+        }
+      }
+
+      return first + second;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
