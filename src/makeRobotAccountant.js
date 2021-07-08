ff5e8742f6044d45b1759708f91cb134c2710865
Сакевич Анастасія <asakevych@ASMAC.local>
2021-07-08 14:04:02 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,26 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  let amountOftries = 0;
+
+  const getSum = (a) => {
+    return (b) => {
+      count++;
+
+      if (count <= 3) {
+        return a + b;
+      } else if (amountOftries === 0) {
+        amountOftries++;
+
+        return `Bzzz... Error!`;
+      } else if (amountOftries > 0) {
+        return a + b;
+      }
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
