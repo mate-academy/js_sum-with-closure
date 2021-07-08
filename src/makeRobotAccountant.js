d737687d4627f5f8fc133063af2e290d5a255115
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-07-08 14:51:39 +0300

    sol 2.0

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,17 +6,15 @@
  */
 
 function makeRobotAccountant() {
-  let count = 1;
+  let count = 0;
 
   return (x) => {
     return (y) => {
-      if (count <= 3 || (count % 2) !== 0) {
-        count++;
-
-        return x + y;
-      }
-
       count++;
+
+      if (count <= 3 || (count % 2) !== 0) {
+        return x + y;
+      };
 
       return 'Bzzz... Error!';
     };
