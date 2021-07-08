a33f5f4a14e3ca3d3a390529201557d1f5355d68
Михаил <mihay0113@gmail.com>
2021-07-08 13:59:54 +0300

    redacted

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -12,9 +12,7 @@ function makeRobotAccountant() {
     count++;
 
     return function(b) {
-      if (count > 3 && ((a + b) % 2) === 0) {
-        count = 0;
-
+      if (count > 3 && (count % 2) === 0) {
         return 'Bzzz... Error!';
       }
 
