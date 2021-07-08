d49112034c2df242c2870ed73742bb1413045c2b
Yehor Merzlenko <yehor.merzlenko@gmail.com>
2021-07-08 09:06:18 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+  const errorMessage = 'Bzzz... Error!';
+
+  const getSum = (x) => {
+    return (y) => {
+      if (count % 2 !== 0 || count < 3) {
+        count++;
+
+        return x + y;
+      } else {
+        count++;
+
+        return errorMessage;
+      }
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
