45868609358ade39d9c57cd03ff00e0fc7d44e6d
Artem <temaqwerty15@gmail.com>
2021-07-08 16:06:32 +0300

    fin

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,32 @@
 'use strict';
 
-/**
+/*
  *
  * @return {function}
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let iterations = 0;
+
+  return (elementA) => {
+    return (elementB) => {
+      const aaddition = elementA + elementB;
+
+      if (iterations < 3) {
+        iterations++;
+
+        return aaddition;
+      } else {
+        iterations++;
+
+        if ((iterations - 1) % 2 !== 0) {
+          return 'Bzzz... Error!';
+        } else {
+          return aaddition;
+        }
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
