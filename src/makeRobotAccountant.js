07f946492ea86b2b0f27eb0cefbf516950f37367
Михаил <mihay0113@gmail.com>
2021-07-08 13:38:13 +0300

    makeRobot

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(a) {
+    count++;
+
+    return function(b) {
+      if (count > 3 && ((a + b) % 2) === 0) {
+        count = 0;
+
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
