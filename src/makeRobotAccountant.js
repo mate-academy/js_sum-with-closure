5a5ced0c5ca458eea6caaa164e910cadb00a71da
tkachukione <tkachukione@gmail.com>
2021-07-08 20:43:39 +0300

    first solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let cnt = 0;
+
+  return function getSum(firstNumber) {
+    cnt++;
+
+    return function calculate(secondNumber) {
+      return (cnt > 3 && cnt % 2 === 0)
+        ? 'Bzzz... Error!'
+        : firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
