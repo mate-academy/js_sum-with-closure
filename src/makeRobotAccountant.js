84072faa210c5257383e7e4b9c02a6a7d7fbafd0
Larin Dmytro <dimiklarin420@gmail.com>
2021-07-12 19:55:11 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const robotAccountant = (num1) => {
+    count++;
+
+    return (num2) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return num1 + num2;
+    };
+  };
+
+  return robotAccountant;
 }
 
 module.exports = makeRobotAccountant;
