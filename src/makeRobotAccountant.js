59b4a75cf906c0e825e4a10fa3cf4c49053d568b
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-07-16 11:20:56 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCount = 0;
+
+  const getSum = (param1) => {
+    const getSum2 = (param2) => {
+      callsCount++;
+
+      const sum = param2 + param1;
+
+      if (callsCount > 3 && callsCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      };
+
+      return sum;
+    };
+
+    return getSum2;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
