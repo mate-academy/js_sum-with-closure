4e896382605aa972e2dd7c8d05dbe235ee70469a
Raz-Dva <my.poshta.goncharov@gmail.com>
2021-07-18 16:38:00 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
+
+  return (num1) => {
+    return (num2) => {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      };
+
+      return num1 + num2;
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
