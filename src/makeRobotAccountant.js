7f211e75576d9a66b3cb17a20dde0cc2a598de1e
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-07-27 12:42:26 +0300

    Task Done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(x) {
+    count++;
+
+    return function(y) {
+      if (count <= 3) {
+        return x + y;
+      } else {
+        if (count % 2 !== 0) {
+          return x + y;
+        }
+
+        return 'Bzzz... Error!';
+      };
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
