afc3e2abbc377bdfb6a151eb9e1aaac96cea03e5
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-10 20:56:59 +0300

    changes after review

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,14 +8,16 @@
 function makeRobotAccountant() {
   let sumCallsCount = 0;
 
-  return (a) => (b) => {
-    sumCallsCount++;
+  return (a) => {
+    return (b) => {
+      sumCallsCount++;
 
-    if (sumCallsCount > 3 && sumCallsCount % 2 === 0) {
-      return 'Bzzz... Error!';
-    }
+      if (sumCallsCount > 3 && sumCallsCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
 
-    return a + b;
+      return a + b;
+    };
   };
 }
 
