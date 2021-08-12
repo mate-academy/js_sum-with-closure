454cf88dcd7df006572b1ff6eac619a924b92c38
darynakravchuk <darinakravchuk@gmail.com>
2021-08-12 16:05:14 +0300

    accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,21 @@
 
 function makeRobotAccountant() {
   // write code here
-}
+  let count = 0;
+
+  function newValue(x) {
+    count++;
+
+    return function(y) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return x + y;
+      }
+    };
+  };
+
+  return newValue;
+};
 
 module.exports = makeRobotAccountant;
