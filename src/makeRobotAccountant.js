e7d9fad91d3af82ba0c12637751253aed70a0308
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-08-14 21:52:39 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let evenOdd = 0;
+
+  return (a) => {
+    return (b) => {
+      if (evenOdd < 3) {
+        evenOdd++;
+
+        return a + b;
+      }
+
+      if (evenOdd % 2 === 1) {
+        evenOdd++;
+
+        return 'Bzzz... Error!';
+      }
+
+      evenOdd++;
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
