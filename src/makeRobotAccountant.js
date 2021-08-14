7212876ee5fc5b6a7f90156dc8d3264c4c1b72f4
Dima Tkachenko <DimaTkachenko3@gmail.com>
2021-08-14 21:56:28 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let getSumCounter = 0;
+  const getSum = (firstNumber) => {
+    return (secondNumber) => {
+      getSumCounter++;
+
+      if (getSumCounter > 3 && getSumCounter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
