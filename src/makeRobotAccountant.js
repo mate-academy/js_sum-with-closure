3a906cefccaa467cf2d2daa3e0d2fa89205a331a
lera-yaskevych <redrum.back@gmail.com>
2021-08-15 12:32:10 +0300

    solved

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let operationCount = 0;
+
+  const getSum = (x) => {
+    return function(y) {
+      operationCount++;
+
+      if (operationCount < 3 || operationCount % 2 !== 0) {
+        return x + y;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
