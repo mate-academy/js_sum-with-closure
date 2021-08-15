9a04b9a0f49f8f4f276fb58152bdb9ac8f3fc52d
Vova-Stoliar <stoliarmoney@gmail.com>
2021-08-15 08:55:09 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,25 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
+function makeRobotAccounterant() {
   // write code here
+  let currentResult = 0;
+  let counter = 0;
+
+  return (firstNumber) => {
+    currentResult = 0;
+    currentResult += firstNumber;
+
+    return (secondNumber) => {
+      counter++;
+
+      if (counter <= 3 || counter % 2 !== 0) {
+        return currentResult + secondNumber;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
-module.exports = makeRobotAccountant;
+module.exports = makeRobotAccounterant;
