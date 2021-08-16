02809a33761fff88ae8661ad642bdd05fa6c942e
unknown <hado.roman.ihorovych@gmail.com>
2021-08-16 23:22:48 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  const getSum = function(firstNum) {
+    calls++;
+
+    return secondNum => {
+      if (calls >= 4 && calls % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return firstNum + secondNum;
+      }
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
