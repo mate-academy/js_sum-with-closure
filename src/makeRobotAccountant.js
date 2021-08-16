ddc15e6b149a2a7ae4aee71c82560889f60c8ef3
Ula Dutchak <ulana.dutchak@gmail.com>
2021-08-16 16:10:44 +0300

    make robot accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countOfCalls = 0;
+
+  return (a) => {
+    countOfCalls++;
+
+    return (b) => {
+      if (countOfCalls > 3 && countOfCalls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
