32f1bb2c93c0e7e835843411592f69e305e7d380
Derelict <oleksandr.dzyndzyura@gmail.com>
2021-08-16 12:16:16 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (firstNumber) => (secondNumber) => {
+    count += 1;
+
+    if (count > 3 && count % 2 === 0) {
+      return 'Bzzz... Error!';
+    } else {
+      return firstNumber + secondNumber;
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
