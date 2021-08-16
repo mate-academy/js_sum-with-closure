59a38742999c8387ec47bcaf5185f208cc0ebc2f
Andrii-Pinchuk <altkobalt4@gmail.com>
2021-08-16 19:02:30 +0300

    Robot accountant solved

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return number1 => {
+    count++;
+
+    if (count > 3 && count % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return (number2) => number1 + number2;
+  };
 }
 
 module.exports = makeRobotAccountant;
