fd88950ac307eb419a335dab4a123bc896117447
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-08-16 21:05:30 +0300

    done task sum with closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return function scope(arg1) {
+    return function getSum(arg2) {
+      const sum = arg1 + arg2;
+
+      counter++;
+
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return sum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
