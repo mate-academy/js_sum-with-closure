5053b254e57a8fe5bfc8a2633340ea25a10f8acf
Yevhenii Sviderskyi <evgen1sider@gmail.com>
2021-08-17 11:41:06 +0300

    that is work

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const getSum = function(a) {
+    return function(b) {
+      count++;
+
+      if (count < 3 || count % 2 !== 0) {
+        return a + b;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
