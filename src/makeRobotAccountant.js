40c27709e52c02f948252d439ba527e1319d766d
Oleksandr Zhitenko <geetonio28@gmail.com>
2021-08-17 17:48:51 +0300

    task is done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  const sum = firstNumber => {
+    count++;
+
+    if (count <= 3 || count % 2 !== 0) {
+      return secondNumber => firstNumber + secondNumber;
+    }
+
+    return () => 'Bzzz... Error!';
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
