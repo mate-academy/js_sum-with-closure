e55fc0fae3ee06d61671c7c462290cd05f6c577c
Clon <clonvhs@gmail.com>
2021-08-18 12:25:26 +0300

    Solution
    v1.0

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return (a) => {
+    callCount++;
+
+    if (callCount % 2 === 0 && callCount > 3) {
+      return (b) => {
+        return 'Bzzz... Error!';
+      };
+    } else {
+      return (b) => {
+        return a + b;
+      };
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
