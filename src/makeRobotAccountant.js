b8ff882e5e4885aafdc6aa8ac0c1c221bb2e5403
Oleksandr Strilchuk <virass.m@gmail.com>
2021-08-19 11:55:29 +0300

    Complete task

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return function getSum(firstValue) {
+    return function(secondValue) {
+      counter++;
+
+      if (counter <= 3 || counter % 2 === 1) {
+        return firstValue + secondValue;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
