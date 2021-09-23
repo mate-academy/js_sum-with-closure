06d717f32f1f71ff83f591e285cc97ae1abaa4d0
Victor Shevchuk <callmevitok@gmail.com>
2021-09-24 02:05:13 +0300

    rBt

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const adder = (a) => {
+    count++;
+
+    if (count > 3) {
+      if (count % 2 === 0) {
+        return (b) => 'Bzzz... Error!';
+      } else {
+        return (b) => a + b;
+      }
+    } else {
+      return (b) => a + b;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeRobotAccountant;
