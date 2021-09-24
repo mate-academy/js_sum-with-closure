eab1b88a4c0f72180a1591312f9b763de1f9fc54
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-09-24 15:40:16 +0300

    js_sum-with-closure done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
+
+  return function(term1) {
+    count++;
+
+    return function(term2) {
+      if (count % 2 === 0 && count > 3) {
+        return 'Bzzz... Error!';
+      } else {
+        return term1 + term2;
+      };
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
