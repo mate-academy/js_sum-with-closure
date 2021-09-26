037b72ab7d8463a13bf4e74fc77016aeec18f747
Iurii Medvedchuk <io.medvedchuk@gmail.com>
2021-09-26 11:04:00 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,31 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  let numberToRemember = 0;
+
+  const adder = (firstNumber) => {
+    numberToRemember = firstNumber;
+    counter++;
+
+    const sum = (secondNumber) => {
+      return numberToRemember + secondNumber;
+    };
+
+    const glitch = () => {
+      return 'Bzzz... Error!';
+    };
+
+    if (counter > 3) {
+      return (counter % 2)
+        ? sum
+        : glitch;
+    } else {
+      return sum;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeRobotAccountant;
