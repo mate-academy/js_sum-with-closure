54ffed92ffb1f998dae87a23308ec0391be84b7c
Artem Bolshak <fcdnipro20152705@gmail.com>
2021-09-27 23:27:13 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => {
+    return (y) => {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return x + y;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
