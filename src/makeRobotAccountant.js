58534a5cc99176d326f34f4c81b935662f39e0ad
Oleksij Lyashenko <oleksijlyashenko@MacBook-Air-Mary.local>
2021-09-28 14:32:41 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const result = (a) => {
+    count++;
+
+    return (b) => {
+      return (count > 3 && count % 2 === 0)
+        ? 'Bzzz... Error!'
+        : a + b;
+    };
+  };
+
+  return result;
 }
 
 module.exports = makeRobotAccountant;
