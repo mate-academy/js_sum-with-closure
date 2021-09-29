eb582488f383b00c156c81b745b51f98a233ae15
Sviat Torba <torba.svyatoslav.fox@gmail.com>
2021-09-29 22:21:12 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return (firstNum) => {
+    callCount++;
+
+    return (secondNum) => {
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNum + secondNum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
