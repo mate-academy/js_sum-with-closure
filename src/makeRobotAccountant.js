eb065a9a71dbf7af7d62f98e184e55f826d4264e
Taras Kozak <taraskozakmail@gmail.com>
2021-10-15 16:51:05 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,19 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+ function makeRobotAccountant() {
+  let counter = 0;
+
+  return function sum (x) {
+    return function sum1 (y) {
+      const result = x + y
+      counter++;
+      if(counter < 3) {
+        return result
+      }
+      return counter % 2 === 0 ? 'Bzzz... Error!' : result;
+    }
+  }
 }
 
 module.exports = makeRobotAccountant;
