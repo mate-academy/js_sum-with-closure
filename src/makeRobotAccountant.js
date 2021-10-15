98494fde1740525d4cc9b38c90f17ae6850fd425
Olexandr Yatsykovych <sashayatsikovic@gmail.com>
2021-10-15 17:55:27 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let attempts = 3;
+  let functionCallCounter = 0;
+
+  return function(num1) {
+    --attempts;
+
+    return function(num2) {
+      if (attempts < 0) {
+        ++functionCallCounter;
+
+        if (functionCallCounter % 2) {
+          return 'Bzzz... Error!';
+        }
+      }
+
+      return num1 + num2;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
