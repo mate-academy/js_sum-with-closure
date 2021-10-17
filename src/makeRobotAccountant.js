bff962df0c3d3479672119ec03700598b93f1674
cyb1ove <cyb1ove.ua@gmail.com>
2021-10-17 13:28:19 +0300

    Task was solved

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      count++;
+
+      if (count % 2 === 0 && count !== 2) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
