c347a7e0247a121b3c04bfec51da5b12775df2c3
Igor Kovalev <bizzzy0@gmail.com>
2021-10-19 16:41:50 +0300

    Task was solved

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,30 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let outerCount = 0;
+  let innerCounter = 0;
+
+  return function(firstNumber) {
+    return function(secondNumber) {
+      if (outerCount >= 3) {
+        const isEven = innerCounter % 2 === 0;
+
+        if (isEven) {
+          innerCounter++;
+
+          return 'Bzzz... Error!';
+        } else {
+          innerCounter++;
+
+          return firstNumber + secondNumber;
+        }
+      } else {
+        outerCount++;
+
+        return firstNumber + secondNumber;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
