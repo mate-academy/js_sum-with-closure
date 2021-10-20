ca3cd1e9646aacce73bce6ddfcf21e317fb2c78e
Denys Stryzhalko <samu3lkacis@gmail.com>
2021-10-20 14:42:24 +0300

    Sum-with-Closures v1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return function(firstOperand) {
+    return function(secondOperand) {
+      callCount++;
+
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstOperand + secondOperand;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
