af8fa22b69b007830a1b5b59c1f10233304bca53
Misha Rosa <misharosa.lviv@gmail.com>
2021-10-20 13:41:21 +0300

    fabruka stvorena=)

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (a) => {
+    counter++;
+
+    return (b) => {
+      if (counter <= 3) {
+        return a + b;
+      } else {
+        return counter % 2 === 0 ? `Bzzz... Error!` : a + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
