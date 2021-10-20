689e0496401a881366dcd97e0bd31d9c75914048
Denys Anosov <anosovdenys@gmail.com>
2021-10-20 18:18:16 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(number) {
+    return function(digit) {
+      count++;
+
+      if (count % 2 === 0 && count > 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return number + digit;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
