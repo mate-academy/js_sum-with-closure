e433e2078d8ced5816437e0f2b6bbefabcfab43e
Andrii Suprun <86914377+andrewSuprun@users.noreply.github.com>
2021-10-22 19:31:18 +0200

    Update makeRobotAccountant.js

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,22 @@
  * @return {function}
  */
 
+
 function makeRobotAccountant() {
   // write code here
+  let counter = 0;
+
+  return (x) => {
+    return (y) => {
+      counter++;
+
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
