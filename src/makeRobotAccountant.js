52e53a35b038de896a410c9dfdd01a1209b4e833
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-11 18:50:03 +0200

    Implement a machine that will be counting my money from now on

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const CAN_HANDLE = 3;
+  let executionCount = 0;
+
+  const timeToGlitch = () =>
+    executionCount > CAN_HANDLE && executionCount % 2 === 0;
+
+  function account(bucksFromUncle) {
+    executionCount++;
+
+    return (bucksFromAunt) => {
+      if (timeToGlitch()) {
+        return 'Bzzz... Error!';
+      }
+
+      const myBucks = bucksFromUncle + bucksFromAunt;
+
+      return myBucks;
+    };
+  }
+
+  return account;
 }
 
 module.exports = makeRobotAccountant;
