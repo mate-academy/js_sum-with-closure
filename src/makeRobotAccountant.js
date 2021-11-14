799dcc669e633dc46c8bf1e6128336da69cf3025
Dariia Romanova <dariia.romanova@gmail.com>
2021-11-14 20:59:11 +0200

    Robot Accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,26 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 3;
+  let evenCount = 0;
+
+  const getSum = (num1) => {
+    const adding = (num2) => {
+      count--;
+
+      if (count < 0 && evenCount % 2 === 0) {
+        evenCount++;
+
+        return 'Bzzz... Error!';
+      }
+
+      return num1 + num2;
+    };
+
+    return adding;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
