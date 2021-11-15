99733761326884bdc612471975b6795f3c32e8d9
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-15 19:21:19 +0200

    Fix syntatic issues

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,14 +9,15 @@ function makeRobotAccountant() {
   const CAN_HANDLE = 3;
   let executionCount = 0;
 
-  const timeToGlitch = () =>
-    executionCount > CAN_HANDLE && executionCount % 2 === 0;
+  const isTimeToGlitch = () => (
+    executionCount > CAN_HANDLE && executionCount % 2 === 0
+  );
 
   function account(bucksFromUncle) {
     executionCount++;
 
     return (bucksFromAunt) => {
-      if (timeToGlitch()) {
+      if (isTimeToGlitch()) {
         return 'Bzzz... Error!';
       }
 
