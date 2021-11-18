d0cd3aa93eac299fd374d829325186315e0c1584
Vitaliy Voytovich <voitovichatque@gmail.com>
2021-11-18 08:36:32 +0200

    RobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const addNum = (a) => {
+    return (b) => {
+      if (count > 2 && count % 2 === 1) {
+        count++;
+        return `Bzzz... Error!`;
+      }
+      count++;
+  
+      return a + b;
+    }
+  };
+
+  return addNum;
 }
 
 module.exports = makeRobotAccountant;
