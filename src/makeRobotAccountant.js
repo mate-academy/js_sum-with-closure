264e82f2c346bdbffc3975c84b9e716388b3762b
Alex Kostomarov <kostomarov.aleks@gmail.com>
2021-11-18 16:58:31 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,31 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const accountantData = {
+    callsNum: 0,
+    num: null,
+  };
+
+  const accountant = (num) => {
+    if (accountantData.num === null) {
+      accountantData.callsNum++;
+      accountantData.num = num;
+
+      return accountant;
+    }
+
+    const sum = num + accountantData.num;
+
+    accountantData.num = null;
+
+    if (accountantData.callsNum < 3 || accountantData.callsNum % 2 !== 0) {
+      return sum;
+    }
+
+    return 'Bzzz... Error!';
+  };
+
+  return accountant;
 }
 
 module.exports = makeRobotAccountant;
