40ffd67f919deb1c8ab3e15af898f92a01faafaa
Olha Timoshenko <o.b.timoshenko@gmail.com>
2021-11-19 17:38:26 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let cashCount = 1;
+
+  return (a) => (b) => {
+    if (cashCount % 2 === 0 && cashCount !== 2) {
+      ++cashCount;
+
+      return 'Bzzz... Error!';
+    } else {
+      ++cashCount;
+
+      return a + b;
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
