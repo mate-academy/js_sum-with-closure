2d1ec5c24d397bb03a4d19f32cacfeb621f21b5e
Oleksiy Pochkayev <oleksiypochkayev@gmail.com>
2021-11-19 15:38:47 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+
+  const result = (a) => {
+    return (b) => {
+      if (count <= 3 || count % 2 !== 0) {
+        count++;
+
+        return a + b;
+      } else {
+        count++;
+
+        return 'Bzzz... Error!';
+      }
+    };
+  };
+
+  return result;
 }
 
 module.exports = makeRobotAccountant;
