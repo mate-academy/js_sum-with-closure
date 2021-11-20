badd6eba501fad705d1f49952ee43f0eec0f5cbd
WorkAccCom <work.acc.com@gmail.com>
2021-11-20 19:45:16 +0200

    First solution: switch case

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCounter = 0;
+
+  return (x) => {
+    return (y) => {
+      callCounter++;
+
+      switch (true) {
+        case (callCounter <= 3): {
+          return x + y;
+        }
+
+        case (callCounter % 2 === 0): {
+          return 'Bzzz... Error!';
+        }
+
+        default: {
+          return x + y;
+        }
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
