42ce8590a8d2ec81afe069b0989d54e515d5760e
Vasiliy <voronvasia@gmail.com>
2021-11-20 13:34:44 +0200

    task complite

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  if (count < 3) {
+    return function (n1) {
+      return function (n2) {
+        count++;
+
+        if (count >= 4 && count % 2 === 0) {
+          return 'Bzzz... Error!';
+        }
+
+        return n1 + n2;
+      };
+    };
+  }
 }
 
 module.exports = makeRobotAccountant;
