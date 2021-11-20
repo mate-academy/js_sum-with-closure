1e5fc1ec6eb9cf2d2f6ae197b3223ebcad32825c
Vasiliy <voronvasia@gmail.com>
2021-11-20 19:22:13 +0200

    update code

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,19 +8,18 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  if (count < 3) {
-    return function (n1) {
-      return function (n2) {
-        count++;
+  return function (n1) {
+    return function (n2) {
+      count++;
 
-        if (count >= 4 && count % 2 === 0) {
-          return 'Bzzz... Error!';
-        }
+      if (count >= 4 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
 
-        return n1 + n2;
-      };
+      return n1 + n2;
     };
-  }
+  };
+
 }
 
 module.exports = makeRobotAccountant;
