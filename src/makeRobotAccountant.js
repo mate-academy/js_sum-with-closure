e69c645ca6cf5fb834cba197df0ddc4d1d5f2b22
VladDyrenkov <59515329+VladDyrenkov@users.noreply.github.com>
2021-11-21 03:08:43 +0200

    Done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countCallFuncrion = 0;
+
+  const getSum = (firstValue) => {
+    countCallFuncrion++;
+
+    return function(secondValue) {
+      if (countCallFuncrion > 3 && !(countCallFuncrion % 2)) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstValue + secondValue;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
