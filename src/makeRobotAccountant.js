d1644a8df4a0dd0d0c306e2f0608db26f87f3dc9
Lera Permiakova <vv.permiakova@gmail.com>
2021-11-21 10:30:13 +0200

    Make Robot Acoount

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => {
+    return (y) => {
+      count++;
+
+      if (count <= 3) {
+        return x + y;
+      } else if (count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return x + y;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
