ee6c51a29d959b7c57ef73f9f29cd8dd00497aaa
Ihor Poliakov <junglqq@gmail.com>
2021-11-21 11:44:26 +0200

    init

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (firstNum) => {
+    counter++;
+
+    let num = firstNum;
+
+    return (secondNum) => {
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      num += secondNum;
+
+      return num;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
