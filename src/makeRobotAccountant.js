ef5f04ee3a3594a94bf71067d344475f19d3d0fd
Vladyslav Gonsales <www.magicbox@gmail.com>
2021-11-22 15:02:25 +0200

    fixed code style

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,15 +8,16 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  const getSum = (a) => (b) => {
-    count++;
+  const getSum = (a) =>
+    (b) => {
+      count++;
 
-    if (count >= 3 && count % 2 === 0) {
-      return 'Bzzz... Error!';
-    }
+      if (count >= 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
 
-    return a + b;
-  };
+      return a + b;
+    };
 
   return getSum;
 }
