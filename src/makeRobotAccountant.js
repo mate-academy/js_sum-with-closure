d888c909a0773423324c0e9d33023077073670ed
lanebx <havchikos@gmail.com>
2021-11-22 13:33:43 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let checker = -1;
+
+  return (a) => {
+    return (b) => {
+      checker++;
+
+      return checker % 2 === 0 || checker === 1
+        ? a + b
+        : 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
