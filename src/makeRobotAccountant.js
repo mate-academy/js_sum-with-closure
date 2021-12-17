e22f573383c8cddd3e2a8b3ad7911edf86980006
Oleksandr Leshchenko <bulochkin72@gmail.com>
2021-12-17 17:14:39 +0200

    commit added

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,28 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const sum = 0;
+  let count = 0;
+
+  const getSum = (a) => {
+    const firstIteration = sum + a;
+
+    return (b) => {
+      const secondIteration = firstIteration + b;
+
+      count++;
+
+      if (count > 3) {
+        count = 0;
+
+        return 'Bzzz... Error!';
+      }
+
+      return secondIteration;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
