7dd2ac9b407becd14a43d11d4e1fa30a3f77a7b0
levix2l <alexleviz13@gmail.com>
2021-12-23 14:28:24 +0200

    sum with closure solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (num1) => {
+    const sum = (num2) => {
+      if (count % 2 !== 0) {
+        return num1 + num2;
+      }
+
+      if (count >= 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return num1 + num2;
+    };
+
+    count++;
+
+    return sum;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
