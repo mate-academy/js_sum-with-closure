921d92ec07d0c0a56a13e7d63691c0e79ddbc4ae
levix2l <alexleviz13@gmail.com>
2021-12-23 15:24:54 +0200

    count fix

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -10,19 +10,30 @@ function makeRobotAccountant() {
 
   const getSum = (num1) => {
     const sum = (num2) => {
-      if (count % 2 === 0 && count >= 3) {
+      count++;
+
+      if (count % 2 === 0 && count > 3) {
         return 'Bzzz... Error!';
       }
 
       return num1 + num2;
     };
 
-    count++;
-
     return sum;
   };
 
   return getSum;
+  // let count = 0;
+
+  // const countSum = (value = 0) => {
+  //   count++;
+
+  //   return (secondValue = 0) => count > 3 && count % 2 === 0
+  //     ? 'Bzzz... Error!'
+  //     : value + secondValue;
+  // };
+
+  // return countSum;
 }
 
 module.exports = makeRobotAccountant;
