6877efc2c6c07eb72e011e1bbd64048e75cd9536
SavelovSerhii <savelov12111@gmail.com>
2021-12-24 13:27:03 +0200

    First version

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function calculate(x) {
+    return function(y) {
+      if (count < 3) {
+        count++;
+
+        return x + y;
+      }
+
+      count--;
+
+      return 'Bzzz... Error!';
+    };
+  }
+
+  return calculate;
 }
 
 module.exports = makeRobotAccountant;
