e2cbf45ad53ef94c0897c201bd7afe593696bf38
Serhii Haidukov <sergeyglazgo@gmail.com>
2021-12-25 18:50:21 +0200

    solution2

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,24 +5,15 @@
  * @return {function}
  */
 
-// Reproduce the behavior of our robots. Create a function
-// `makeRobotAccountant`, the result of which is a function that
-// works on the pattern `getSum (4) (5) === 9`.
-// The first 3 calls to this feature work fine.
-// Then all even calls return the string `Bzzz... Error!`,
-// And odd calls the correct value.
-
 function makeRobotAccountant() {
   let counter = 1;
 
-  return (firstValue) => {
+  return (firstValue) => (secondValue) => {
     counter++;
 
-    return (secondValue) => {
-      return (counter > 3 && counter % 2)
-        ? 'Bzzz... Error!'
-        : firstValue + secondValue;
-    };
+    return (counter > 3 && counter % 2)
+      ? 'Bzzz... Error!'
+      : firstValue + secondValue;
   };
 }
 
