09ba634a63ee71f83047217efc54aa401e3aebb5
michael valiahmetov <michael.valiahmetov@gmail.com>
2021-12-25 21:43:11 +0200

    Robot-accauntant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let limit = 3;
+
+  const getSum = function(a) {
+    limit--;
+
+    if (limit >= 0 || limit % 2 === 0) {
+      const sum = function(b) {
+        return a + b;
+      };
+
+      return sum;
+    } else {
+      const error = function() {
+        return 'Bzzz... Error!';
+      };
+
+      return error;
+    }
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
