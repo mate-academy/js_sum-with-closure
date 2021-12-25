f79c5a8c51d578ac2e0e86dbc47911748c56d77e
Samson Sim <samson.sim@ukr.net>
2021-12-26 00:46:29 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (number1 = 0) => {
+    count++;
+
+    if (count > 3 && count % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return (number2 = 0) => number1 + number2;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
