1e1631f0fcd0c70ca311139f6e1cce9d82ae90a7
Alex Ivanov <bcmp4p1@gmail.com>
2021-12-27 23:38:52 +0200

    save

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (firstNumber) => {
+    return (secondNumber) => {
+      count++;
+
+      if ((count > 3) && (count % 2 === 0)) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
