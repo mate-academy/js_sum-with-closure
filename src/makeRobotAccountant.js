5de9d669894208350a91716db03956f0bcac922e
Yevhenii Rozdorozhnii <rozdorozhnii@ukr.net>
2021-12-28 00:04:49 +0200

    solution_v1.0

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  const getSum = function(x) {
+    counter++;
+
+    return function(y) {
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
