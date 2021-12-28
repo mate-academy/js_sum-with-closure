ff2daf4c4febf22a1e5c05475d79f43e22953f74
Iryna Zolko <koko_irina@ukr.net>
2021-12-28 17:40:11 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,28 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  const getSum = (x) => {
+    counter++;
+
+    return function(y) {
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
+
+// Создайте функцию makeRobotAccountant,
+// результатом которой будет функция,
+// работающая по шаблону getSum (4) (5) === 9.
+// Первые 3 вызова этой функции работают нормально.
+// Тогда все четные вызовы возвращают строку Bzzz ... Error !,
+// а нечетные вызовы правильное значение.
