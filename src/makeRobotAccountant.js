7a069d01fe69e01eb30b96c91d13ee5ff9c0def4
Yevhenii Korzun <marty.music.official@gmail.com>
2021-12-28 02:20:58 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const sum = function(a) {
+    count++;
+
+    if ((count <= 3) || (count % 2 !== 0)) {
+      return (b) => a + b;
+    }
+
+    return () => 'Bzzz... Error!';
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
