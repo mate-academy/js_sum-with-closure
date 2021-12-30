1809179631e1508ff504e0be036b222268b0caf1
Ernesto Fernandez <ernesto.fernandez.1980@gmail.com>
2021-12-30 04:05:33 +0200

    some message

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  const getSum = (number) => {
+    counter++;
+
+    if (counter < 3) {
+      return (x) => x + number;
+    } else {
+      if (counter % 2 === 0) {
+        return (x) => 'Bzzz... Error!';
+      } else {
+        return (x) => x + number;
+      }
+    }
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
