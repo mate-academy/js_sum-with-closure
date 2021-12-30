75e8765bae6d76cc915354537205b04fe6482b7c
Ernesto Fernandez <ernesto.fernandez.1980@gmail.com>
2021-12-30 11:25:47 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -11,15 +11,11 @@ function makeRobotAccountant() {
   const getSum = (number) => {
     counter++;
 
-    if (counter < 3) {
-      return (x) => x + number;
-    } else {
-      if (counter % 2 === 0) {
-        return (x) => 'Bzzz... Error!';
-      } else {
-        return (x) => x + number;
-      }
+    if (counter >= 3 && counter % 2 === 0) {
+      return (x) => 'Bzzz... Error!';
     }
+
+    return (x) => x + number;
   };
 
   return getSum;
