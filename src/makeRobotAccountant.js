b7a6a456c1cb852aaa1dbb8ea4e2906edb4d51ba
Ihor Barban <barbanihor@gmial.com>
2022-01-10 20:08:22 +0200

    Done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  function firstCall(x) {
+    function secondCall(y) {
+      counter++;
+
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+
+    return secondCall;
+  }
+
+  return firstCall;
 }
 
 module.exports = makeRobotAccountant;
