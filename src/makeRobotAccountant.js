89357461626ca7ce876879ec98b3c52672996ff8
Ladyha Roman <ladrom.ua@gmail.com>
2022-01-18 20:20:49 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let correction = 3;
+
+  const accounting = function(firstNum) {
+    return function(secondNum) {
+      if (correction === 0) {
+        correction++;
+
+        return 'Bzzz... Error!';
+      } else {
+        correction--;
+
+        return firstNum + secondNum;
+      }
+    };
+  };
+
+  return accounting;
 }
 
 module.exports = makeRobotAccountant;
