a91deeede12b2af9e587ffa0c698aef0fc031913
Roma Afanasiev <romafanasiev123@gmail.com>
2022-01-24 15:56:58 +0200

    add function

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,33 @@
 
 function makeRobotAccountant() {
   // write code here
+  let counter = 0;
+
+  return function(x) {
+    counter++;
+
+    return function(y) {
+      if (counter % 2 === 0 && counter >= 4) {
+        return 'Bzzz... Error!';
+      } else {
+        return x + y;
+      };
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
+
+// let counter = 0;
+
+//   const result = function(b) {
+//     counter++;
+
+//     if (counter % 2 === 0 && counter >= 3) {
+//       return `Bzzz... Error!`;
+//     } else {
+//       return a + b;
+//     }
+//   };
+
+//   return result;
