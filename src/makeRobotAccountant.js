c4aa345563697aaa35144ba697457230088241c8
Vadym Suslov <vsuslov2906@gmail.com>
2022-01-25 12:51:48 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let numberOfCalls = 0;
+
+  return (firstNnumber) => {
+    return (secondNnumber) => {
+      numberOfCalls++;
+
+      if (numberOfCalls > 3 && numberOfCalls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNnumber + secondNnumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
