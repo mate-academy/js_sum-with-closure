c610d247be1c1eb860cb2df8a04689eb13f2d79e
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-01-26 17:14:27 +0200

    ...

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,12 +8,12 @@
 function makeRobotAccountant() {
   let operationCount = 0;
 
-  return (a) => {
-    return (b) => {
+  return (num1) => {
+    return (num2) => {
       operationCount++;
 
       if (operationCount <= 3 || operationCount % 2 === 1) {
-        return a + b;
+        return num1 + num2;
       }
 
       return 'Bzzz... Error!';
