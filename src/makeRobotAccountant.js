059c5ef07b9283f306db1f027f057002ce0f082f
Yevhen Baiev <baev838@gmail.com>
2022-01-26 23:08:38 +0200

    First attempt

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (firstVar) => {
+    count += 1;
+
+    return (secondVar) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstVar + secondVar;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
