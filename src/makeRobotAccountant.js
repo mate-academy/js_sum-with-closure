613ecaae79e713e2c762d7cfaa722ec38a5c0421
Oleksii Pinchuk <oleksii.pin4uk@gmail.com>
2022-01-30 16:27:40 +0300

    sum

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,21 @@
 
 function makeRobotAccountant() {
   // write code here
+  let call = 0;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      call++;
+
+      const sum = firstNumber + secondNumber;
+
+      if (call > 3 && call % 2 !== 1) {
+        return 'Bzzz... Error!';
+      }
+
+      return sum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
