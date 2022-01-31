293683c3db2c2bc326f9181242e5e35b29f9d4a3
Олег Майборода <xa12850446@student.karazin.ua>
2022-01-31 22:42:02 +0200

    ez

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCounter = 0;
+
+  return function(firstNumber) {
+    return function(secondNumber) {
+      callCounter++;
+
+      if (callCounter > 3 && callCounter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
