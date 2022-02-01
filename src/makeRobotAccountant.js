9819ef535982e2d2fefc0c26635f0a72d1034417
Dorosh13 <dorosh.d.13@gmail.com>
2022-02-01 20:49:06 +0200

    created sum-with-closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+
+  return (x) => {
+    return (y) => {
+      if (count > 3 && count % 2 === 0) {
+        count++;
+
+        return 'Bzzz... Error!';
+      }
+
+      count++;
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
