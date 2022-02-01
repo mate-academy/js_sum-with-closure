e2aec74db281be4409c9d12df06e473056f69790
Kostya Kovbel <dovkerdota@gmail.com>
2022-02-01 14:23:44 +0200

    sol

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let currentIteration = 0;
+
+  return (number1) => {
+    return (number2) => {
+      currentIteration++;
+
+      return currentIteration <= 3 || currentIteration % 2 !== 0
+        ? number1 + number2
+        : 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
