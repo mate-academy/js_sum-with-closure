b4513cf43f27b5310f03b0941f0de3c5b936860c
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-02-01 13:44:41 +0200

    makeRobot

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCounter = 0;
+
+  return (firstCall) => {
+    return (secondCall) => {
+      callsCounter++;
+
+      if (callsCounter > 3 && callsCounter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstCall + secondCall;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
