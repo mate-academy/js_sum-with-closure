830ee0c22047aedd2a73b283b40947115509d5d8
Roman Kozhukhovskyi <35900365+GitRoko@users.noreply.github.com>
2022-02-01 16:32:22 +0200

    Update makeRobotAccountant.js

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,14 +7,13 @@
 
 function makeRobotAccountant() {
   // write code here
-  let count = 1;
+  let count = 0;
 
   return (x) => {
     return (y) => {
       count++;
       
       if (count > 3 && count % 2 === 0) {
-        
         return 'Bzzz... Error!';
       }
 
