79dc6f1dec86618d8b8074c6c3cfd0392b589095
Denis Stepanov <tt.denis.stepanov1996@gmail.com>
2022-02-01 23:53:06 +0200

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let counter = 0;
+
+  return (firstCall) => {
+    return (secondCall) => {
+      counter++;
+
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstCall + secondCall;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
