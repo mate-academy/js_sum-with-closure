c1cc2b7cd23220e54bb226abe09be7f4179d1d27
Andrian Melnyk <andrian_m@ukr.net>
2022-02-02 13:29:11 +0200

    Solution-1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
+
+  const getSum = function(x) {
+    return function(y) {
+      count++;
+
+      if (count <= 3) {
+        return x + y;
+      }
+
+      if (count > 3) {
+        if (count % 2 === 0) {
+          return 'Bzzz... Error!';
+        } else {
+          return x + y;
+        }
+      };
+    };
+  };
+
+  return getSum;
+};
 
 module.exports = makeRobotAccountant;
