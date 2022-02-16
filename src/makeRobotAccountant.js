653ccd7897e5759fff1942d2f1c2139a71451571
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-02-16 14:06:00 +0200

    Accountant task

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return number1 => number2 => {
+    counter++;
+
+    if (counter > 3 && ((counter - 3) % 2 !== 0)) {
+      return 'Bzzz... Error!';
+    }
+
+    return number1 + number2;
+  };
 }
 
 module.exports = makeRobotAccountant;
