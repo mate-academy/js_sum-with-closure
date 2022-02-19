85925127dea2bbfbfdd4113d71f411257c8d4170
krolich500grn <edu.izaytsev@gmail.com>
2022-02-19 13:59:31 +0200

    solution sum with closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (a) => (b) => {
+    count++;
+
+    if (count % 2 === 0 && count > 3) {
+      return 'Bzzz... Error!';
+    }
+
+    return a + b;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
