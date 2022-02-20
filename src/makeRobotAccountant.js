1a8857be76b0144d9df604394e56f75b8c364ca7
addKiwi <skrypak1997@gmail.com>
2022-02-20 21:28:11 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let canWork = 3;
+
+  return first => second => {
+    if (canWork > 0) {
+      canWork--;
+
+      return first + second;
+    } else {
+      canWork++;
+
+      return 'Bzzz... Error!';
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
