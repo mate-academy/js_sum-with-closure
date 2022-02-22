ea75c0f24b03a23bce2ffa8dbc3338aeda53311e
Danylo Kotliarenko <kotlyar099@gmail.com>
2022-02-22 17:54:49 +0200

    Sum of numbers

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  const firstNumber = (a) => {
+    counter++;
+
+    const secondNumber = (b) => {
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+
+    return secondNumber;
+  };
+
+  return firstNumber;
 }
 
 module.exports = makeRobotAccountant;
