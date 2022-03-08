a6e444be2b44c836590e02445295eb589e97c36c
Yuriy Shvorob <yuriyshvorob@Yuriys-MacBook-Pro.local>
2022-03-08 11:40:34 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  const errorMessage = 'Bzzz... Error!';
+
+  const getSum = (x) => {
+    counter++;
+
+    return (y) => {
+      if (counter > 3 && counter % 2 === 0) {
+        return errorMessage;
+      }
+
+      return x + y;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
