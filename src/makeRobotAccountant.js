7c1c63b7604fc8d287d5f2a05cb1c63277da9a73
Lilia Mazur <liliamazur2508@gmail.com>
2022-03-14 10:54:35 +0200

    first

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  const errorMessage = `Bzzz... Error!`;
+
+  const getSum = (firstElemen) => {
+    counter++;
+
+    return (secondElement) => {
+      if (counter > 3 && counter % 2 === 0) {
+        return errorMessage;
+      }
+
+      return firstElemen + secondElement;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
