eb8c1dd6ba64f9beaceae8d966df70cbe3d1b851
Dmytro Bychkivskyi <dbychkivskyi@cccis.com>
2022-03-20 21:35:37 +0200

    Finish hw.

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,16 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let attempts = 0;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      attempts++;
+
+      return (attempts <= 3 || attempts % 2 !== 0)
+        ? firstNumber + secondNumber : 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
