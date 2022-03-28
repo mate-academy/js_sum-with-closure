34394e474e941701ed06ba38b4598e63da4085fd
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-03-28 18:20:13 +0300

    remove comments

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,33 +1,5 @@
 'use strict';
 
-/**
- *
- * Accounting, numbers, many numbers. Boring.
- * The solution is to automate! The first robot accountant
- * came off the assembly line. I don't know how accountants'
- * brains don't melt, but when our robot starts adding numbers,
- * it starts to burn significantly and it counts every other time.
-  Reproduce the behavior of our robots.
-  Create a function `makeRobotAccountant`,
-  the result of which is a function that works on the pattern
-  `getSum (4) (5) === 9`.
-  The first 3 calls to this feature work fine.
-  Then all even calls return the string `Bzzz... Error!`,
-  And odd calls the correct value.
-
-  const getSum = makeRobotAccountant();
-  getSum(5)(6) === 11;
-  getSum(1)(3) === 4;
-  getSum(6)(6) === 12;
-  getSum(55)(16) === 'Bzzz... Error!';
-  getSum(55)(16) === 71;
-  getSum(55)(17) === 'Bzzz... Error!';
-  getSum(5)(17) === 22;
-  getSum(1)(1) === 'Bzzz... Error!';
- *
- * @return {function}
- */
-
 function makeRobotAccountant() {
   let count = 0;
 
