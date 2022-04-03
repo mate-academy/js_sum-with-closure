b6b45e8f0cd8bed2094e904542070aa1c1f1ad35
Dima <d.maik2178@gmail.com>
2022-04-03 06:29:02 +0300

    First commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => {
+    return (y) => {
+      if (count++ > 2) {
+        if (count % 2 === 0) {
+          return 'Bzzz... Error!';
+        } else {
+          return x + y;
+        }
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
