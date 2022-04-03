aa35cf9dd57d237b5359db851869a4509a9f339b
Inna <dragotaiv@gmail.com>
2022-04-03 19:39:55 +0300

    sum

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,26 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
+
+  const getSum = (x) => {
+    return function(y) {
+      const sum = x + y;
+
+      count++;
+
+      if ((count > 3) && (count % 2 === 0)) {
+        return 'Bzzz... Error!';
+      } else
+      if ((count > 3) && (count % 2 !== 0)) {
+        return sum;
+      }
+
+      return sum;
+    };
+  };
+
+  return getSum;
+};
 
 module.exports = makeRobotAccountant;
