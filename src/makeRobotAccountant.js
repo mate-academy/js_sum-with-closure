6579a6769dd25005706ebd7ba38b90bdebed44bc
MariaBondar <marybondar777@gmail.com>
2022-04-06 20:47:07 +0200

    develop

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const robot = (counter1) => {
+    count++;
+
+    return (counter2) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return counter1 + counter2;
+      }
+    };
+  };
+
+  return robot;
 }
 
 module.exports = makeRobotAccountant;
