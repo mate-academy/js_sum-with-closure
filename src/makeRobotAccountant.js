180ccc3fd8fe59cd802b0b1ada23f962878ee89d
Александр Погребняк <pogrebnyak19999@gmail.com>
2022-04-08 16:56:18 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (firstNum) => {
+    return (secondNum) => {
+      if (count < 3) {
+        count++;
+
+        return firstNum + secondNum;
+      } else if (count % 2 === 0) {
+        count++;
+
+        return firstNum + secondNum;
+      } else {
+        count++;
+
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
