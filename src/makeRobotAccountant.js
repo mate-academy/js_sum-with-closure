7fc19d34a01295283cc10acfe724917d99ca9bfb
Vlad Skoromnyi <VectraSton@gmail.com>
2022-04-11 11:48:23 +0300

    task complete

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(a) {
+    count++;
+
+    function add(b) {
+      if (count > 3 && count % 2 === 0) {
+        return `Bzzz... Error!`;
+      }
+
+      return a + b;
+    }
+
+    return add;
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
