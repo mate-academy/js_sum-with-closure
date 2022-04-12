0ecfdb951561358468ac6a77a57dc90d6856ca09
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-04-12 12:39:52 +0300

    Create solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,18 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  const errorMessage = 'Bzzz... Error!';
+
+  return (x) => {
+    return (y) => {
+      counter++;
+
+      const isEven = (counter % 2 === 0);
+
+      return (isEven && counter >= 3) ? errorMessage : x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
