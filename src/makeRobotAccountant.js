b6af5de216f622f1b77e594e57c8c446b878e65d
Max Nikiforov <maxxnikiforov@gmail.com>
2022-04-13 21:48:48 +0700

    Solution robot Accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = -1;
+
+  const robot = (arg1) => {
+    count++;
+
+    const robot2 = (arg2) => {
+      if (count <= 2) {
+        return arg1 + arg2;
+      }
+
+      if (count > 2 && count % 2 !== 0) {
+        return `Bzzz... Error!`;
+      }
+
+      return arg1 + arg2;
+    };
+
+    return robot2;
+  };
+
+  return robot;
 }
 
 module.exports = makeRobotAccountant;
