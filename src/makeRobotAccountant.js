9c15888a2b9bac8806171fa109bb1983bf09f468
Ruslana Tomnyuk <rusia.vasia1@gmail.com>
2022-04-14 20:51:47 +0300

    robotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(number1) {
+    count++;
+
+    function add(number2) {
+      if (count > 3 && count % 2 === 0) {
+        return `Bzzz... Error!`;
+      }
+
+      return number1 + number2;
+    }
+
+    return add;
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
