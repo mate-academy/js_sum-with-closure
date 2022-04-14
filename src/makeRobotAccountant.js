718405f7f6d9699dc1216d1985e442ced835531b
Ihor Mural <igor201278789887000@gmail.com>
2022-04-14 15:21:56 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  function getSum(x) {
+    return function(y) {
+      calls++;
+
+      if (calls < 4) {
+        return x + y;
+      }
+
+      if (calls > 4 && calls % 2 !== 0) {
+        return x + y;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
