0f1fc819a03076984ebe4ec39ffd3674bb77247e
Oleg Lementarev <lementarev@gmail.com>
2022-04-14 14:32:57 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,18 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const errorMessage = 'Bzzz... Error!';
+  let countOfCalls = 0;
+
+  return (x) => {
+    return (y) => {
+      countOfCalls++;
+
+      return countOfCalls <= 3 || countOfCalls % 2 === 1
+        ? x + y
+        : errorMessage;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
