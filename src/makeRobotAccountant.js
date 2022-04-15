027d56d81f3cef9b28407ab67edf14a933b10223
Dima Kotovich <kotovih950@gmail.com>
2022-04-15 18:42:03 +0300

    finish

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,25 @@
 
 function makeRobotAccountant() {
   // write code here
+  let result = 0;
+  let count = 0;
+
+  const getSum = (a) => {
+    const c = (b) => {
+      result = a + b;
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return result;
+      }
+    };
+
+    return c;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
