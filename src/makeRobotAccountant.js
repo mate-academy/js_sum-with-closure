f625461b535ba0322c44c24dc90ab2373a52cc59
unknown <avicostov2@gmail.com>
2022-04-19 10:45:17 +0200

    solut

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -4,9 +4,23 @@
  *
  * @return {function}
  */
-
 function makeRobotAccountant() {
   // write code here
+  let counter = 0;
+
+  function getSum(a) {
+    if (counter > 2) {
+      counter++;
+
+      return (counter - 1) % 2 !== 0 ? () => 'Bzzz... Error!' : (b) => a + b;
+    }
+
+    counter++;
+
+    return (b) => a + b;
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
