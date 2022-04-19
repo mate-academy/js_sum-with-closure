095907d9f2f3d04b96821346ea2c9cc2384f9533
unknown <avicostov2@gmail.com>
2022-04-19 10:50:36 +0200

    solut2 developed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,15 +9,11 @@ function makeRobotAccountant() {
   let counter = 0;
 
   function getSum(a) {
-    if (counter > 2) {
-      counter++;
-
-      return (counter - 1) % 2 !== 0 ? () => 'Bzzz... Error!' : (b) => a + b;
-    }
-
     counter++;
 
-    return (b) => a + b;
+    return counter < 3
+      ? (b) => a + b : (counter - 1) % 2 !== 0
+        ? () => 'Bzzz... Error!' : (b) => a + b;
   }
 
   return getSum;
