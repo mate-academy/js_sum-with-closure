eba525310ed6d39105f86ca753c5c84763fb7fba
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-05-04 22:16:28 +0300

    robot-account

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,24 @@
 
 function makeRobotAccountant() {
   // write code here
+
+  let count = 0;
+
+  const sumOfAll = (first) => {
+    count++;
+
+    const evenCalls = (second) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return first + second;
+    };
+
+    return evenCalls;
+  };
+
+  return sumOfAll;
 }
 
 module.exports = makeRobotAccountant;
