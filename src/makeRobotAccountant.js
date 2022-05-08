3ee4739e9af9761325f3cc8ec0d37a7778808b48
Alexandra Protyanova <protyanova22@gmail.com>
2022-05-08 12:52:52 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(x) {
+    count++;
+
+    return function(y) {
+      if (count <= 3) {
+        return y + x;
+      } else if (count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return y + x;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
