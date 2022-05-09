5fdde3528068b860f0a2a62ad330dc88564dc450
Roman Pavlenko <proman9973@gmail.com>
2022-05-09 13:56:37 +0300

    solutionI

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,15 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return function getSum(x) {
+    count++;
+
+    return function secNum(y) {
+      return ((((count % 2) !== 0) || (count <= 3)) ? x + y : 'Bzzz... Error!');
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
