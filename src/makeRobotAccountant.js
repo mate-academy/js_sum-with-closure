b87cc5596786ee32c10d7ef910b92f4fd64fc6c6
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-05-10 19:53:25 +0300

    Robot accountant was done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function getSum(firstAdder) {
+    count++;
+
+    return function secondFn(secondAdder) {
+      if (count !== 4) {
+        return firstAdder + secondAdder;
+      } else if (count === 4) {
+        count = 0;
+
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
