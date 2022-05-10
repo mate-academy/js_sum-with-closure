1ded04d9cdb723bd400d00838254d85995edda51
koshachina <koshachina@yahoo.com>
2022-05-10 14:23:13 +0300

    solution Make robot account

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,22 @@
 
 function makeRobotAccountant() {
   // write code here
+  let callsNumber = 0;
+  const robocount = (x) => {
+    callsNumber++;
+
+    if ((callsNumber > 3) && (callsNumber % 2 === 0)) {
+      return function() {
+        return 'Bzzz... Error!';
+      };
+    } else {
+      return function(y) {
+        return x + y;
+      };
+    }
+  };
+
+  return robocount;
 }
 
 module.exports = makeRobotAccountant;
