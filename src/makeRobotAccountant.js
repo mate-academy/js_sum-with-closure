d49aa3241ad585c740d2c9fa817660e6bc242790
RomekIvantsiv <ivantsiv.romek@gmail.com>
2022-05-11 16:42:17 +0200

    I dont know how but it works

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getAdd = (num1) => {
+    count++;
+
+    return function adder(num2) {
+      if (count % 2 === 0 && count > 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return num1 + num2;
+    };
+  };
+
+  return getAdd;
 }
 
 module.exports = makeRobotAccountant;
