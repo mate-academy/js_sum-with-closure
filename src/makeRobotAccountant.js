4f9dfe5bc6a11199467d68a49856b3a4d4a99fa5
Valerii Derkach <valerij.derkach@gmail.com>
2022-05-11 15:58:01 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const adder = (a) => {
+    count++;
+
+    const secondAdder = (b) => {
+      return count <= 3
+        ? a + b
+        : count % 2 === 0
+          ? 'Bzzz... Error!'
+          : a + b;
+    };
+
+    return secondAdder;
+  };
+
+  return adder;
 }
 
 module.exports = makeRobotAccountant;
