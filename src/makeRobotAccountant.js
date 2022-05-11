5da254fc484659027011657dabaf17116ea33ff1
foxifio <foxifio00@gmail.com>
2022-05-11 18:46:04 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,25 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  const sum = (arg1) => {
+    count++;
+
+    return (arg2) => {
+      if (count <= 3) {
+        return arg1 + arg2;
+      }
+
+      if (count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return arg1 + arg2;
+    };
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
