beeb4a7d1adb7ed790972ac58a56fd880be0b540
Zubov Illia <contactduodiamonds@gmail.com>
2022-05-11 09:24:18 +0300

    solution - something hapening here - done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = -1;
+
+  return (firstArg) => {
+    counter++;
+
+    return (secArg) => {
+      if (counter < 3) {
+        return firstArg + secArg;
+      }
+
+      if (counter % 2 === 0) {
+        return firstArg + secArg;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
