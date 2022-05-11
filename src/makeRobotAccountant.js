b4db303b761781f6cef93c152eca8b8c137133e5
Valerii Derkach <valerij.derkach@gmail.com>
2022-05-11 17:09:29 +0300

    mistakes are fixed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,18 +8,12 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  const adder = (a) => {
+  const adder = a => {
     count++;
 
-    const secondAdder = (b) => {
-      return count <= 3
-        ? a + b
-        : count % 2 === 0
-          ? 'Bzzz... Error!'
-          : a + b;
-    };
-
-    return secondAdder;
+    return b => count <= 3 || count % 2 !== 0
+      ? a + b
+      : 'Bzzz... Error!';
   };
 
   return adder;
