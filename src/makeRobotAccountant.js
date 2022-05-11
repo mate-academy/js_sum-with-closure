3c94c5d52adcc1f018dbbec816d3390d17b1116d
Rashaad Ebrahim <rmebrahim@gmail.com>
2022-05-11 22:46:58 +0200

    Task complete

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (firstValue) => {
+    count++;
+
+    return (secondValue) => {
+      return count > 3 && count % 2 === 0
+        ? 'Bzzz... Error!'
+        : firstValue + secondValue;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
