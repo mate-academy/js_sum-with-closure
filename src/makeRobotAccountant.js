531877c732e5b9185951bc0742ba7be748e4681d
Lebogang Mogane <mogane420@gmail.com>
2022-05-12 17:15:05 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return firstCall => {
+    counter++;
+
+    return secondCall => {
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstCall + secondCall;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
