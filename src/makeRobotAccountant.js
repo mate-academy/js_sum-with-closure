e6ad7aa6e26c68d77ec77d8b7aa1d7b535040cf2
Andrei Sucman <sucmanandrei@gmail.com>
2022-05-12 12:25:46 +0300

    task completed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let counter = 0;
+
+  return (firstNum) => {
+    counter++;
+
+    return (secondNum) => {
+      const sum = firstNum + secondNum;
+
+      if (counter > 3) {
+        if (counter % 2 === 0) {
+          return 'Bzzz... Error!';
+        }
+      }
+
+      return sum;
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
