a203ad06714b36001b107f0df35f38ac4a16ba88
elkhayate <melkhayate61@gmail.com>
2022-05-12 03:18:15 +0100

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 4;
+
+  const inner = (part) => {
+    return (par) => {
+      count--;
+
+      if (count === 0) {
+        count = 3;
+
+        return 'Bzzz... Error!';
+      }
+
+      return par + part;
+    };
+  };
+
+  return inner;
 }
 
 module.exports = makeRobotAccountant;
