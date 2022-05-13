a6f220ba7885c27e52c4c08a12f1ced0f6a1703c
Vicky Miziukanova <enrikadej@gmail.com>
2022-05-13 16:44:07 +0300

    task is done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (firstNum) => {
+    count++;
+
+    const secondSum = (secondNum) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNum + secondNum;
+    };
+
+    return secondSum;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
