a4f95cc89c91dafe1289c843d2347c7a71eed8d7
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-13 15:35:20 +0200

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const getSum = (num) => {
+    count++;
+
+    const getSum2 = (n) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return num + n;
+    };
+
+    return getSum2;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
