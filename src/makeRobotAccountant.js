4dbf056fb29435db131e7a97d6b8c74fa639b636
Anastasiia Nikita <anastasiya.folk@gmail.com>
2022-05-13 12:04:45 +0300

    Solution-ready

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -12,7 +12,7 @@ function makeRobotAccountant() {
     return function(y) {
       counter++;
 
-      if (counter >= 4 && counter % 2 === 0) {
+      if (counter > 3 && counter % 2 === 0) {
         return `Bzzz... Error!`;
       }
 
