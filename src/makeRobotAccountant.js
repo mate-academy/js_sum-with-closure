902a29c9f7a879cd4f499e739fc025de6549eddf
Oleksandr Ivanchenko <aleksandrod92@gmil.com>
2022-05-13 16:47:05 +0300

    Robot Bzzz...

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,15 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function addCount(a) {
+    count++;
+
+    return function sumRes(b) {
+      return (count % 2 !== 0 || count <= 3) ? a + b : 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
