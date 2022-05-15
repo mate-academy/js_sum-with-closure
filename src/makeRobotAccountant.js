8323e17c23b16d186e183179b238bfec8143aa83
Volodymyr <lesksv2000@gmail.com>
2022-05-16 00:25:03 +0300

    makeRobotAccountant commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,22 @@
 
 function makeRobotAccountant() {
   // write code here
+  let counter = 0;
+  const getSum = (number) => {
+    counter++;
+
+    const newGetsum = (newNumber) => {
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return number + newNumber;
+    };
+
+    return newGetsum;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
