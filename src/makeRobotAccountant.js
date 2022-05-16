a8db56da80632db782653251cd1329c3a44c55b3
Swazi Kunene <kuneneswazi18@gmail.com>
2022-05-16 17:47:41 +0200

    simplified code

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,18 +6,15 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
   let count = 0;
 
   return (firstNumber) => {
     count++;
 
     return (secondNumber) => {
-      if (count > 3 && count % 2 === 0) {
-        return 'Bzzz... Error!';
-      }
-
-      return firstNumber + secondNumber;
+      return (count > 3 && count % 2 === 0)
+        ? 'Bzzz... Error!'
+        : firstNumber + secondNumber;
     };
   };
 }
