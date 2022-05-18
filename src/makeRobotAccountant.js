b6c2fb4a550687f94c69aba58f0fddf8a82887a4
Yaroslav Kononenko <konoyaroslav@gmail.com>
2022-05-18 07:22:49 +0300

    Some solve

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,15 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (firstArg) => (secondArg) => {
+    count++;
+
+    return (count > 3 && count % 2 === 0)
+      ? 'Bzzz... Error!'
+      : firstArg + secondArg;
+  };
 }
 
 module.exports = makeRobotAccountant;
