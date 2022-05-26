4e481014d8e33d0550c56168ac3e962189c0bf03
Oles Partyka <oles.partyka.27@gmail.com>
2022-05-26 08:08:46 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (value1) => {
+    counter++;
+
+    return (value2) => {
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return value1 + value2;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
