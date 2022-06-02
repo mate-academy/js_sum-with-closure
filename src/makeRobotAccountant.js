806f31e8fe9815772c5a0e45386818f831abb695
Olexandr Istomin <gn.53@ukr.net>
2022-06-03 00:24:00 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(numOne) {
+    count++;
+
+    return function(numTwo) {
+      switch (true) {
+        case count <= 3 || count % 2 !== 0:
+          return numOne + numTwo;
+        case count % 2 === 0:
+          return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
