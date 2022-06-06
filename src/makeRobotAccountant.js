bf75d56d269b4f8f1b2847f7d45bb920ff2b49b0
Aleksey Pushkalov <pushkalov86@gmail.com>
2022-06-06 22:51:36 +0300

    solution2

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -14,9 +14,9 @@ function makeRobotAccountant() {
     return (y) => {
       if (count > 3 && count % 2 === 0) {
         return 'Bzzz... Error!';
-      } else {
-        return x + y;
       }
+
+      return x + y;
     };
   };
 }
