1c85af37c4e3e04b057b698267c24d3ff2046937
Artem Furdela <furdela.artem@gmail.com>
2022-06-09 21:12:52 +0300

    Robot accounting task commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,21 @@
 
 function makeRobotAccountant() {
   // write code here
+  let attemps = 0;
+
+  const getSum = (x) => {
+    attemps++;
+
+    return function(y) {
+      if (attemps > 3 && attemps % 2 === 0) {
+        return `Bzzz... Error!`;
+      }
+
+      return x + y;
+    }
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
