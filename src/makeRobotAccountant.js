42b573bfb4b304a1cd471c0a051c3ef2be307e1e
Kostya Berehoviy <kostyaber3@gmail.com>
2022-06-09 21:55:06 +0300

    Closure Sum Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (a) => {
+    count++;
+
+    return function (b) {
+      const sum = a + b;
+  
+      if (count <= 3 || count % 2 !== 0) {
+        return sum;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
