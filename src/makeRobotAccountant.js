812a818f2d778c572f7fd9e47d2be08ba68e5e5c
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-06-11 11:53:04 +0300

    firstCommit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,29 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return function(a) {
+    const x = count++ + 1;
+
+    return function(b) {
+      if (x === 4) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
+
+// const getSum = makeRobotAccountant();
+
+// getSum(5)(9);
+// getSum(5)(6);
+// getSum(11)(53);
+
+// const actual = getSum(1)(13);
+
+// console.log(actual);
