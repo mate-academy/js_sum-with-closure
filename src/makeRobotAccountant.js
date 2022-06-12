79cc1b818b411784f9aca76115a31d214bc6b3bb
Ihor Kuchin <vedunigor@gmail.com>
2022-06-12 07:09:47 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsLimit = 3;
+
+  return function(a) {
+    return function(b) {
+      if (callsLimit <= 0 && callsLimit % 2 === 0) {
+        callsLimit--;
+
+        return 'Bzzz... Error!';
+      }
+
+      callsLimit--;
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
