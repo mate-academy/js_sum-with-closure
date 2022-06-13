0ac7f9299edc2fcfde11fba481290b4ccacb49eb
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-06-13 09:55:00 +0300

    fixedCommit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -10,10 +10,10 @@ function makeRobotAccountant() {
   let count = 0;
 
   return function(a) {
-    const x = count++ + 1;
-
     return function(b) {
-      if (x === 4) {
+      count++;
+
+      if (count % 2 === 0 && count > 3) {
         return 'Bzzz... Error!';
       }
 
@@ -23,13 +23,3 @@ function makeRobotAccountant() {
 }
 
 module.exports = makeRobotAccountant;
-
-// const getSum = makeRobotAccountant();
-
-// getSum(5)(9);
-// getSum(5)(6);
-// getSum(11)(53);
-
-// const actual = getSum(1)(13);
-
-// console.log(actual);
