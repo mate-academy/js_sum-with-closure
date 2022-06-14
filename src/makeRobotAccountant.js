e68ae95c82edb641894fa304ea059ee374d4d10f
Roman Marunkevich <vinnibea@gmail.com>
2022-06-14 21:30:54 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,23 @@
 
 function makeRobotAccountant() {
   // write code here
+  let sum = 0;
+  let count = 0;
+  const errorMessage = 'Bzzz... Error!';
+
+  return (x) => {
+    return (y) => {
+      count++;
+
+      if (count < 4 || count % 2 !== 0) {
+        sum = x + y;
+        
+        return sum;
+      }
+
+      return errorMessage;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
