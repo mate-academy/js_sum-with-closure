5a30bcc6908582b1e9df4f7b69330482cc71ab0b
Dmytro Kaunin <kaunin1198@te.net.ua>
2022-06-14 14:58:14 +0300

    Initial commit/ solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (number1) => {
+    counter++;
+
+    return (number2) => {
+      const evenCondition = (counter % 2 === 0);
+
+      if (counter > 3 && evenCondition) {
+        return 'Bzzz... Error!';
+      }
+
+      const summary = number1 + number2;
+
+      return summary;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
