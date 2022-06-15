02969a20c7d2639af23be113ffb38e488510ce2e
Tetiana Koval <tetiana.koval.bit@gmail.com>
2022-06-15 11:31:52 +0300

    solution1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function sumXY(x) {
+    return (y) => {
+      const no = count;
+
+      count++;
+
+      if (no < 3) {
+        return x + y;
+      } else if (no % 2 === 0) {
+        return x + y;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
+
+  return sumXY;
 }
 
 module.exports = makeRobotAccountant;
