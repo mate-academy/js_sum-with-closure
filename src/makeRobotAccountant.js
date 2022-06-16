4a7721ee4132d051c329dfc9693b88d6eeeb0700
viktorlyt <viktorlyt@gmail.com>
2022-06-16 21:33:04 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(x) {
+    return function(y) {
+      count++;
+
+      if (count > 3) {
+        if (count % 2 !== 0) {
+          return x + y;
+        } else {
+          return `Bzzz... Error!`;
+        }
+      } else {
+        return x + y;
+      }
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
