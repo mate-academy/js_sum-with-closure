80a5dc4c307211df50303b8e197accd91dc12950
Hovera-Liubomyr <ljusuk@gmail.com>
2022-06-18 17:24:04 +0300

    makeRobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,6 +6,22 @@
  */
 
 function makeRobotAccountant() {
+  let count = 0;
+  let sum = 0;
+
+  return function getFirst(arg1) {
+    return function getSecond(arg2) {
+      count++;
+
+      if ((count < 4) || (count % 2 !== 0)) {
+        sum = arg1 + arg2;
+
+        return sum;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
   // write code here
 }
 
