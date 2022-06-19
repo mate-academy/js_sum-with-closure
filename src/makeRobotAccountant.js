15bd3adbd2e9d7f6c78018806c25599044bb029f
Anton Shaga <toxa1381@gmail.com>
2022-06-19 18:29:17 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,6 +6,19 @@
  */
 
 function makeRobotAccountant() {
+  let calls = 1;
+
+  return (getSum1) => {
+    return (getSum2) => {
+      calls++;
+
+      if (calls % 2 !== 0 && calls > 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return getSum1 + getSum2;
+    };
+  };
   // write code here
 }
 
