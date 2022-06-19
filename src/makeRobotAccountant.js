df07908a8dec64d3e17607214614f80129827708
2022YEAR <tonnyedgy@gmail.com>
2022-06-19 22:08:49 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+  const limitOfCalls = 3;
+
+  const firstBatch = (firstAmount) => {
+    calls++;
+
+    const secondBatch = (secondAmount) => {
+      if (calls > limitOfCalls && calls % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return firstAmount + secondAmount;
+      }
+    };
+
+    return secondBatch;
+  };
+
+  return firstBatch;
 }
 
 module.exports = makeRobotAccountant;
