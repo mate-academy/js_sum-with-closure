10849e70f770704f64dc24f37b600ed2e8289266
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-19 17:56:59 +0300

    first commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,21 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  const getSum = (outerValue) => {
+    count++;
+
+    return function(innerValue) {
+      if (count >= 4 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return innerValue + outerValue;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
