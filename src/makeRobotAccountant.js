8626d01564a4a88d742a9e1d497d083eca9cde20
Julia Makarenko <julys.makarenko@gmail.com>
2022-06-20 22:43:41 +0300

    Add sum with closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const calls = (a) => {
+    const sum = (b) => {
+      count++;
+
+      if (count % 2 !== 0 || count <= 3) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+
+    return sum;
+  };
+
+  return calls;
 }
 
 module.exports = makeRobotAccountant;
