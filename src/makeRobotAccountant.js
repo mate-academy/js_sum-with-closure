a17f21ab44c61e29b04cd6ef0fa304e80ffed92b
Serhii Vorona <seregavorona94@gmail.com>
2022-06-23 08:31:24 +0300

    sumWithClosure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,20 @@
 
 function makeRobotAccountant() {
   // write code here
+
+  let count = 0;
+
+  return value1 => {
+    return value2 => {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return value1 + value2;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
