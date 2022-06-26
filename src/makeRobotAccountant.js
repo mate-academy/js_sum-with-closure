f1723f3593ec2db9116be7b38e6c5e181c452583
Hovera-Liubomyr <ljusuk@gmail.com>
2022-06-26 17:35:40 +0300

    makeRobotAccountant-1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,16 +7,13 @@
 
 function makeRobotAccountant() {
   let count = 0;
-  let sum = 0;
 
   return function getFirst(arg1) {
     return function getSecond(arg2) {
       count++;
 
-      if ((count < 4) || (count % 2 !== 0)) {
-        sum = arg1 + arg2;
-
-        return sum;
+      if (count < 4 || count % 2 !== 0) {
+        return arg1 + arg2;
       }
 
       return 'Bzzz... Error!';
