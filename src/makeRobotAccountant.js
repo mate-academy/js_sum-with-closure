543cbffcde68e9a65523a40b4d58a7dd307af304
Pavlo Pokryvets <pavelpokryvets@gmail.com>
2022-06-27 19:59:00 +0300

    solution 1.0

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,27 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 1;
+
+  return function(x) {
+    const copyX = x;
+
+    return function(y) {
+      if (count <= 3) {
+        count++;
+
+        return copyX + y;
+      } else if (count > 3 && count % 2 === 1) {
+        count++;
+
+        return copyX + y;
+      } else {
+        count++;
+
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
