82d1c6ed8d07781c5a69ab0b4a9a4fded4dcc9d9
silchyn <ivan.silchin@gmail.com>
2022-07-04 12:33:20 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsNumber = 0;
+
+  return (a) => {
+    return (b) => {
+      ++callsNumber;
+
+      if (callsNumber > 3 && !(callsNumber % 2)) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
