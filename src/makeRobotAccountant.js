48e81e851b0b4c11717843840e15118ab6928f78
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-07-06 13:47:46 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,15 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let calls = 0;
+
+  return (firstDigit) => {
+    calls++;
+
+    return calls > 3 && calls % 2 === 0
+      ? () => 'Bzzz... Error!'
+      : (secondDigit) => firstDigit + secondDigit;
+  };
+};
 
 module.exports = makeRobotAccountant;
