c7d0dd5e51a5ec8776accbfaaa54991a50fe75a4
Katerina Hradinarova <katerinahradinarova@gmail.com>
2022-07-12 14:54:41 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+
+  return function(a) {
+    return function(b) {
+      count++;
+
+      if (count > 3 && count % 2 === 1) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
