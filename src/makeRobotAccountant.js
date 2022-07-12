644637798a970d35718c7c7c6e1ecc9cc4a34a56
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-07-12 23:06:10 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return (valueX) => {
+    callCount++;
+
+    return (valueY) => {
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return valueX + valueY;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
