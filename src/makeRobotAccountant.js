74ad6fadb9516bd9d9ece0eb90d4290d832bb4f1
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-07-13 17:20:41 +0300

    added make robot accountant function

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,34 @@
 'use strict';
 
 /**
- *
+ * function(num1)(num2) === num1 + num2;
  * @return {function}
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let firstNumber;
+  let callNumber = 0;
+
+  const add = function(number = 0) {
+    if (firstNumber === undefined) {
+      firstNumber = number;
+
+      return add;
+    }
+
+    const sum = firstNumber + number;
+
+    firstNumber = undefined;
+    callNumber++;
+
+    if (callNumber > 3 && callNumber % 2 === 0) {
+      return 'Bzzz... Error!';
+    }
+
+    return sum;
+  };
+
+  return add;
 }
 
 module.exports = makeRobotAccountant;
