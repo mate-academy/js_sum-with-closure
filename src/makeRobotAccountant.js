f9dc45c4303a56d28a66ff83f5b89d146f3525ed
Leoned Pustovit <lrerednawl@gmail.com>
2022-07-13 17:16:01 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let errorCounter = 3;
+
+  const robot = (a) => {
+    return (b) => {
+      if (errorCounter === 0) {
+        errorCounter++;
+
+        return 'Bzzz... Error!';
+      }
+
+      errorCounter--;
+
+      return a + b;
+    };
+  };
+
+  return robot;
 }
 
 module.exports = makeRobotAccountant;
