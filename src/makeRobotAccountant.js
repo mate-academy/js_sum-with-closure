031a91f3fc825909189dfada4863a8a271c5f1b3
Nikita Biriukov <flimsy00@gmail.com>
2022-07-15 16:11:21 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,41 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(x) {
+    let sum = x;
+
+    count++;
+
+    return function(y) {
+      sum += y;
+
+      return count < 3 || count % 2 !== 0
+        ? sum
+        : 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
+
+// let count = 0;
+
+// return function(x) {
+//   let sum = x;
+
+//   count++;
+
+//   return function(y) {
+//     sum += y;
+
+//     if (count > 3 && count % 2 === 0) {
+//       return 'Bzzz... Error!';
+//     } else if (count > 3 && count % 2 !== 0) {
+//       return sum;
+//     }
+
+//     return sum;
+//   };
+// };
