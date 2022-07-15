b3e3c6b7565e56122084a821634f61461dd6d8e4
dasha <dashasv76@gmail.com>
2022-07-15 08:41:49 +0300

    Ternary operator was added

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -10,14 +10,11 @@ function makeRobotAccountant() {
 
   return function(firstNumber) {
     return function(secondNumber) {
-      if (count >= 3 && count % 2 !== 0) {
-        count++;
-
-        return 'Bzzz... Error!';
-      }
       count++;
 
-      return firstNumber + secondNumber;
+      return count >= 3 && count % 2 === 0
+        ? 'Bzzz... Error!'
+        : firstNumber + secondNumber;
     };
   };
 }
