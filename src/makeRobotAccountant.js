e8e982d3f363b437d1fdf07b29597cba215bc4c7
Yuliia Dronova <106536680+yuliia-dronova@users.noreply.github.com>
2022-07-16 18:32:11 +0300

    makeRobotAccountant func was created

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let callCount = 0;
+
+  return (arg1) => {
+    return (arg2) => {
+      callCount++;
+
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return arg1 + arg2;
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
