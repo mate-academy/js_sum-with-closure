191ee14137a3c900c350fa5f5e0c385d0adcad03
PavloMarkov <r9markovpn@gmail.com>
2022-07-17 12:23:12 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,30 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (param) => {
+    let result = 0;
+    let msg;
+
+    count++;
+
+    result += param;
+
+    const innerGetSum = (anotherNum) => {
+      result += anotherNum;
+
+      count === 2 || count % 2 !== 0
+        ? msg = result
+        : msg = `Bzzz... Error!`;
+
+      return msg;
+    };
+
+    return innerGetSum;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
