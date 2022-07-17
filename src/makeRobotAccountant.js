f516243b19ad3ab18a4f7673d5f906327d53e5dd
Anton Chetveryk <chetverykantonma@gmail.com>
2022-07-17 17:32:53 +0300

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (a) => {
+    counter++;
+
+    if (counter > 3 && counter % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return (b) => a + b;
+  };
 }
+
+// const getSum = makeRobotAccountant();
+
+// console.log(getSum);
 
 module.exports = makeRobotAccountant;
