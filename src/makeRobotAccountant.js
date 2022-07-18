88bfb13cc47041e5ea9a9e00602bcc5deeb969ee
Dmitriy Teteryn <dmitriy.teteryn@gmail.com>
2022-07-18 12:34:56 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let numberOfCals = 0;
+
+  return (firstValue) => {
+    return (secondValue) => {
+      numberOfCals++;
+
+      if ((numberOfCals > 3) && (numberOfCals % 2 === 0)) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstValue + secondValue;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
