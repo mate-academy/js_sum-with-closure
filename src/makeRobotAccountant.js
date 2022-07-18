eea94908f75f51d81243546e8f67352b18b5cf92
Zykhor Andrii <zuxoran@gmail.com>
2022-07-18 13:57:41 +0300

    task done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  function getSum(a) {
+    count++;
+
+    if (count > 3 && count % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return (b) => a + b;
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
