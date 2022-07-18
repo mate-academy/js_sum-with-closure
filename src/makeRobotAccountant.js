c5081d06c46940196489e8c887dc29859d3fc375
AndreyLeonov1 <65847113+AndreyLeonov1@users.noreply.github.com>
2022-07-18 12:53:21 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countOfCalls = 0;
+
+  return (firstNum) => {
+    return (secondNum) => {
+      countOfCalls++;
+
+      if ((countOfCalls % 2 === 0) && (countOfCalls > 3)) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNum + secondNum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
