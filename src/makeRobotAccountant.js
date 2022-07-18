989f57e0a708dc2d349000c31212c70da1844971
Vitaliy <vitmixgt@icloud.com>
2022-07-18 15:57:01 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countOfCall = 0;
+
+  return function(firstDigit) {
+    return function(secondDigit) {
+      countOfCall++;
+
+      return (countOfCall > 3 && countOfCall % 2 === 0)
+        ? 'Bzzz... Error!'
+        : firstDigit + secondDigit;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
