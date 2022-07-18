8f71267e7cc207e9425cb7969e3a77e3eb8626a3
Volodya <balyshev.volodya@gmail.com>
2022-07-18 11:34:36 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let iterateCount = 0;
+
+  return function getSum(y) {
+    return (x) => {
+      iterateCount++;
+
+      return iterateCount > 3 && iterateCount % 2 === 0
+        ? 'Bzzz... Error!'
+        : x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
