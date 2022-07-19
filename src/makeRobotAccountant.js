bba1c6fe147d39491605f9585ce64584cb2a9110
Ruslan Petrenko <ruslanpetrenko2203@gmail.com>
2022-07-19 12:37:49 +0300

    solution task

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(x) {
+    return function(y) {
+      count++;
+
+      const amount = (count % 2 === 0 && count > 3)
+        ? 'Bzzz... Error!'
+        : x + y;
+
+      return amount;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
