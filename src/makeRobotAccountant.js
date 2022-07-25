3ba0c0378aeeaee52e6a7d0d86736fd029f1bf26
Kamil Witek <kamilprogramuje@gmail.com>
2022-07-25 21:58:08 +0200

    added changes based on checklist.md

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -11,25 +11,16 @@ function makeRobotAccountant() {
   // 2.save second number from param into local array
   // 3. if length of this array equals 2 then
   let whichCall = 0;
-  let numbers = [];
 
-  function count(number) {
-    numbers = [number, ...numbers];
-
-    if (numbers.length === 2) {
+  return function(firstNumber) {
+    return function(secondNumber) {
       whichCall++;
 
-      const sum = numbers[0] + numbers[1];
-
-      numbers.length = 0;
+      const sum = firstNumber + secondNumber;
 
       return whichCall % 2 === 0 && whichCall > 2 ? 'Bzzz... Error!' : sum;
     };
-
-    return count;
   };
-
-  return count;
 }
 
 module.exports = makeRobotAccountant;
