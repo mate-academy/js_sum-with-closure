7a8f593986f8c0825581408854d22bb2a5af761f
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-08-01 21:46:56 +0300

    First commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countOfCalls = 0;
+
+  return function(a) {
+    let result = 0;
+
+    return function(b) {
+      countOfCalls++;
+      result += a + b;
+
+      if (countOfCalls % 2 === 0 && countOfCalls !== 2) {
+        return `Bzzz... Error!`;
+      }
+
+      return result;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
