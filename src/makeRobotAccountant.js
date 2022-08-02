da99a56f847b02ff658f26cb7afe415db3382619
Yevhen Veselov <veselovyevhen@gmail.com>
2022-08-02 12:56:24 +0300

    1st commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(x) {
+    count++;
+
+    return function(y) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
