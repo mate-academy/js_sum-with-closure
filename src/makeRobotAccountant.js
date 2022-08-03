be26fe9d9bf8e01941437435765ce6853f255d56
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-08-03 20:12:53 +0300

    Fianal commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,17 +9,14 @@ function makeRobotAccountant() {
   let countOfCalls = 0;
 
   return function(a) {
-    let result = 0;
-
     return function(b) {
       countOfCalls++;
-      result += a + b;
 
       if (countOfCalls % 2 === 0 && countOfCalls !== 2) {
         return `Bzzz... Error!`;
       }
 
-      return result;
+      return a + b;
     };
   };
 }
