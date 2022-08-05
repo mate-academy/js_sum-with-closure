2a0a91331d8eeda90a6d13817b0897bd635d8b93
Denys Topchyi <miliarder97@gmail.com>
2022-08-05 15:15:41 +0300

    makeRobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const first = (a) => {
+    const second = (b) => {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return `Bzzz... Error!`;
+      }
+
+      return a + b;
+    };
+
+    return second;
+  };
+
+  return first;
 }
 
 module.exports = makeRobotAccountant;
