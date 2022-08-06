9c8fc823c774e78ef33620914848a02a6fdcbb0c
Pelahiia Khalishkhova <pelahiia.kh@gmail.com>
2022-08-06 17:48:22 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  return function(x) {
+    calls++;
+
+    return function(y) {
+      if (calls > 3 && calls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
