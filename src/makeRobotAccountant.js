f094384c9bcf5b03f1d92e2e5591a0645946567a
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-08 14:31:21 +0300

    Task solution code

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let runsCount = 0;
+
+  return (x) => (y) => {
+    runsCount++;
+
+    if (runsCount > 3 && runsCount % 2 === 0) {
+      return 'Bzzz... Error!';
+    }
+
+    return x + y;
+  };
 }
 
 module.exports = makeRobotAccountant;
