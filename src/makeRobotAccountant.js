ef7ba5ebff87a99a916d8b2acd169553b310f084
Mark Shapka <mark.shapka1@gmail.com>
2022-08-08 19:29:30 +0300

    Task_sum-with-closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,22 @@
 
 function makeRobotAccountant() {
   // write code here
+  const errMessage = `Bzzz... Error!`;
+  let count = 0;
+
+  const sumFun = (a) => {
+    count++;
+
+    return function(b) {
+      if (count >= 4 && count % 2 === 0) {
+        return errMessage;
+      }
+
+      return a + b;
+    };
+  };
+
+  return sumFun;
 }
 
 module.exports = makeRobotAccountant;
