955c9ddda1d177c6e792d490d5e45fe42fb5c685
Andrej Gutnik <gutnikandrej0@gmail.com>
2022-08-11 10:10:19 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,16 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const maxCalls = 3;
+
+  return function getSum(x) {
+    count++;
+
+    return (y) => ((count % 2 === 0) && (count > maxCalls))
+      ? 'Bzzz... Error!'
+      : x + y;
+  };
 }
 
 module.exports = makeRobotAccountant;
