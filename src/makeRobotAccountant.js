2a19021734938c13bcf8111788dad0f66f462a5c
Dima Korol <korovaidima@gmail.com>
2022-08-13 15:51:00 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,32 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  let result;
+
+  const accounter = function(x) {
+    return function(y) {
+      counter++;
+      result = x + y;
+
+      if ((counter > 3) && (counter % 2 === 0)) {
+        result = 'Bzzz... Error!';
+      }
+
+      return result;
+    };
+  };
+
+  return accounter;
 }
+
+// const getSum = makeRobotAccountant();
+
+// getSum(5)(6);
+// getSum(5)(6);
+// getSum(5)(6);
+// getSum(5)(6);
+// getSum(5)(6);
+// getSum(5)(6);
 
 module.exports = makeRobotAccountant;
