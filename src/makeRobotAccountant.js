0e34d78dcd79e7938018f1e602742acf231da97e
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-08-18 00:45:07 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let counter = 0;
+
+  return first => second => {
+    counter++;
+
+    if (counter > 3 && counter % 2 === 0) {
+      return 'Bzzz... Error!';
+    }
+
+    return first + second;
+  };
+};
 
 module.exports = makeRobotAccountant;
