6188dd2513da50e575364e52b18745107da600b7
Oksana Melnyk <kseniadanyluk1994@gmail.com>
2022-08-19 17:39:19 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const firstRightCalls = 3;
+
+  return (x) => {
+    return (y) => {
+      count++;
+
+      if (count > firstRightCalls && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
