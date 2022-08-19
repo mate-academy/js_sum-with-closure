44e85f9ee5ceb70a59d424b3f808d2dbf2b740c8
Sheremet Oleksandra <sandasheremet@gmail.com>
2022-08-19 12:47:58 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
+  let count = 0;
   // write code here
-}
+  const add = (a) => {
+    count++;
+
+    return function(b) {
+      if (count >= 4 && count % 2 === 0) {
+        return `Bzzz... Error!`;
+      }
+
+      return a + b;
+    };
+  };
+
+  return add;
+};
 
 module.exports = makeRobotAccountant;
