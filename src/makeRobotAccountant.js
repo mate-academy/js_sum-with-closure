39bc8b8e73fceb48257a80532a9bf91f26645652
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-08-19 12:19:07 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,16 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  const maxCalls = 3;
+
+  return (x) => {
+    counter++;
+
+    return (counter > maxCalls && !(counter % 2)
+      ? () => 'Bzzz... Error!'
+      : (y) => x + y);
+  };
 }
 
 module.exports = makeRobotAccountant;
