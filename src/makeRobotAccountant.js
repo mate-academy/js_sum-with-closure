dd0faa202fa91656f39579d814547f9b93e83a7b
Shahmatov David <davidsahmatov4@gmail.com>
2022-08-20 09:11:54 +0300

    Version 0.1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,22 @@
 
 function makeRobotAccountant() {
   // write code here
+  let callsCount = 0;
+  const limit = 3;
+
+  const getSum = (x) => {
+    return (y) => {
+      callsCount++;
+
+      if (callsCount % 2 === 0 && limit < callsCount) {
+        return 'Bzzz... Error!';
+      };
+
+      return x + y;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
