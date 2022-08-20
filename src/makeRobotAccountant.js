e57fb44662de597805b6166bb3ead09148c7c6f4
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-08-20 15:00:42 +0300

    function getSum was created

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(firstAddend) {
+    return function(secondAddend) {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstAddend + secondAddend;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
