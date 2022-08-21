78ac90983e0320a601e8444a42c914589d141c02
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-08-21 10:40:16 +0300

    Func makeRobotAccountant implemented

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,18 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  const sum = (a) => {
+    counter++;
+
+    if (counter < 4 || counter % 2 === 1) {
+      return (b) => a + b;
+    }
+
+    return () => 'Bzzz... Error!';
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
