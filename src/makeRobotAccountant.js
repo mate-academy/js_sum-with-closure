d73f2276eeca0b4421a89248436b5c7b04f65d15
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-08-21 18:43:15 +0300

    makeRobotAccountable

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,15 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return function sum(n) {
+    count++;
+
+    return (n2) => (count < 4 || count % 2)
+      ? n + n2
+      : 'Bzzz... Error!';
+  };
 }
 
 module.exports = makeRobotAccountant;
