ce17312e7b3e3dad64ad330df02e2e26814e1bc8
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-08-21 18:37:38 +0300

    calculation of robot accounts was realized with closures

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let operationCount = 0;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      operationCount++;
+
+      if (operationCount < 4 || operationCount % 2 !== 0) {
+        return firstNumber + secondNumber;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
