62517d17788eb374f47ed6840477a329c4f8cfff
Anthony Buhaienko <dubsterok@gmail.com>
2022-08-21 12:09:23 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,21 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+function makeRobotAccountant(digit) {
+  let count = 0;
+  const firstThreeCalls = 3;
+
+  return (firstClosure) => {
+    count++;
+
+    return (secondClosure) => {
+      if (count > firstThreeCalls && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstClosure + secondClosure;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
