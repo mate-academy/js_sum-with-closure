9858971cdb5d306aad4bec9a6c19543701f1d42b
Anthony Buhaienko <dubsterok@gmail.com>
2022-08-22 08:53:11 +0300

    deleted useless variable and made ternary operator instead of IF block

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,17 +7,14 @@
 
 function makeRobotAccountant() {
   let count = 0;
-  const firstThreeCalls = 3;
 
   return (firstClosure) => {
     count++;
 
     return (secondClosure) => {
-      if (count > firstThreeCalls && count % 2 === 0) {
-        return 'Bzzz... Error!';
-      }
-
-      return firstClosure + secondClosure;
+      return count > 3 && count % 2 === 0
+        ? 'Bzzz... Error!'
+        : firstClosure + secondClosure;
     };
   };
 }
