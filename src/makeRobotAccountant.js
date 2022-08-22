2a62821cc2542488b20b9b0352dfad2b1532a21c
Yurii Pronin <ProninYurij@i.ua>
2022-08-22 12:37:37 +0300

    solutionRobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (number1) => (number2) => {
+    count++;
+
+    if (count <= 3) {
+      return number1 + number2;
+    }
+
+    if (count > 3) {
+      if (count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return number1 + number2;
+    }
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
