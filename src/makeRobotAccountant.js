0f419273ca6dcfeac00e65f30c5f14e239ef6b32
Boris Kerekesha <kerekesha90@ukr.net>
2022-08-22 07:58:54 +0300

    sum with clousure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function getSum(y) {
+    count++;
+
+    let sum = y;
+
+    return function(x) {
+      if (count <= 3 || count % 2 !== 0) {
+        sum += x;
+
+        return sum;
+      }
+
+      if (count > 3) {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
