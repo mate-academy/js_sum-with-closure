d1bcfa8db82c576c51a9d0a2095117045b8c25f6
Andrew Zaitsev <alzay007@gmail.com>
2022-08-22 14:29:25 +0300

    function makeRobotAccountant was created

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let startCount = 0;
+
+  const getSum = (num1) => {
+    return (arg2) => {
+      startCount++;
+
+      if (startCount > 3 && startCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return num1 + arg2;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
