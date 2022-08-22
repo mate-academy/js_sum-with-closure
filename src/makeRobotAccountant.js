87a15d8be308dd6cd8a7c449f3db6d16c100c2c8
Artem Kodesnikov <artiguard@gmail.com>
2022-08-22 17:19:30 +0300

    robot accountant solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const calls = 3;
+
+  const getSum = (x) => {
+    return (y) => {
+      count++;
+
+      if (count > calls && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
