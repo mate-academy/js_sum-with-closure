b007fd9436c37f3d00f2a319fc8a4b0fbd0aa3b2
Anastasiia Khmelovska <hmelanast1@gmail.com>
2022-08-22 12:00:07 +0300

    Solution1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -14,9 +14,9 @@ function makeRobotAccountant() {
     return function(secondNum) {
       if (count > 3 && count % 2 === 0) {
         return 'Bzzz... Error!';
-      } else {
-        return firstNum + secondNum;
-      };
+      }
+
+      return firstNum + secondNum;
     };
   }
 
