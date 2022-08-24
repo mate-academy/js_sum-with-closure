cb47156a84df734e8394b9ab8dd9e75b7e9a53f0
Andrii Dunets <davbiznesman@gmail.com>
2022-08-24 21:15:27 +0300

    makeRobotAccountant function done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => {
+    return (y) => {
+      count++;
+
+      if (count <= 3 || count % 2 === 1) {
+        return x + y;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
