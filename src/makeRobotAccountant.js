25e28f27f4d7a1353a83814a1087f75f9256c91f
HusakOleh <husakoleh91@gmail.com>
2022-08-25 14:11:38 +0300

    create function for addition

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const getSum = (num) => {
+    const x = (n) => {
+      count++;
+
+      if (count > 3 && Number.isInteger(count / 2)) {
+        return 'Bzzz... Error!';
+      }
+
+      return n + num;
+    };
+
+    return x;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
