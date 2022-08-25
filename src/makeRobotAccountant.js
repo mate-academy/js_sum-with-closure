211e425d240d3ddd02883806522e87978a03996f
Artem Kodesnikov <artiguard@gmail.com>
2022-08-25 12:29:36 +0300

    fixes after review

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,13 +7,12 @@
 
 function makeRobotAccountant() {
   let count = 0;
-  const calls = 3;
 
   const getSum = (x) => {
     return (y) => {
       count++;
 
-      if (count > calls && count % 2 === 0) {
+      if (count > 3 && count % 2 === 0) {
         return 'Bzzz... Error!';
       }
 
