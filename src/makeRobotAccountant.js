9583bb4a7672e37b2f931af8c86acd1af374c54c
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-08-25 10:17:02 +0300

    solution 1 fixed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,14 +9,12 @@ function makeRobotAccountant() {
   let limit = 0;
 
   return (firstNum) => {
-    const sum = firstNum;
-
     limit++;
 
     return ((limit > 3 && limit % 2 === 0))
       ? () => 'Bzzz... Error!'
       : (secondNum) => {
-        return sum + secondNum;
+        return firstNum + secondNum;
       };
   };
 }
