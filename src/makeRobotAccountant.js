4d6a279aa2b609391f032b11d8f7ce9b18161842
Ostap Blystsiv <ostapcr7@gmail.com>
2022-08-26 19:07:34 +0300

    added fixes

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,8 +8,8 @@
 function makeRobotAccountant(a) {
   let count = 0;
 
-  return function(b) {
-    return function(c) {
+  return (b) => {
+    return (c) => {
       count++;
 
       if (count % 2 === 0 && count > 3) {
