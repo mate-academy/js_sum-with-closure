ea9f80f35aa677eaf5c87401a1907c7d2c8147a6
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-08-30 22:47:08 +0300

    fixed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,19 +7,14 @@
 
 function makeRobotAccountant() {
   let counter = 0;
-  let sum = 0;
 
   return function(a) {
     return function(b) {
       counter += 1;
 
       if (counter <= 3 || counter % 2 > 0) {
-        sum = sum + a + b;
-
-        return sum;
+        return a + b;
       } else {
-        sum = 0;
-
         return 'Bzzz... Error!';
       }
     };
