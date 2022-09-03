51e7379de3e672275595e9707997641688282bc0
Oleksii <oleksii@it.com>
2022-09-03 14:23:37 +0300

    Accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const adding = (x) => {
+    return (y) => {
+      count++;
+
+      if (count >= 4 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
+
+  return adding;
 }
 
 module.exports = makeRobotAccountant;
