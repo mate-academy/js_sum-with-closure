786997c20847279692b6a7e1662fd3fdd9fd260e
EhorSaprykin <astralik1993@gmail.com>
2022-09-09 14:28:33 +0300

    Task done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,29 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+
+  const sum = (a) => {
+    return function(b) {
+      if (count <= 3) {
+        count++;
+
+        return a + b;
+      } else {
+        if (count % 2 === 0) {
+          count++;
+
+          return 'Bzzz... Error!';
+        } else {
+          count++;
+
+          return a + b;
+        };
+      };
+    };
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
