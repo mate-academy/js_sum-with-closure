a50b69e4127b522aaa5b47523bc4c5985977325d
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-09-15 14:09:53 +0300

    done!

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  return function(firstNum) {
+    calls++;
+
+    return function(secondNum) {
+      if (calls <= 3 || calls % 2 !== 0) {
+        return firstNum + secondNum;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
