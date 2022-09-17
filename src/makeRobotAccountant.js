4e06ea9b265e38669847d9831f79303fa56c1e70
grafit-off <graf.off.it@gmail.com>
2022-09-17 22:32:06 +0300

    feat: makeRobotAccountant function was implemented

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return (a) => {
+    count++;
+
+    return (b) => {
+      if (!(count % 2) && count > 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
