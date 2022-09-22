2462d15aa3188e06a59ff025bc9d3c224cbaf918
Myroslav Diiak <diiakm23819@gmail.com>
2022-09-22 20:42:04 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  const getSum = (firstValue) => {
+    calls++;
+
+    return (secondValue) => {
+      if (calls <= 3 || calls % 2 !== 0) {
+        return firstValue + secondValue;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
