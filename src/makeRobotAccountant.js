d6a2f4738a149355b181ac51ebda618a75f6e25e
fatihsamur <a.fatihsamur@gmail.com>
2022-09-22 21:29:06 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,26 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const errorMessage = 'Bzzz... Error!';
+  let count = 0;
+
+  const robot = (a) => {
+    count++;
+
+    return count < 4
+      ? (b) => {
+        return a + b;
+      }
+      : count % 2 === 0
+        ? (b) => {
+          return errorMessage;
+        }
+        : (b) => {
+          return a + b;
+        };
+  };
+
+  return robot;
 }
 
 module.exports = makeRobotAccountant;
