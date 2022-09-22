ecef673491e2219d4ed257fa8482df185e386486
Yura Dikanev <yuriydikanev@gmail.com>
2022-09-22 11:23:00 +0300

    sum_with_closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,17 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return (firstNumber) => {
+    count++;
+
+    return (secondNumber) => {
+      return (count > 3 && count % 2 === 0)
+        ? 'Bzzz... Error!'
+        : firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
