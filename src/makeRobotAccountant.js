a806d8c7e307d90b24898ab74217c68c780bc339
Vlad Malinin <vladmalinin1999@gmail.com>
2022-09-22 14:59:25 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let oddReturn = 0;
+
+  function sum(num1) {
+    oddReturn++;
+
+    return (num2) => {
+      if (oddReturn > 3 && oddReturn % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return num1 + num2;
+      }
+    };
+  }
+
+  return sum;
+};
 
 module.exports = makeRobotAccountant;
