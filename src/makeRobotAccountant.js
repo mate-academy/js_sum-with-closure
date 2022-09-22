ffe90dd407269c411e4479650f892058e3a57456
Tilak Kumarclear <mailtotilakkumar@gmail.com>
2022-09-23 01:42:51 +0530

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  *
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (num1) => {
+    return (num2) => {
+      count++;
+
+      return (count > 3 && count % 2 === 0)
+      ? 'Bzzz... Error!'
+      : (num1 + num2);
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
