b02c942271b519ddba1200baf248660abb80d745
Anna Borodaienko <anna.borodayenko@gmail.com>
2022-09-22 23:18:09 +0300

    RobotAccountant solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,18 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
 
+  return (firstNumber) => {
+    count++;
+
+    return (secondNumber) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
+}
 module.exports = makeRobotAccountant;
