854a3b4811b2a60890996320efeb83fbc774eae3
Vlad Malinin <vladmalinin1999@gmail.com>
2022-09-23 13:04:00 +0300

    change function

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,19 +8,17 @@
 function makeRobotAccountant() {
   let oddReturn = 0;
 
-  function sum(num1) {
+  return (num1) => {
     oddReturn++;
 
     return (num2) => {
       if (oddReturn > 3 && oddReturn % 2 === 0) {
         return 'Bzzz... Error!';
-      } else {
-        return num1 + num2;
       }
-    };
-  }
 
-  return sum;
+      return num1 + num2;
+    };
+  };
 };
 
 module.exports = makeRobotAccountant;
