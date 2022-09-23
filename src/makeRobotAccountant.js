7392fcf5d6a2853bd0bd223b9157efaf277fc208
korniienkokarina <k.tashlytska@gmail.com>
2022-09-23 19:04:26 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return x => y => {
+    callCount++;
+
+    const breakCondition = callCount > 3 && callCount % 2 === 0;
+    const sum = x + y;
+
+    const result = breakCondition
+      ? 'Bzzz... Error!'
+      : sum;
+
+    return result;
+  };
 }
 
 module.exports = makeRobotAccountant;
