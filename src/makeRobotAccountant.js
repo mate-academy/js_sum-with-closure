3af56ac26accc60142f30fc97b427ea7719fce18
Artem Musii <musiy.artem@gmail.com>
2022-09-24 00:50:51 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -4,9 +4,20 @@
  *
  * @return {function}
  */
-
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function getSum(numA) {
+    count++;
+
+    return function add(numB) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      };
+
+      return numA + numB;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
