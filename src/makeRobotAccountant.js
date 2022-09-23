7c273f4769492af97712b5fd0653c1bcc5e05192
Maksym845 <mzuravel845@gmail.com>
2022-09-23 13:40:11 +0300

    solution was added

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let invokedTimes = 0;
+
+  const add = (x) => {
+    invokedTimes++;
+
+    if (invokedTimes < 3 || invokedTimes % 2 !== 0) {
+      return (y) => {
+        return x + y;
+      };
+    }
+
+    return () => 'Bzzz... Error!';
+  };
+
+  return add;
 }
 
 module.exports = makeRobotAccountant;
