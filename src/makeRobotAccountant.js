a81193e542ef1e1d261163f1c58aa15669c0678f
Hlazman <glazman.b@gmail.com>
2022-09-24 12:34:32 +0300

    made function makeRobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const getSum = (numOne) => {
+    count++;
+
+    return (numTwo) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return numOne + numTwo;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
