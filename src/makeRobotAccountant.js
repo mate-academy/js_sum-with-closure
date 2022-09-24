f46438e7f56ad74ceb9b52b30c78daccccde90a5
Serhii Romaniuk <deepspark@gmail.com>
2022-09-24 17:20:14 +0300

    implemented makeRobotAccountant function

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  const add = (firstValue) => {
+    return (secondValue) => {
+      counter++;
+
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstValue + secondValue;
+    };
+  };
+
+  return add;
 }
 
 module.exports = makeRobotAccountant;
