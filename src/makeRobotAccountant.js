a6cc8536a954f7d05b27192554bfcf1dcbcf7e5d
poznianski <poznianski@gmail.com>
2022-09-25 15:58:16 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let processesCounter = 0;
+
+  return (firstNumber) => {
+    processesCounter++;
+
+    return (secondNumber) => {
+      if (processesCounter > 3 && processesCounter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
