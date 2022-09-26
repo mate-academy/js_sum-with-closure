c1f1a337fa0167ba55bba3015866f9dbe41574c5
George.kom <komolovua@gmail.com>
2022-09-26 17:00:26 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let getCallCounter = 0;
+
+  return fistValue => secondValue => {
+    getCallCounter++;
+
+    if (getCallCounter >= 3 && getCallCounter % 2 === 0) {
+      return 'Bzzz... Error!';
+    } else {
+      return fistValue + secondValue;
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
