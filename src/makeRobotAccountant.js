1c1ede146878407fde5ff954fd02b60b9e3be702
Arsen-Shpak <a.shpakovich2000@gmail.com>
2022-09-27 21:45:30 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,22 @@
 
 function makeRobotAccountant() {
   // write code here
+
+  let count = 0;
+
+  const add = (first) => {
+    count++;
+
+    if (count < 3 || count % 2 === 1) {
+      return (second) => {
+        return first + second;
+      };
+    }
+
+    return () => `Bzzz... Error!`;
+  };
+
+  return add;
 }
 
 module.exports = makeRobotAccountant;
