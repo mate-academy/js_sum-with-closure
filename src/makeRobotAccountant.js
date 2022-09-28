7ca0c4b2df84545cefe23dc59472917b31d7855c
Bibicutu <pauleugen@gmail.com>
2022-09-28 19:56:56 +0300

    First commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let operationCounter = 2;
+
+  const getsum = (firstNumber) => {
+    return (secondNumber) => {
+      if (operationCounter === -1) {
+        operationCounter++;
+
+        return 'Bzzz... Error!';
+      }
+
+      operationCounter--;
+
+      return firstNumber + secondNumber;
+    };
+  };
+
+  return getsum;
 }
 
 module.exports = makeRobotAccountant;
