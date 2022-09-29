64ca33f3761f9c85cb58c7203d62303bc4a90ba2
Danil <dasha8854567@gmail.com>
2022-09-29 19:15:46 +0300

    commit1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,24 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+ function makeRobotAccountant() {
+  let counter = 0;
+
+  function fn(x) {
+    counter++;
+
+    function fn2(y) {
+      if (counter % 2 || counter <= 3) {
+        return x + y;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    }
+
+    return fn2;
+  };
+
+  return fn;
 }
 
 module.exports = makeRobotAccountant;
