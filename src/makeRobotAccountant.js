03467647ee113eff2bef1c9e1438b28a383cf854
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2022-10-01 08:15:51 +0300

    Solution 2

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,16 +8,16 @@
 function makeRobotAccountant() {
   let counter = 0;
 
-  return (value1) => {
-    return (value2) => {
+  return (firstValue) => {
+    return (secondValue) => {
       if (counter === 3) {
         counter--;
 
         return 'Bzzz... Error!';
       }
-      counter = counter + 1;
+      counter++;
 
-      return value1 + value2;
+      return firstValue + secondValue;
     };
   };
 }
