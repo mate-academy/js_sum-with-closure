76519fa4f00134493de24c230989a3203dfd3c64
Sapna Choudhary <sapnachoudhary97491@gmail.com>
2022-10-02 21:11:26 +0200

    Solution for Robot Accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,23 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  const getSum1 = function(num1) {
+    count += 1;
+
+    const getSum2 = function(num2) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return num1 + num2;
+    };
+
+    return getSum2;
+  };
+
+  return getSum1;
 }
 
 module.exports = makeRobotAccountant;
