884dfe1eb06735046b9b705622545b15dcdec69e
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2022-10-02 16:23:41 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  const result = (num1) => {
+    const adding = (num2) => {
+      const sum = num1 + num2;
+
+      counter++;
+
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return sum;
+    };
+
+    return adding;
+  };
+
+  return result;
 }
 
 module.exports = makeRobotAccountant;
