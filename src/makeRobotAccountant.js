3fbd383bdcc3e5fb032ec939194d53a98f1dab34
Nhlanhla Thubana <nm.thubana@gmail.com>
2022-10-03 19:10:40 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,28 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let sum = 0;
+  let callTimes = 0;
+
+  const adder = (firstnNumber) => {
+    return (secondNumber) => {
+      callTimes++;
+
+      sum = firstnNumber + secondNumber;
+
+      const result = sum;
+
+      sum = 0;
+
+      if (callTimes > 3) {
+        return callTimes % 2 ? result : 'Bzzz... Error!';
+      }
+
+      return result;
+    };
+  };
+
+  return adder;
 }
 
 module.exports = makeRobotAccountant;
