377f814322dfff4c9c0101d6309ece2f4f92faaa
Mykyta Krainik <nikinta2003@gmail.com>
2022-10-04 13:17:03 +0300

    makeRobotAccountant was implemented

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let amountOfCounts = 0;
+
+  const getSum = numberA => {
+    amountOfCounts++;
+
+    return numberB => {
+      if (amountOfCounts > 3 && amountOfCounts % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return numberA + numberB;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
