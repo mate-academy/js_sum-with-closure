6876b55155a47fb55f83ab2e2934626a8c6d5a60
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2022-10-04 15:31:40 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let call = 0;
+
+  return (x) => {
+    return (y) => {
+      call++;
+
+      if (call > 3 && call % 2 === 0) {
+        return 'Bzzz... Error!';
+      };
+
+      if (call > 3 && call % 2 === 1) {
+        return x + y;
+      }
+
+      return x + y;
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
