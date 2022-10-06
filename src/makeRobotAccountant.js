d7af20ad92bda9560b158b43205df609c4f9d5a9
Viktoria Krasnyuk <krasnyuk_viktoria>
2022-10-06 17:59:54 +0300

    first commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const robot = function(firstValue) {
+    count++;
+
+    const sum = function(secondValue) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstValue + secondValue;
+    };
+
+    return sum;
+  };
+
+  return robot;
 }
 
 module.exports = makeRobotAccountant;
