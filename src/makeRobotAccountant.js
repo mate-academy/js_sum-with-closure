df2a9007c2ebd87f86e1498551d839028bb5c22c
Yurii Teslia <teslya.yuriy@gmail.com>
2022-10-09 13:01:24 +0300

    corrected

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -12,11 +12,12 @@ function makeRobotAccountant() {
   return function getSum(numberOne) {
     return function(numberTwo) {
       count++;
+
       if (count > 3 && count % 2 === 0) {
         return 'Bzzz... Error!';
-      } else {
-        return numberOne + numberTwo;
       }
+
+      return numberOne + numberTwo;
     };
   };
 }
