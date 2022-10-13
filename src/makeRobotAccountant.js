7515c1ae302ab797c0d937367291ad21d2e79fa9
Elena Ver <hellena84@i.ua>
2022-10-13 11:00:14 +0200

    mySolution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,23 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  const getSum = (x) => {
+    count++;
+
+    const oddEven = (y) => {
+      if (count >= 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return x + y;
+      };
+    };
+
+    return oddEven;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
