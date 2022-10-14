80f19a4dc8b550339d28eaf45ebdb88f6f1b9f45
Anastasia Babko <hr.anastasia.t@gmail.com>
2022-10-14 15:57:46 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const firstStatus = (x) => {
+    count++;
+
+    const secondStatus = (y) => {
+      if (count >= 4 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      const sum = x + y;
+
+      return sum;
+    };
+
+    return secondStatus;
+  };
+
+  return firstStatus;
 }
 
 module.exports = makeRobotAccountant;
