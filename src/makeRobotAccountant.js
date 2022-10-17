51af34c9789b9bf27fff0e423bf0c2d2cb348ddb
Mykola Yakymiv <mykola8426@gmail.com>
2022-10-17 23:13:07 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countOfCalls = 0;
+
+  return (firstNumber) => {
+    countOfCalls++;
+
+    return (secondNUmber) => {
+      if (countOfCalls > 3 && countOfCalls % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return firstNumber + secondNUmber;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
