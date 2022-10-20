b8687374a2c7c9fa9126d73a6aded156473ad01f
Oleksandr Lytvynenko <olek.lytvynenko@gmail.com>
2022-10-20 20:10:29 +0300

    solve

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,36 @@
  * @return {function}
  */
 
+// function makeRobotAccountant(firstValue) {
+//   let count = 0;
+
+//   return (secondValue) => {
+//     if (count < 4) {
+//       count++;
+
+//       const sum = firstValue + secondValue;
+
+//       return sum;
+//     }
+
+//     return 'Bzzz... Error!';
+//   };
+// }
+
 function makeRobotAccountant() {
-  // write code here
+  let amount = 0;
+
+  return (a) => {
+    return (b) => {
+      amount++;
+
+      if ((amount % 2) === 0 && amount > 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
