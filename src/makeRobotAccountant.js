42f3f5b0a1ea2526aa3407d62fc4860d6a864bba
Ruslan Miroshnychenko <rusikm87@gmail.com>
2022-10-22 13:34:13 +0300

    Done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (x) => {
+    count++;
+
+    const sum = (y) => x + y;
+
+    return (count <= 3 || count % 2) ? sum : () => 'Bzzz... Error!';
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
