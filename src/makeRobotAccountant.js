c9d49081fb35c3beb62d58a283ed3fea1b58bcad
Kalmykov Mikhail <kalmykov.mikhail2@gmail.com>
2022-10-25 17:25:35 +0300

    task was completed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,23 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+function makeRobotAccountant(x, y) {
+  let count = 0;
+
+  function getSum(a) {
+    return function(b) {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return `Bzzz... Error!`;
+      } else {
+        return a + b;
+      }
+    };
+  };
+
+  return getSum;
 }
 
+// console.log(makeRobotAccountant(5)(2))
 module.exports = makeRobotAccountant;
