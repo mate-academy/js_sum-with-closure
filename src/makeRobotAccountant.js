40a193895034497605e3f5c7e431d0ded4d7ae1b
bokorzita <104830913+bokorzita@users.noreply.github.com>
2022-10-25 13:30:40 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let numcall = 0;
+
+  return (a) => {
+    return (b) => {
+      numcall += 1;
+
+      if (numcall < 4) {
+        return a + b;
+      }
+
+      if (numcall % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
