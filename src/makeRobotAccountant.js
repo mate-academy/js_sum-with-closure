336f9c73d7712e89df1b3c2954a03cfc7caab45c
Illia Skladnik <scladnik@gmail.com>
2022-10-30 20:53:33 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(a) {
+    return b => {
+      count++;
+
+      if (count % 2 === 0 && count !== 2) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
