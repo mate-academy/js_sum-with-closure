c8cafa1ed2787f8eae5a8fff620468cb095e7d28
Taras Pustovyi <taraselnik@gmail.com>
2022-11-02 00:32:13 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countCalls = 0;
+  const getSum = (a) => {
+    const isEven = countCalls % 2 === 1;
+
+    countCalls++;
+
+    return (b) => {
+      if (countCalls > 3 && isEven) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
