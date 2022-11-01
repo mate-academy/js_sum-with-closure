33822aa8e14b4fbdc9f1818b440bd81563a77e4e
Masha <teenderrr@gmail.com>
2022-11-01 09:24:53 +0200

    robot accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,30 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(a) {
+    return function(b) {
+      count++;
+
+      return count < 3 || count % 2 !== 0
+        ? (a + b)
+        : 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
+
+  /*
+      if (count < 3) {
+        return (a + b);
+      }
+
+      if (count % 2 !== 0) {
+        return (a + b);
+      };
+
+      if (count % 2 === 0) {
+        return 'Bzzz... Error!';
+      };*/
