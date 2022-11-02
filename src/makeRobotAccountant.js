ca5b0eb85047db00844325b97047cefa0007f75c
Artem66 <melodpi@gmail.com>
2022-11-02 10:06:44 +0000

    impleneted makeRobotAccaountant function

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -4,9 +4,22 @@
  *
  * @return {function}
  */
-
 function makeRobotAccountant() {
+  let count = 1;
   // write code here
+
+  return function(x) {
+    return function(y) {
+      if (count % 2 === 0 && count !== 0 && count > 3) {
+        count++;
+
+        return 'Bzzz... Error!';
+      }
+      count++;
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
