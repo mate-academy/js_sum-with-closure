219f5e0d39f7600fbf5af49137493b49ff12a4a6
mitattt <garkavivii@gmail.com>
2022-11-02 13:39:07 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,24 @@
 'use strict';
-
+// # Sum with a closure
 /**
  *
  * @return {function}
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(num1) {
+    return function(num2) {
+      count++;
+
+      return count > 3 && count % 2 === 0
+        ? 'Bzzz... Error!'
+        : num1 + num2;
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
