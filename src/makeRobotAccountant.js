97e2aa06484d04eed1c27f720ceb54bfdd8b24cd
Svitlana Yudina <osbka31@gmail.com>
2022-11-02 16:02:32 +0200

    fixes by checklist

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,15 +7,29 @@
 
 function makeRobotAccountant() {
   let callsCount = 0;
+
+  return (a) => (b) => {
+    callsCount++;
+
+    return (callsCount > 3 && (callsCount % 2 === 0))
+      ? 'Bzzz... Error!'
+      : a + b;
+  };
+}
+
+/* Maybe it is better to use this variant?
+It is more readable and understandable, as for me.
+Or it is bad?
+
+function makeRobotAccountant() {
+  let callsCount = 0;
   const getSum = (a) => {
     const sum = (b) => {
       callsCount++;
 
-      if (callsCount > 3 && (callsCount % 2 === 0)) {
-        return 'Bzzz... Error!';
-      }
-
-      return a + b;
+      return (callsCount > 3 && (callsCount % 2 === 0))
+        ? 'Bzzz... Error!'
+        : a + b;
     };
 
     return sum;
@@ -23,5 +37,6 @@ function makeRobotAccountant() {
 
   return getSum;
 }
+ */
 
 module.exports = makeRobotAccountant;
