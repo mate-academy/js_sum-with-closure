3b67d8be4fdc91c4c2868e2ba764a649b8e47c4f
Yurii-Pozho <46326784+Yurii-Pozho@users.noreply.github.com>
2022-11-02 17:29:00 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 1;
+
+  return function(a) {
+    return function(b) {
+      if (count % 2 === 0 && count !== 0 && count >= 4) {
+        count++;
+
+        return `Bzzz... Error!`;
+      }
+      count++;
+
+      return a + b;
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
