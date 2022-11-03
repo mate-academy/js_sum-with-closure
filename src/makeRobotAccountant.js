86132abb4e1db2940ee4904a0fe516fe9e92aca7
Alex Tsimba <alexander.tsimba@gmail.com>
2022-11-03 13:41:02 +0200

    add solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  const getSum = (first) => {
+    calls++;
+
+    const result = (second) => calls <= 3 || calls % 2
+      ? first + second
+      : 'Bzzz... Error!';
+
+    return result;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
