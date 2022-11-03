0feda07973a15fa1ce8244848f3257fe9780f592
AndreyChernysh <achernysh744@gmail.com>
2022-11-03 08:07:34 +0200

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  function getSum(x) {
+    return function(y) {
+      counter++;
+
+      return counter < 3 || counter % 2 !== 0
+        ? (x + y)
+        : 'Bzzz... Error!';
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
