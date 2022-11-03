26b7ebb3afaaa3eef9226081ca1df6ee7165c302
Roman Maksymuk <romamaksymuk@gmail.com>
2022-11-03 08:07:02 +0200

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,24 @@
 
 function makeRobotAccountant() {
   // write code here
+
+  let counterCalls = 0;
+
+  return a => {
+    return b => {
+      counterCalls++;
+
+      if (counterCalls <= 3) {
+        return a + b;
+      }
+
+      if (counterCalls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
