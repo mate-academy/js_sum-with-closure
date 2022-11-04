f2c84a35bfcd3c95cecc86882ea0a274862ee9d6
Taras Vorobets <vorobetst777@gmail.com>
2022-11-04 22:41:39 +0200

    Sol

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let sum = 0;
+
+  return (x) => {
+    return (y) => {
+      sum++;
+
+      if (sum >= 4 && sum % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
