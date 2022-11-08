a958891f5f7508ff29a1e84b23cfccd36cfc841c
Anatolii Voloshyn <a.voloshin.p@gmail.com>
2022-11-08 14:01:41 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countOfAccount = 0;
+
+  return (first) => {
+    return (second) => {
+      countOfAccount++;
+
+      if (countOfAccount > 3 && countOfAccount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return first + second;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
