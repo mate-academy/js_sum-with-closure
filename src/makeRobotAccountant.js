53cca07e397da3d51ffd7e699f66e3f5a664aa9a
Eugene Farafonov <eu.farafonov@gmail.com>
2022-11-14 14:00:56 +0100

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (num1) => {
+    return (num2) => {
+      count++;
+
+      if (count <= 3) {
+        return num1 + num2;
+      } else {
+        if (count % 2 === 0) {
+          return 'Bzzz... Error!';
+        } else {
+          return num1 + num2;
+        }
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
