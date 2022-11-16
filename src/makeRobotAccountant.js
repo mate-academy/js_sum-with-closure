8de1ca1fc2c16de051f5a3a0d34d7b986d246682
Sergey Kovyazin <mr.roland1989@gmail.com>
2022-11-16 20:50:21 +0200

    Robot Accountant solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const getSum = function(number1) {
+    return function(number2) {
+      callCount++;
+
+      if (callCount <= 3 || callCount % 2 !== 0) {
+        return number1 + number2;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
