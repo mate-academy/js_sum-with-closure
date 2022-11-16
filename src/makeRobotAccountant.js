24fc7a6b34e32973f1779c0163d4efd58760e027
Vadym Kimpaiev <kivabi.uk@gmail.com>
2022-11-16 21:25:10 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (num1 = 0) => {
+    count++;
+
+    return (num2 = 0) => {
+      if (count <= 3 || count % 2 !== 0) {
+        return num1 + num2;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
