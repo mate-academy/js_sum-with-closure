d7343ecf56b69725b0e6ebe41d0712eb18456ca4
zbyn3k <coding707@mailbox.org>
2022-11-18 15:16:41 +0100

    Implemented the solution of makeRobotAccountant (sum with a closure)

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const addTwoNumbers = (x) => {
+    callCount++;
+
+    if (callCount > 3 && callCount % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return (y) => x + y;
+  };
+
+  return addTwoNumbers;
 }
 
 module.exports = makeRobotAccountant;
