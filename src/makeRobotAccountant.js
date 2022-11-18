696451ab05d1f6e25baf50cf29830eaa3d078b2e
Anastasiia Shyptia <shyptianastya27@gmail.com>
2022-11-18 22:19:29 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countCalculate = 0;
+
+  return (a) => {
+    return (b) => {
+      countCalculate++;
+
+      if (countCalculate < 4
+        || (countCalculate >= 4 && countCalculate % 2 !== 0)) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
