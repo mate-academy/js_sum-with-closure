2c3b5062f2d6c71fcb6e27884b26e208a9abae24
Oleksandr Filo <filo.sanya@gmail.com>
2022-11-24 16:23:45 +0200

    optimized solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,15 +9,13 @@ function makeRobotAccountant() {
   let callsCounter = 0;
 
   return function(firstNumber) {
-    callsCounter++;
-
-    if (callsCounter > 3 && callsCounter % 2 === 0) {
-      return function() {
-        return 'Bzzz... Error!';
-      };
-    }
-
     return function(secondNumber) {
+      callsCounter++;
+
+      if (callsCounter > 3 && callsCounter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
       return firstNumber + secondNumber;
     };
   };
