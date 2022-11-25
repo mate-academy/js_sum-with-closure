0caa71ebf387e69b1540deadd63f34fd7b8f228f
Dmitriy Sporysh <dopmoneyd@gmail.com>
2022-11-25 15:43:51 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const phrase = 'Bzzz... Error!';
+
+  return (a) => {
+    return (b) => {
+      count++;
+
+      return (
+        count > 3 && (count % 2 === 0)
+          ? phrase
+          : Number(a + b)
+      );
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
