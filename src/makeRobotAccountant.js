5ce85f59a3552a6880e17495ebfc50720294d8a7
Dmitriy Sporysh <dopmoneyd@gmail.com>
2022-11-26 22:49:19 +0200

    fix some things

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -14,9 +14,9 @@ function makeRobotAccountant() {
       count++;
 
       return (
-        count > 3 && (count % 2 === 0)
+        count > 3 && count % 2 === 0
           ? phrase
-          : Number(a + b)
+          : (a + b)
       );
     };
   };
