b34421218691f414c31871419dff8ef0c9965501
vadiimvooo <ASD4S.Profile@gmail.com>
2022-11-28 00:30:43 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let iterationCount = 0;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      iterationCount++;
+
+      if (iterationCount > 3 && !(iterationCount % 2)) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
