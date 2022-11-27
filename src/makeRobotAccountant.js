f2a8754ab5c5f4ec638553db5382f4fda84fb406
Artem Hirzhev <a.hirzhev@gmail.com>
2022-11-27 10:28:55 +0200

    Initial commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let launchCount = 0;
+
+  return function getSum(x) {
+    return function(y) {
+      launchCount++;
+
+      if (launchCount <= 3 || launchCount % 2 !== 0) {
+        return x + y;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
