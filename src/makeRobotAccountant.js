2c91a7787b92246f20b407d0b5798ff184c1bc2e
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2022-12-01 15:24:59 +0100

    fixed arrow function

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,7 +9,7 @@ function makeRobotAccountant() {
   let count = 0;
 
   return (number1 = 0) => {
-    return function(number2 = 0) {
+    return (number2 = 0) => {
       count++;
 
       if (count > 3 && count % 2 === 0) {
