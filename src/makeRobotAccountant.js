2add7da5b6768d3732adba286865ce641fe54b9f
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2022-12-01 21:30:59 +0200

    remove one return from b function

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,12 +8,10 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  return (a) => {
-    return (b) => {
-      count++;
+  return (a) => (b) => {
+    count++;
 
-      return (count > 3 && count % 2 === 0) ? `Bzzz... Error!` : a + b;
-    };
+    return (count > 3 && count % 2 === 0) ? `Bzzz... Error!` : a + b;
   };
 }
 
