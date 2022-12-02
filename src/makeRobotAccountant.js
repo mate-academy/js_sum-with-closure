6aae5e8a7efbbb406321f5d467c8c61258cff6ac
Lefteris Loizides <loizides.lef@gmail.com>
2022-12-02 17:59:56 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let callCount = 0;
+
+  return (a) => {
+    callCount++;
+
+    return (b) => {
+      return (callCount > 3 && callCount % 2 === 0)
+        ? 'Bzzz... Error!'
+        : a + b;
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
