14b3acefbd9a864bc3dd57682073d4f748e4415f
ysrckr <dyt.yasarcakir@gmail.com>
2022-12-02 18:30:19 +0300

    after review

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,8 +8,8 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  const getSum = (num1 = 0) => {
-    const sum = (num2 = 0) => {
+  return (num1 = 0) => {
+    return (num2 = 0) => {
       count++;
 
       if (count > 3 && count % 2 === 0) {
@@ -18,11 +18,7 @@ function makeRobotAccountant() {
 
       return (num1 + num2);
     };
-
-    return sum;
   };
-
-  return getSum;
 }
 
 module.exports = makeRobotAccountant;
