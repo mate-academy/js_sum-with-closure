bcd9a06877fa737058a009cd2dfe7ab2503c2d1a
Oleksandr Filo <filo.sanya@gmail.com>
2022-12-02 12:04:30 +0200

    functions were replaced with arrow functions

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,16 +8,14 @@
 function makeRobotAccountant() {
   let callsCounter = 0;
 
-  return function(firstNumber) {
-    return function(secondNumber) {
-      callsCounter++;
+  return (firstNumber) => (secondNumber) => {
+    callsCounter++;
 
-      if (callsCounter > 3 && callsCounter % 2 === 0) {
-        return 'Bzzz... Error!';
-      }
+    if (callsCounter > 3 && callsCounter % 2 === 0) {
+      return 'Bzzz... Error!';
+    }
 
-      return firstNumber + secondNumber;
-    };
+    return firstNumber + secondNumber;
   };
 }
 
