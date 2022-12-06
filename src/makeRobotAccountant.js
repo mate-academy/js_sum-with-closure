86bf1633fe0c21fc03da4d05f413cc7bb90813fb
Anton Martynes <martynes1997@gmail.com>
2022-12-06 23:12:35 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,15 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return (x) => {
+    count++;
+
+    return (y) => {
+      return (count <= 2 || count % 2) ? x + y : 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
