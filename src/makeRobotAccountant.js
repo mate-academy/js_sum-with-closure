2d5d41462c69cd46dfdc49ae55fcf5b3230ee29a
OLEXANDRA KOROL <sandrakorol3@gmail.com>
2022-12-07 23:23:35 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let counter = 0;
+
+  return (b) => (a) => {
+    counter++;
+
+    const checkCounting = (counter < 3)
+      ? a + b
+      : (counter % 2 !== 0)
+        ? a + b
+        : 'Bzzz... Error!';
+
+    return checkCounting;
+  }
+};
 
 module.exports = makeRobotAccountant;
