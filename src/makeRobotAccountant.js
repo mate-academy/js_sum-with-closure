3dd01bf37736b4fb41c38c3701fdb64e4853b2c2
Iorga Nicu <ioghigithublab@gmail.com>
2022-12-07 19:44:19 +0200

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,16 @@
 'use strict';
 
-/**
- *
- * @return {function}
- */
-
 function makeRobotAccountant() {
-  // write code here
+  // counter
+  let c = 0;
+
+  return a => b => {
+    c++;
+
+    return (c > 3 && c % 2 === 0)
+      ? 'Bzzz... Error!'
+      : a + b;
+  };
 }
 
 module.exports = makeRobotAccountant;
