fa848c7ab06e5b05346e6182329c127c3893c4f2
Levente Krotos <krotos.levi@gmail.com>
2022-12-07 20:08:59 +0100

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  function getSum(a) {
+    return function(b) {
+      counter++;
+
+      return (counter > 3 && counter % 2 === 0)
+        ? 'Bzzz... Error!'
+        : a + b;
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
