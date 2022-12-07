f3a56ff502bcb863a31a4eb2874d61771f6662f1
Anastasiia Roiek <anastasiiaroiek@gmail.com>
2022-12-07 22:05:31 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countCall = 0;
+
+  return (a) => {
+    return (b) => {
+      countCall++;
+
+      if (countCall < 4 || (countCall >= 4 && countCall % 2 !== 0)) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
