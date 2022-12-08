ec4e8dcd6a8e0656c92ff7ab19a97952f33b497b
Diana Demkiv <dianademkiv789@gmail.com>
2022-12-09 01:23:38 +0200

    robot accountant solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCounter = 0;
+
+  const addFirstNum = (firstNumber) => {
+    const addSecondNum = (secondNumber) => {
+      callsCounter++;
+
+      if (callsCounter > 3 && callsCounter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+
+    return addSecondNum;
+  };
+
+  return addFirstNum;
 }
 
 module.exports = makeRobotAccountant;
