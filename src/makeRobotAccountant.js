dac29f4d6d02a6fcb775d5b6f0b5f2106786fd3d
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2022-12-09 16:19:39 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let accountant = 0;
+
+  return (x) => {
+    accountant++;
+
+    return (y) => {
+      if (accountant <= 3 || accountant % 2 === 1) {
+        return x + y;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
