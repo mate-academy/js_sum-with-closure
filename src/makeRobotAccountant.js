621480ddbc5137d64ea7838c7f5116024aa9100a
Kenneth Delgado <kenvastolord@gmail.com>
2022-12-10 16:53:39 +0100

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
 
+  const result = (a) => {
+    count++;
+
+    const sum = (b) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+
+    return sum;
+  };
+
+  return result;
+}
 module.exports = makeRobotAccountant;
