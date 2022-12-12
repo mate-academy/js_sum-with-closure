5de98195c50f7d529b968cfe63ff1dd1bdb85b52
Romeo Kiss <kiss.romeo.richard@gmail.com>
2022-12-12 13:28:07 +0200

    Changed it into an arrow func

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,15 +8,14 @@
 function makeRobotAccountant() {
   // write code here
   let counter = 0;
-  const sum = (a) => (b) => {
+
+  return (a) => (b) => {
     counter++;
 
     return counter > 3 && counter % 2 === 0
       ? 'Bzzz... Error!'
       : a + b;
   };
-
-  return sum;
 }
 
 module.exports = makeRobotAccountant;
