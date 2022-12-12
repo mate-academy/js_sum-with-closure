39201427d1eceffcb53ff4e2d974181263d9de2d
Romeo Kiss <kiss.romeo.richard@gmail.com>
2022-12-12 13:25:48 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,16 @@
 
 function makeRobotAccountant() {
   // write code here
+  let counter = 0;
+  const sum = (a) => (b) => {
+    counter++;
+
+    return counter > 3 && counter % 2 === 0
+      ? 'Bzzz... Error!'
+      : a + b;
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
