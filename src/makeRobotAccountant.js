119b79a1d824cc35576baa64675d1b9152978831
Kavster92 <kav.athi1992@googlemail.com>
2022-12-12 12:03:29 +0100

    accountant replicating error

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => {
+    count++;
+
+    return (y) => {
+      if (count < 3 || count % 2 !== 0) {
+        return x + y;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
