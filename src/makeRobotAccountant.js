677f18afe760ea7877d663a27d5efc6423345e7e
Ala El Achkar <iamalaaashkar97@gmail.com>
2022-12-14 17:32:31 +0300

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const first = (a) => {
+    count++;
+
+    const second = (b) => {
+      if (count < 3 || count % 2 !== 0) {
+        return a + b;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+
+    return second;
+  };
+
+  return first;
 }
 
 module.exports = makeRobotAccountant;
