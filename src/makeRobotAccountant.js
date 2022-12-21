4397012f93875afb858db9c9276df33a573c7b80
haduigon <haduigon@gmail.com>
2022-12-21 23:06:24 +0200

    first coomit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return function(first) {
+    count++;
+
+    return function(second) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return first + second;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
