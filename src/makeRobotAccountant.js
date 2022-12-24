9e58f1d2f2139fc4903b1754bdfc682d3b99b2c0
Anna Koval <anny.koval@icloud.com>
2022-12-24 19:04:51 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (number1) => {
+    return (number2) => {
+      count++;
+
+      if (count < 4 || count % 2 === 1) {
+        return number1 + number2;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
