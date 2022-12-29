0b164b68c34088ecc7bc512de2ef21ed02826f74
Iryna Pysanko <malva37@gmail.com>
2022-12-29 11:35:51 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const errorMessage = 'Bzzz... Error!';
+  let countAttempt = 0;
+  const getSum = (a) => {
+    countAttempt++;
+
+    if (countAttempt > 3 && countAttempt % 2 === 0) {
+      return () => {
+        return errorMessage;
+      };
+    } else if (countAttempt > 3 && countAttempt % 2 !== 0) {
+      return (b) => {
+        return a + b;
+      };
+    } else {
+      return (b) => {
+        return a + b;
+      };
+    }
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
