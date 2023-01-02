72520dd7b7b26075198529ef474ae99b488e821b
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-01-02 10:24:00 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 1;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      const isEven = counter % 2 === 0;
+
+      if (counter <= 3 || !isEven) {
+        counter++;
+
+        return firstNumber + secondNumber;
+      }
+
+      counter++;
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
