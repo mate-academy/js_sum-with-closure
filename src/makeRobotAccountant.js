be93940b2b9660c8617404562090720701fbf5fd
Anastasiia-Khudych <anastasiiakhudych@gmail.com>
2023-01-04 21:29:17 +0200

    add solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (x) => {
+    return (y) => {
+      counter++;
+
+      return counter % 2 === 0 && counter !== 2
+        ? 'Bzzz... Error!'
+        : x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
