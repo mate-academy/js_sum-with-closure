3fded16aa9b560b6fae6ebc31451501eb6facd71
Vitalii Borzov <amirborzo99@gmail.com>
2023-01-06 17:38:02 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,23 @@
 
 function makeRobotAccountant() {
   // write code here
+  let numbersOfCalls = 0;
+
+  const sum = (a) => {
+    const result = (b) => {
+      numbersOfCalls++;
+
+      if (numbersOfCalls <= 3 || numbersOfCalls % 2 !== 0) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+
+    return result;
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
