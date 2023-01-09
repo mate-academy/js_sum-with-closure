90c9a3dfa8447dba27ca7d776a6f3929c9216b51
Brandon Sunrise <stepansky93@gmail.com>
2023-01-09 16:57:22 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return function(numb1) {
+    return function(numb2) {
+      counter++;
+
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return Number(numb1 + numb2);
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
