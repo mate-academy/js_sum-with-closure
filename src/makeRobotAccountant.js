0be731e24ce490ee86e8e4e72c5d9786e8ad6af6
bart6man <igor.musson.55@gmail.com>
2023-01-09 15:21:07 +0200

    task was solved

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (numberOne) => {
+    const result = (numbertwo) => {
+      const sum = numberOne + numbertwo;
+
+      if (counter > 3) {
+        return counter % 2 === 0
+          ? 'Bzzz... Error!'
+          : sum;
+      }
+
+      return sum;
+    };
+
+    counter++;
+
+    return result;
+  };
 }
 
 module.exports = makeRobotAccountant;
