a7a98ad48d51879924ae8eff4874b1a8c63d9bf9
Sviatoslav <sv_dubov@ukr.net>
2023-01-09 18:17:14 +0200

    Remake with ternary

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -12,11 +12,9 @@ function makeRobotAccountant() {
     return (secondNumber) => {
       count++;
 
-      if (count > 3 && count % 2 === 0) {
-        return 'Bzzz... Error!';
-      }
-
-      return firstNumber + secondNumber;
+      return count > 3 && count % 2 === 0
+        ? 'Bzzz... Error!'
+        : firstNumber + secondNumber;
     };
   };
 }
