56af276daab9db89e3a56d020d601751e9f08d3a
Dashaverz <dashaverzhanskayaa@gmail.com>
2023-01-10 23:27:59 +0100

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,17 @@
 
 function makeRobotAccountant() {
   // write code here
-}
+  let callCount = 0;
+
+  return (firstNum) => {
+    return (secondNum) => {
+      callCount++;
+
+      return callCount < 4 || callCount & 1
+        ? firstNum + secondNum
+        : 'Bzzz... Error!';
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
