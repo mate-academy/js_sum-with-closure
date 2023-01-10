7d466fddcfea9a1737bd106c8a291f6310528f2a
Your Name <you@example.com>
2023-01-10 15:44:11 +0200

    develop

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  return (y) => {
+    return x => {
+      calls++;
+
+      if (calls > 3 && calls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return y + x;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
