ee6a61b23becb16f415b5775abe535309b0c1ca2
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2023-01-10 21:02:06 +0200

    comleted

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,28 @@
 'use strict';
 
-/**
+/*
+ *
  *
  * @return {function}
  */
 
 function makeRobotAccountant() {
   // write code here
+  let counter = 0;
+
+  return (firstNum) => {
+    return (secondNum) => {
+      counter++;
+
+      if (counter > 3) {
+        return counter % 2 === 0
+          ? 'Bzzz... Error!'
+          : firstNum + secondNum;
+      }
+
+      return firstNum + secondNum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
