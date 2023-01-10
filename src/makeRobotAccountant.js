f801255fa9e19d629c654ebc212db78d0e9aaf66
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-01-10 18:02:29 +0200

    task done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const add = (number) => {
+    if (callCount >= 3) {
+      callCount++;
+
+      if (callCount % 2 === 0) {
+        return () => 'Bzzz... Error!';
+      }
+
+      return nextNumber => number + nextNumber;
+    }
+    callCount++;
+
+    return nextNumber => number + nextNumber;
+  };
+
+  return add;
 }
 
 module.exports = makeRobotAccountant;
