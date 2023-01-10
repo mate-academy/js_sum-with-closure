0b0de1eb76269bee5cc445cbdcbd42c741f9355c
svitjojo <kornotavr@gmail.com>
2023-01-10 15:41:34 +0200

    Result

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let workCounter = 0;
+
+  return (x) => {
+    workCounter++;
+
+    return (y) => {
+      if (workCounter % 2 === 0 && workCounter > 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
