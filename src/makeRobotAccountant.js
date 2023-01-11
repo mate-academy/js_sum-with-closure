f5780b43e645ef7541796e688af2b7b60e0529ed
Olya Zaiets <zayets.olga@gmail.com>
2023-01-11 21:31:36 +0100

    task sum with closure done 2

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,27 +8,17 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  const numbers = (num1) => {
-    const number = (num2) => {
+  return (num1) => {
+    return (num2) => {
       count++;
-
-      if (count <= 3) {
-        return num1 + num2;
-      }
 
       if (count > 3 && count % 2 === 0) {
         return 'Bzzz... Error!';
       }
 
-      if (count > 3 || count % 2 !== 0) {
-        return num1 + num2;
-      }
+      return num1 + num2;
     };
-
-    return number;
   };
-
-  return numbers;
 }
 
 module.exports = makeRobotAccountant;
