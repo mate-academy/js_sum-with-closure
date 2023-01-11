f85fa4daac76fb8422c10a2f956a4599d5c92727
victorionov <victorionovcvlrs@gmail.com>
2023-01-11 12:42:53 +0100

    made solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 3;
+
+  return (x) => {
+    return (y) => {
+      counter--;
+
+      if (counter > 0 || counter % 2 === 0) {
+        return x + y;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
