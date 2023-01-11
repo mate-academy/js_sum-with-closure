0dfe4b0da46276b53d8a460862fe4f0506a6effe
Oleksandr Burovetskyi <oleksandr.burovetskyi.knm.2020@lpnu.ua>
2023-01-11 17:19:17 +0200

    Added solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countCalls = 0;
+
+  return (first) => {
+    countCalls++;
+
+    return (second) => {
+      const sum = first + second;
+
+      return countCalls > 3 && countCalls % 2 === 0
+        ? 'Bzzz... Error!'
+        : sum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
