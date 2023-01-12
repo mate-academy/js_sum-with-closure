8f826e5b582dd498ce99dad0327da31248a41ad2
Andrew Borys <borysandrey1988@gmail.com>
2023-01-12 21:47:38 +0200

    solution_1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,25 @@
 
 function makeRobotAccountant() {
   // write code here
+  let leftCalls = 3;
+
+  const calc = (x) => {
+    const sum = (y) => {
+      if (leftCalls <= 0) {
+        leftCalls += 1;
+
+        return `Bzzz... Error!`;
+      }
+
+      leftCalls -= 1;
+
+      return x + y;
+    };
+
+    return sum;
+  };
+
+  return calc;
 }
 
 module.exports = makeRobotAccountant;
