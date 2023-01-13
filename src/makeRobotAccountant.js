48cf7841ea0b6d0d5affc12f3c401f8485d49222
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-13 07:25:30 +0200

    Finish task

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  *
@@ -7,6 +7,17 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return (a) => {
+    return (b) => {
+      count++;
+      if (count < 4) {
+        return a + b;
+      }
+      return count % 2 !== 0 ? a + b : "Bzzz... Error!";
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
