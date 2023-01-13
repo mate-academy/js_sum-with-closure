a3348746d0f3e6a20a706967ded4b737cd20bfe4
Vadivel-Babu <vadivelbabu1996@gmail.com>
2023-01-13 12:34:38 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,21 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return (b) => {
+    return (c) => {
+      if (count === 3) {
+        count = 0;
+
+        return 'Bzzz... Error!';
+      }
+
+      count++;
+
+      return b + c;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
