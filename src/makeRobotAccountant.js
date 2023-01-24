93cdb294dcdde8bd08e6afaac7102fa51c11df1e
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-01-24 17:06:48 +0200

    not perfect Robot-accountant done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countOfCalls = 0;
+
+  return a => {
+    return b => {
+      countOfCalls++;
+
+      if (countOfCalls % 2 === 0 && countOfCalls > 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
