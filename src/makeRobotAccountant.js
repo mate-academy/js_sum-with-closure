ba35e2f462e11fa9666188cee7827351f53ffcd7
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-01-29 20:01:43 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,13 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = -3;
+
+  return (num1) => (num2) => {
+    count++;
+
+    return count % 2 === 1 ? `Bzzz... Error!` : num1 + num2;
+  };
 }
 
 module.exports = makeRobotAccountant;
