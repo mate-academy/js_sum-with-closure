4640413d4fbdfef115b486d59215bcb69bee8976
Dmytro Hubych <dh.hubych@gmail.com>
2023-02-05 17:50:56 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let permit = 0;
+
+  const getSum = (a) => {
+    permit++;
+
+    if (permit <= 3) {
+      return (b) => a + b;
+    }
+
+    if (permit % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return (b) => a + b;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
