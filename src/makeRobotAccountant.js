f4e78eb8d3307a53c32223782cec2865adcf4278
Ilona Romaniuk <ilona09041983@gmail.com>
2023-02-08 20:10:51 +0200

    Function makeRobotAccountant: shot 1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const accountanting = [];
+
+  const getSum = (x) => {
+    return function(y) {
+      while (true) {
+        if (accountanting.length <= 2 || accountanting.length % 2 === 0) {
+          accountanting.push(x + y);
+          break;
+        } else {
+          accountanting.push(`Bzzz... Error!`);
+          break;
+        }
+      }
+
+      return accountanting[accountanting.length - 1];
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
