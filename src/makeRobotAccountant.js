2b757b15783079cf21a882a7c8247c273cd79ede
Vlad <bojko.vladislav16@gmail.com>
2023-02-09 12:18:06 +0200

    Fixes after code review

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,16 +9,12 @@ function makeRobotAccountant() {
   let counter = 0;
 
   return firstNumber => {
-    counter++;
-
-    if (counter >= 4 && counter % 2 === 0) {
-      return () => {
-        return 'Bzzz... Error!';
-      };
-    }
-
     return secondNumber => {
-      return firstNumber + secondNumber;
+      counter++;
+
+      return counter >= 4 && counter % 2 === 0
+        ? 'Bzzz... Error!'
+        : firstNumber + secondNumber;
     };
   };
 }
