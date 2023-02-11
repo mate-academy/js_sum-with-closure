0659044db9a6b6b79a20736870f9231475bc3bd6
ivanm2706 <ivanmorhun664@gmail.com>
2023-02-11 08:09:40 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  return function (num1) {
+    return function (num2) {
+      count++;
+      const sum = num1 + num2;
+
+      if (count <= 3) {
+        return sum;
+      }
+
+      return count % 2 !== 0
+        ? sum
+        : 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
