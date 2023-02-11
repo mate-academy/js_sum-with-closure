d2be13698b3ba06aebca291998329a1c29ee72ef
ivanm2706 <ivanmorhun664@gmail.com>
2023-02-11 12:56:30 +0200

    remove ternar operator

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,18 +7,17 @@
 
 function makeRobotAccountant() {
   let count = 0;
-  return function (num1) {
-    return function (num2) {
-      count++;
-      const sum = num1 + num2;
 
-      if (count <= 3) {
+  return function(num1) {
+    return function(num2) {
+      const sum = num1 + num2;
+      count++;
+
+      if (count <= 3 || count % 2 !== 0) {
         return sum;
       }
 
-      return count % 2 !== 0
-        ? sum
-        : 'Bzzz... Error!';
+      return 'Bzzz... Error!';
     };
   };
 }
