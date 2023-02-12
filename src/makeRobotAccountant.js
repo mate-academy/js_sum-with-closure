04bd96888ab54b2e774f96f959d13671c5c94c12
Ivan Krynychnyi <sinus1313@gmail.com>
2023-02-12 22:53:19 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const count = 0;
+
+  return function(a) {
+    return function(b) {
+      if (count % 2 === 0 && count > 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
