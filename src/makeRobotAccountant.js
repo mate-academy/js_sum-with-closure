f8d02ac8f9dfd60a3a56359a8b253c3c75db37d6
tarasnakonechnyi <nakonechnyi.taras@gmail.com>
2023-02-15 01:05:22 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => (y) => {
+    if (count === 3) {
+      count++;
+
+      return 'Bzzz... Error!';
+    };
+
+    count++;
+
+    return x + y;
+  };
 }
 
 module.exports = makeRobotAccountant;
