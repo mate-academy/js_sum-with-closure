820dc7d1d869201d31b56a21e8549fa62b74c43b
MarynaTovtyn <marina6782@gmail.com>
2023-02-15 10:41:03 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(firstNumber) {
+    return function(secondNumber) {
+      count++;
+
+      if (count <= 3 || count % 2 !== 0) {
+        return firstNumber + secondNumber;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
