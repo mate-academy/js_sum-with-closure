38068dd7fe8d22bfc42543e3e128dc767f289b6a
Serhii Nikolin <sergeynikolin@gmail.com>
2023-02-20 11:27:07 +0200

    Task done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const getSum = function(a) {
+    return function(b) {
+      count++;
+
+      if (count % 2 === 0) {
+        if (count !== 2) {
+          return `Bzzz... Error!`;
+        }
+      }
+
+      return a + b;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
