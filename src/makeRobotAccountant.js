095c4880c9ac45a804d9cce0faa1313b0d1650a4
Peti51 <galpeter31@gmail.com>
2023-02-21 13:31:30 +0100

    commit message

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countOfOperation = 0;
+
+  const getSum = (a) => {
+    countOfOperation++;
+
+    const add = (b) => {
+      if (countOfOperation <= 3) {
+        return a + b;
+      }
+
+      if (countOfOperation > 3 && countOfOperation % 2 !== 0) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+
+    return add;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
