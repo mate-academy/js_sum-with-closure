6b6c0dd0edcd06416bcb89d5ee69c766b5551845
Sergey Lipikhin <football3@ukr.net>
2023-02-23 23:53:20 +0200

    Complete solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCounter = 0;
+
+  return (operand1) => {
+    return (operand2) => {
+      callCounter++;
+
+      if (
+        (callCounter % 2 === 0 && callCounter > 3)
+        || callCounter === 4
+      ) {
+        return 'Bzzz... Error!';
+      }
+
+      return operand1 + operand2;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
