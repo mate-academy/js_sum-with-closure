b5e9b01900c4da69fa8c1bc34da9ca837db54a9e
Olha Kissa <olhakissa121r@gmail.com>
2023-02-23 19:36:28 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let counter = 1;
+
+  return function count(n) {
+    counter += 1;
+
+    return function sum(m) {
+      if (counter > 3 && counter % 2 === 1) {
+        return 'Bzzz... Error!';
+      }
+
+      return n + m;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
