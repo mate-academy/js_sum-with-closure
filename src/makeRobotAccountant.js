fbef980aa44719d2fcf3764ab2408275d5318664
Sasha Kononenko <nurchiksasha@gmail.com>
2023-02-24 23:07:07 +0200

    robotAcauntant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function getSum(x) {
+    return (y) => {
+      count++;
+
+      if (count >= 4 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
