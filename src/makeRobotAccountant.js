3e1d8d89ba41da72aaa9eed3cdbdec577f297bf8
Volodymyr Klymiuk <volodymyr.klymiuk@jetsoftpro.com>
2023-02-24 17:26:48 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,24 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+function makeRobotAccountant(x) {
+  let count = 0;
+
+  return function(a) {
+    return function(b) {
+      count++;
+
+      if (count <= 3) {
+        return a + b;
+      }
+
+      if (count % 2 === 0) {
+        return `Bzzz... Error!`;
+      } else {
+        return a + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
