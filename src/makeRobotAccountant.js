e347d52a97511934f75c5d41d803686383ba70dc
gretoq <dovhun3d@gmail.com>
2023-02-25 18:15:23 +0200

    Made robot accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callNumber = 0;
+
+  return function(firstCallValue) {
+    return function(secondCallValue) {
+      callNumber++;
+
+      if (callNumber > 3 && callNumber % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstCallValue + secondCallValue;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
