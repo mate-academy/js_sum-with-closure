03fccdc7b8197d56e88aab4b0f0e54b1b4fc03bb
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-02-25 20:36:34 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const sums = [];
+
+  return (x) => {
+    return (y) => {
+      const sum = x + y;
+
+      sums.push(sum);
+
+      return sums.length > 2 && sums.length % 2 === 0 ? 'Bzzz... Error!' : sum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
