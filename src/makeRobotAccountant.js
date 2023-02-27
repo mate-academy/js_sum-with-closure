06944cbb8d34cbc588a9e671b2a1e0fcb04aee7a
Степан <stepan12@MacBook-Air-Stepan.local>
2023-02-27 23:31:57 +0200

    makeRobotAccountant done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function firstOperand(arg1) {
+    return function secondOperand(arg2) {
+      count++;
+
+      if (count < 3 || count % 2 !== 0) {
+        return (arg1 + arg2);
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
