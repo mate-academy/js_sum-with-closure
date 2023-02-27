b49b9117c1d06619925230b26ef95b1f9f12a652
Oleh Chernenko <oleh.chernenko97@gmail.com>
2023-02-27 20:59:53 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(firstNumber) {
+    return function(secondNumber) {
+      count++;
+
+      if (count < 3) {
+        return firstNumber + secondNumber;
+      }
+
+      if (count % 2 === 1) {
+        return firstNumber + secondNumber;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
