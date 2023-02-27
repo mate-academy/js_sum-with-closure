000948ea9f462a2940bddb9131be70894de71be9
Yaroslav <yaroslav.dzyadiv@gmail.com>
2023-02-27 15:37:35 +0200

    Task done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCount = 0;
+
+  return (x) => {
+    callsCount++;
+
+    return (y) => {
+      if (callsCount % 2 === 0 && callsCount >= 3) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
