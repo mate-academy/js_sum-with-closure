639fd9fef03cacbb1ccb70a32381b617230a768e
vadym4che <vadym4che@gmail.com>
2023-02-27 18:09:25 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const store = {};
+
+  store.count = 0;
+
+  const funcObj = function(x) {
+    store.count++;
+
+    if (store.count >= 4 && store.count % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return (y) => x + y;
+  };
+
+  return funcObj;
 }
 
 module.exports = makeRobotAccountant;
