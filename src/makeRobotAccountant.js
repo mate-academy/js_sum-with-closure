accc959bb578f891c9ed96aa487cf4c33f9412b9
Iryna Serniuk <irynaserniuk@gmail.com>
2023-02-27 19:34:01 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(x) {
+    count++;
+
+    return function(y) {
+      if (count <= 3 || count % 2 !== 0) {
+        return x + y;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
