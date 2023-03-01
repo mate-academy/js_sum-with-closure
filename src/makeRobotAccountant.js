f4784b02570204d62b6718a5c3c0ed1b93183a08
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-03-01 11:06:08 +0200

    Sum with closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const sum = (x) => {
+    return function(y) {
+      count += 1;
+
+      const result = count > 3 && count % 2 === 0
+        ? `Bzzz... Error!`
+        : x + y;
+
+      return result;
+    };
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
