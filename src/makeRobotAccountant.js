d0041a3dfa8c856b349134a2a3ca4a7aa661ab4e
Maindloll <valeriy.mikheyev@gmail.com>
2023-03-02 13:50:00 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let reset = false;
+  let counter = 0;
+
+  return function(a) {
+    return function(b) {
+      counter++;
+
+      if (counter <= 3) {
+        return a + b;
+      } else {
+        reset = !reset;
+        counter = 0;
+
+        if (reset) {
+          return 'Bzzz... Error!';
+        } else {
+          return a + b;
+        }
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
