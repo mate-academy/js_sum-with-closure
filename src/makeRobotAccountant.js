ad64818f9b892841ba8a3478ee67ed158c7fc16b
Diana Snihur <dianasnigur2@gmail.com>
2023-03-04 15:13:05 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const f = (x) => {
+    const g = (y) => {
+      callCount++;
+
+      if (callCount % 4 === 0) {
+        callCount = 0;
+
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+
+    return g;
+  };
+
+  return f;
 }
 
 module.exports = makeRobotAccountant;
