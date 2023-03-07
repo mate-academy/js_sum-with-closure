debf5a967c3e0348d114031cafd8a17716f33290
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-03-07 12:51:22 +0300

    'done'

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,20 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+function makeRobotAccountant(x) {
+  let count = 0;
+
+  return function(y) {
+    return function(z) {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return z + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
