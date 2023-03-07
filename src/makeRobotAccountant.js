a457990a16bf9690b9b5b5c5ff9092d0b865a5cb
vadym4che <vadym4che@gmail.com>
2023-03-07 03:55:51 +0200

    fixed and optimized

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,19 +8,17 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  const funcObj = function(x) {
+  return (x) => {
     count++;
 
     return (y) => {
-      if (count >= 4 && count % 2 === 0) {
+      if (count > 3 && count % 2 === 0) {
         return 'Bzzz... Error!';
       }
 
       return x + y;
     };
   };
-
-  return funcObj;
 }
 
 module.exports = makeRobotAccountant;
