2265cc2ee14e253ff0ebb88d94789d80a4f4e05b
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-09 15:09:50 +0100

    fix task

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,17 +7,12 @@
 
 function makeRobotAccountant() {
   let count = 0;
-  const message = 'Bzzz... Error!';
 
   const sum = (a) => {
     const sum1 = (b) => {
-      const add = a + b;
-
-      if (count > 3 && count % 2 === 0) {
-        return message;
-      } else {
-        return add;
-      }
+      return count > 3 && count % 2 === 0
+        ? 'Bzzz... Error!'
+        : a + b;
     };
 
     count++;
