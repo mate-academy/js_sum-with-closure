0968c5dc2e8bd0384f358fe3b6624747637fa319
Anna Peretiatko <tobossanna@gmail.com>
2023-03-11 12:21:33 +0100

    Accountant solution v.2

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,23 +7,16 @@
 
 function makeRobotAccountant() {
   let count = 0;
-  let times = 3;
 
   return function getSum(a) {
     return function(b) {
       count++;
 
-      if (times > 0) {
-        times--;
-
-        return a + b;
-      }
-
-      if (count % 2 === 0) {
+      if (count % 2 === 0 && count >= 3) {
         return 'Bzzz... Error!';
-      } else {
-        return a + b;
       }
+
+      return a + b;
     };
   };
 }
