d95a30aef0f2f25ac625233d5d4a8ad84f0949d0
Alex Stypanets <astep2004@gmail.com>
2023-03-13 19:36:26 +0200

    Solution_1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+  const getSum = (a) => {
+    const sum = (b) => {
+      // console.log('*count - ', count)
+      if (count <= 3 || count % 2 !== 0) {
+        count++;
+
+        return a + b;
+      }
+      count++;
+
+      return 'Bzzz... Error!';
+    };
+
+    return sum;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
