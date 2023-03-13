f180f7880eb9173c00abe27098db9d4f4b226efd
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-03-13 22:16:22 +0200

    sum-with-closure first try

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,17 @@
 
 function makeRobotAccountant() {
   // write code here
+  let numberOfCalls = 0;
+
+  const calculateSum = (a) => {
+    numberOfCalls++;
+
+    return (b) => numberOfCalls > 3 && numberOfCalls % 2 === 0
+      ? 'Bzzz... Error!'
+      : a + b;
+  };
+
+  return calculateSum;
 }
 
 module.exports = makeRobotAccountant;
