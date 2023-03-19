ceb35157f0cd940af101a594db9c4d4fb910bd65
Leonid <leonidbondarchuk6@gmail.com>
2023-03-19 20:15:31 +0200

    edits

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+
+  return numberA => numberB => {
+    count++;
+
+    if (count % 2 === 0 || count <= 3) {
+      return numberA + numberB;
+    }
+
+    return 'Bzzz... Error!';
+  };
 }
 
 module.exports = makeRobotAccountant;
