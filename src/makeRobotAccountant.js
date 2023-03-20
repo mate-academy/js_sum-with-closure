b6470831df7561cb1e5f189de10c3e90128f56f9
diamat1997 <diamat1997@gmail.com>
2023-03-20 15:59:03 +0200

    commit message

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,21 +7,17 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  const getSum = x => {
-    const add = y => {
+  return x => {
+    count++;
+
+    return y => {
       if (count > 3 && count % 2 === 0) {
         return 'Bzzz... Error!';
       }
 
       return x + y;
     };
-
-    count++;
-
-    return add;
   };
-
-  return getSum;
 }
 
 module.exports = makeRobotAccountant;
