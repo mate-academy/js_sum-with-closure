07f2ea92d73f43b824e4def9c960048c2331c242
Zibi95 <sylwester.sob95@gmail.com>
2023-03-21 19:01:19 +0100

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let numberOfCalls = 0;
+
+  return function add(firstNumber) {
+    return function anotherAdd(secondNumber) {
+      numberOfCalls++;
+
+      if (numberOfCalls >= 4 && numberOfCalls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
