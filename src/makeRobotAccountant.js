eeb72b5a35ecfe96eda06b17ed494985f71b0fae
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-03-22 22:11:10 +0200

    robot is ready for count woth bugs

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (firstNum) => {
+    count++;
+
+    return count > 3 && count % 2 === 0
+      ? () => 'Bzzz... Error!'
+      : secondNum => firstNum + secondNum;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
