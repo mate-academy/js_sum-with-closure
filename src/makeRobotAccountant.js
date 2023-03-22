0db7d890596b4816b169d45d3a7d8f87040edd49
Vadym Dubniak <dubnyakv@gmail.com>
2023-03-22 19:15:01 +0200

    added robotAccountant realization

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return (number) => {
+    return (secondNumber) => {
+      callCount++;
+
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return number + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
