4550fb15987bb26e10c4cedcbde825aa07adea50
Daryna Hnidash <melissa998877@gmail.com>
2023-03-22 19:18:06 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let counter = 0;
 
+  function getSum(x) {
+    counter++;
+
+    return (y) => {
+      if (counter < 4) {
+        return x + y;
+      } else {
+        return counter % 2 ? x + y : 'Bzzz... Error!';
+      }
+    };
+  };
+
+  return getSum;
+}
 module.exports = makeRobotAccountant;
