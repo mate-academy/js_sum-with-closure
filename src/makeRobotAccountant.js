bcf0a8c3a1dbc390eb8e2db0655ede484b18caa5
Anna Latusha <annapop03072701@gmail.com>
2023-03-23 14:59:51 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function getSum(x) {
+    count++;
+
+    return function(y) {
+      return count > 3 && count % 2 === 0
+        ? 'Bzzz... Error!'
+        : x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
