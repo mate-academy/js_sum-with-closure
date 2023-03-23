ec4c8aa3585bfc5955e1fb35941f06f96aa13c20
Nataliia Henda <nataliehenda1296@gmail.com>
2023-03-23 15:23:31 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,23 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  return (a) => {
+    return (b) => {
+      const summ = a + b;
+
+      count++;
+
+      if (count < 3) {
+        return summ;
+      } else {
+        return count % 2 === 0
+          ? 'Bzzz... Error!'
+          : summ;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
