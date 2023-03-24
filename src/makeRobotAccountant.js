21f2818ef70e54077bf67e7c40d152626e41fe11
Oleksandr <stambulyak1012@gmail.com>
2023-03-24 12:40:12 +0200

    fixed

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -12,7 +12,7 @@ function makeRobotAccountant() {
     return function(y) {
       count++;
 
-      if (count === 4) {
+      if (count >= 4 && count % 2 === 0) {
         return 'Bzzz... Error!';
       }
 
