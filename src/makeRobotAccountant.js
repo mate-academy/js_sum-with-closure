d3e27433ef5572e44c703ec57269e5319b5e9a4e
Vladyslav <vladlischenovsky@gmail.com>
2023-03-24 20:03:41 +0200

    make robot accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(firstNum) {
+    count++;
+
+    return function(secondNum) {
+      return count > 3 && count % 2 === 0
+        ? 'Bzzz... Error!'
+        : firstNum + secondNum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
