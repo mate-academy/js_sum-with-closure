6275ea51705fb5d1a24060a472424c7360d4c0f2
Aleksandr <alexnovaklawyer@gmail.com>
2023-03-24 17:55:22 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,21 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+function makeRobotAccountant(a) {
+  let counter = 0;
+
+  return (firstNum) => {
+    return (secondNum) => {
+      counter++;
+
+      const isCallCounterBiggerThanThree = counter > 3;
+      const isCallCounterOdd = counter % 2 === 0;
+
+      return isCallCounterBiggerThanThree && isCallCounterOdd
+        ? 'Bzzz... Error!'
+        : firstNum + secondNum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
