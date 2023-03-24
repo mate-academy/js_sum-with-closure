0433ea9e65341d4190d1ac1177621ee2839380ae
Nikita Zadorozhnyi <nikita.zadorozhnyi@nure.ua>
2023-03-24 16:27:07 +0200

    robot works

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (firstNum) => {
+    return (secondNum) => {
+      count++;
+
+      const overload = count >= 3 && count % 2 === 0;
+
+      if (overload) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNum + secondNum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
