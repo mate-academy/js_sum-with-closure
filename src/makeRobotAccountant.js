6fa45a27425bd9f4cf191954ca8fb19cfeb39c1c
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-03-24 12:13:50 +0200

    Robot accountant is ready

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let timesCalled = 0;
+  let firstAddendum = null;
+
+  const robotAccountant = (number) => {
+    if (firstAddendum !== null) {
+      const sum = firstAddendum + number;
+
+      firstAddendum = null;
+      timesCalled++;
+
+      return timesCalled <= 3 || timesCalled % 2
+        ? sum
+        : 'Bzzz... Error!';
+    }
+
+    firstAddendum = number;
+
+    return robotAccountant;
+  };
+
+  return robotAccountant;
 }
 
 module.exports = makeRobotAccountant;
