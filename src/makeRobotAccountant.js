f2f01566d9d4cf18f7aa1c28413864d9ea5c7d5c
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-03-24 12:27:15 +0200

    refactored the function

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,26 +7,15 @@
 
 function makeRobotAccountant() {
   let timesCalled = 0;
-  let firstAddendum = null;
 
-  const robotAccountant = (number) => {
-    if (firstAddendum !== null) {
-      const sum = firstAddendum + number;
-
-      firstAddendum = null;
+  return firstAddendum =>
+    secondAddendum => {
       timesCalled++;
 
       return timesCalled <= 3 || timesCalled % 2
-        ? sum
+        ? firstAddendum + secondAddendum
         : 'Bzzz... Error!';
-    }
-
-    firstAddendum = number;
-
-    return robotAccountant;
-  };
-
-  return robotAccountant;
+    };
 }
 
 module.exports = makeRobotAccountant;
