0b103ad3d862a9b7a3c79500687e7141cc2e6dd8
Hamuud <artemlask2014@gmail.com>
2023-03-24 19:10:49 +0200

    Create headCrash in the robot

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (a) => {
+    count++;
+
+    return (b) => {
+      return count % 2 === 0 && count > 3
+        ? 'Bzzz... Error!'
+        : a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
