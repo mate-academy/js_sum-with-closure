6a4bc64d7448c6b4254a4b6f81c80b1ec897487d
Rodionsav <rodionsav@mate.academy>
2023-03-25 15:09:22 +0100

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countCall = 0;
+
+  return (x) => {
+    return (y) => {
+      countCall++;
+
+      if (countCall > 3 && countCall % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
