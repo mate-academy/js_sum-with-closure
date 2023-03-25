ebb2b1f1235a53aae02064caa3dd98125a203970
glots-a <glots@ukr.net>
2023-03-25 14:11:28 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(number) {
+    return function(argument) {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return argument + number;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
