f2b29436223e9066b4d3a2a1606335e174400dcb
Goto-Light <vadim.tkachenko.1997@gmail.com>
2023-03-25 19:09:12 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const firstValue = (first) => {
+    const secondValue = (second) => {
+      count++;
+
+      if (count >= 4 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return first + second;
+    };
+
+    return secondValue;
+  };
+
+  return firstValue;
 }
 
 module.exports = makeRobotAccountant;
