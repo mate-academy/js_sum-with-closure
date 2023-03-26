c6378d1c8aee52e25338db85358307de78172232
Oleh Kolodii <bigknife4@gmail.com>
2023-03-26 18:33:10 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return function(x) {
+    return function(y) {
+      counter++;
+
+      return (counter % 2 === 0 && counter > 3)
+        ? 'Bzzz... Error!'
+        : x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
