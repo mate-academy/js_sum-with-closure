a528031139cbe83debb13be42a5c3835e10faacc
Aleksandr <alexnovaklawyer@gmail.com>
2023-03-27 15:51:51 +0300

    fixing errors

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,8 +8,8 @@
 function makeRobotAccountant(a) {
   let counterCall = 0;
 
-  return (firstNumber) => {
-    return (secondNumber) => {
+  return (firstNum) => {
+    return (secondNum) => {
       counterCall++;
 
       const isLimitExceeded = counterCall > 3;
@@ -17,7 +17,7 @@ function makeRobotAccountant(a) {
 
       return isLimitExceeded && isCallCounterOdd
         ? 'Bzzz... Error!'
-        : firstNumber + secondNumber;
+        : firstNum + secondNum;
     };
   };
 }
