f3aa97542a281e00ad6ad9a2cb6ff62328714562
kukla1989 <kukla.1989@ukr.net>
2023-03-27 12:05:16 +0300

    solve makeRobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -3,10 +3,30 @@
 /**
  *
  * @return {function}
+ * const getSum = makeRobotAccountant();
+getSum(5)(6) === 11;
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let currentCall = 0;
+
+  return (a) => {
+    currentCall++;
+
+    return (currentCall > 3 && currentCall % 2 !== 1)
+      ? () => 'Bzzz... Error!'
+      : b => a + b;
+  };
 }
 
 module.exports = makeRobotAccountant;
+
+// return (a) => {
+//   currentCall++;
+
+//   if (currentCall > 3 && currentCall % 2 !== 1) {
+//     return () => 'Bzzz... Error!';
+//   }
+
+//   return b => a + b;
+// };
