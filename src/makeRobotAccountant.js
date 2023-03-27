a3e2c37c60cbd1f0dbe22b4cd184b8a1c7bf64ad
Myla Crystal <code.crystal.008@gmail.com>
2023-03-27 09:35:49 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => {
+    return (y) => {
+      count++;
+
+      return (count >= 4 && count % 2 === 0)
+        ? 'Bzzz... Error!'
+        : x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
