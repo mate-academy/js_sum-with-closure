e652f596a36cd4a0b8dda8e3d388ff0c31eed99c
Stas Lukianchuk <stas.lukianchuk@icloud.com>
2023-03-27 16:20:32 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,18 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let accountingCount = 0;
+
+  return function(addition1) {
+    accountingCount++;
+
+    return function(addition2) {
+      return accountingCount > 3
+        && accountingCount % 2 === 0
+        ? 'Bzzz... Error!'
+        : addition1 + addition2;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
