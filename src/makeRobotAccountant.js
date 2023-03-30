0b403a067d22735eb78c81f84b34e1c020675c3f
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-03-30 14:03:18 +0300

    commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,17 @@
 
 function makeRobotAccountant() {
   // write code here
+  let counter = 0;
+
+  const func = function(first) {
+    const secPar = function(sec) {
+      counter++;
+      return counter % 2 === 0 ? 'Bzzz... Error' : first + sec;
+    }
+
+    return secPar;
+  }
+  return func;
 }
 
 module.exports = makeRobotAccountant;
