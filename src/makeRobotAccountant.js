422e04f0b8c9045b0ec913804f3298cbc391a7ae
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-03-31 21:07:53 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const robotAccounter = (a) => {
+    function accounter(b) {
+      count++;
+
+      if (count > 3) {
+        if (count % 2 === 0) {
+          return 'Bzzz... Error!';
+        }
+      }
+
+      const sum = a + b;
+
+      return sum;
+    };
+
+    return accounter;
+  };
+
+  return robotAccounter;
 }
 
 module.exports = makeRobotAccountant;
