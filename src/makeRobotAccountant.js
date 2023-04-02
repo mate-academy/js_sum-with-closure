7a679aa8945e05526124ef33baee90cf89c2455b
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-04-02 19:18:34 +0200

    sum with a closure was created

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = -1;
+
+  return function(x) {
+    count++;
+
+    return (y) => {
+      if (count >= 3) {
+        if (count % 2 !== 0) {
+          return 'Bzzz... Error!';
+        }
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
