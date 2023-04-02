38fba01635a9cbe4c5170e6f24be0eabc8240ee8
Mikołaj <ostermi@wp.pl>
2023-04-02 12:16:52 +0100

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let sumCallCounter = 0;
+
+  function sumOperation(firstNumber) {
+    sumCallCounter += 1;
+
+    function AddNumbers(secondNumber) {
+      if (sumCallCounter > 3 && sumCallCounter % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return firstNumber + secondNumber;
+      }
+    }
+
+    return AddNumbers;
+  }
+
+  return sumOperation;
 }
 
 module.exports = makeRobotAccountant;
