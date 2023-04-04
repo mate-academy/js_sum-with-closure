c523a6778a4c75482408258f545e5c87ab3c30f1
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-04 22:44:17 +0300

    Done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (num1) => (num2) => {
+    count += 1;
+
+    if (count < 4) {
+      return num1 + num2;
+    }
+
+    if (count % 2 === 0) {
+      return 'Bzzz... Error!';
+    }
+
+    return num1 + num2;
+  };
 }
 
 module.exports = makeRobotAccountant;
