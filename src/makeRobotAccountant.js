5fa3c06e034f00d98e16f3eb24bb5912548fbc76
Natalia Klonowska <natalia742@gmail.com>
2023-04-11 20:23:23 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,27 @@
 
 function makeRobotAccountant() {
   // write code here
+  let callsCount = 0;
+
+  function getSum(firstNumber) {
+    return (secondNumber) => {
+      const sum = firstNumber + secondNumber;
+
+      callsCount++;
+
+      if (callsCount <= 3) {
+        return sum;
+      }
+
+      if (callsCount % 2) {
+        return sum;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
