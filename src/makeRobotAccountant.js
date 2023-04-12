307c06b46f6ab1df9d89a8925573456e4290baec
marshalek37 <patrykmarp@gmail.com>
2023-04-12 22:11:09 +0200

    First attempt

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return function(num1) {
+    callCount++;
+
+    return function(num2) {
+      callCount++;
+
+      const isEven = callCount % 2 === 0 && callCount > 3;
+      const result = isEven ? 'Bzzz... Error!' : num1 + num2;
+
+      return result;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
