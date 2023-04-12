33d05d23751afd2f9789d471ba9f65e09e9fc4e8
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-04-12 13:54:36 +0200

    solution v1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,20 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+function makeRobotAccountant(arg) {
+  let callsCounter = 0;
+
+  const getSum = (a) => {
+    callsCounter++;
+
+    if (callsCounter > 3 && callsCounter % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return b => a + b;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
