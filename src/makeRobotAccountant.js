8b40c5d44725f35b85c6b52137dca5a3a82f9dfe
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-04-13 16:33:27 +0200

    solution 1.0

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  function f(a) {
+    ++counter;
+
+    return function(b) {
+      if (counter > 3 && counter % 2 === 0) {
+        return `Bzzz... Error!`;
+      };
+
+      if (counter > 3 && counter % 2 !== 0) {
+        return a + b;
+      };
+
+      return a + b;
+    };
+  }
+
+  return f;
 }
 
 module.exports = makeRobotAccountant;
