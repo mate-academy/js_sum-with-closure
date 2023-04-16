83900d1e2dcd311d9f316839afee29e7df11792d
Antonio S <soi4.toxa@ukr.net>
2023-04-16 15:37:44 +0300

    Robot-accountant-1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  const sum = (digitOne) => {
+    return (digitTwo) => {
+      calls++;
+
+      if (calls > 3 && calls % 2 === 0) {
+        return `Bzzz... Error!`;
+      }
+
+      return digitOne + digitTwo;
+    };
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
