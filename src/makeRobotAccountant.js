5f06cc7e017696122ce2ba7c0cd2e606b33c86d1
Kacper Łyczba <suaby1725@gmail.com>
2023-04-17 06:24:17 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,18 +7,18 @@
 
 function makeRobotAccountant() {
   // write code here
-  let calculationIndex = 0;
+  let calcIndex = 0;
 
   return function(a) {
-    if (calculationIndex < 3 || calculationIndex % 2 === 0) {
-      calculationIndex += 1;
+    if (calcIndex < 3 || calcIndex % 2 === 0) {
+      calcIndex += 1;
 
       return function(b) {
         return a + b;
       };
     }
 
-    calculationIndex += 1;
+    calcIndex += 1;
 
     return function(b) { // This was necessary to fix "getSum is not a function"
       return 'Bzzz... Error!';
