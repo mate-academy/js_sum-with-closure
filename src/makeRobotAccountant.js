cea15e95d62bad5df4c479b59a27fd4c73a04e70
drhtka <drhtka@gmil.com>
2023-04-18 18:04:30 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function getSum(x) {
+    return function(y) {
+      count++;
+
+      if (count <= 3 || count % 2 !== 0) {
+        return x + y;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
