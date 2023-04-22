a7f80950f7a5290b1045450e69b3b70b2034d895
marshalek37 <patrykmarp@gmai.com>
2023-04-22 17:49:35 +0200

    Fix of develop branch

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const getSum = (num1) => {
+    callCount++;
+
+    return (num2) => {
+      const isEvenCall = callCount % 2 === 0;
+      const isInvalidCall = isEvenCall && callCount > 3;
+
+      return isInvalidCall ? 'Bzzz... Error!' : num1 + num2;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
