dc96a78a5ecd2329c55918b5e48818cf84cbcd34
Yura Enttsel <equikmla@gmail.com>
2023-04-25 21:03:49 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(x) {
+    return function(y) {
+      count++;
+
+      if (count <= 3) {
+        return x + y;
+      }
+
+      return (count % 2 === 0)
+        ? 'Bzzz... Error!'
+        : x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
