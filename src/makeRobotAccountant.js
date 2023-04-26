e9628bc0f7cafc47f2b8625002c1587b75f62948
Karyna Onyshchuk <onyshchukkarina@gmail.com>
2023-04-26 16:24:03 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (num1) => {
+    return (num2) => {
+      count++;
+
+      if (count === 4) {
+        count = 0;
+
+        return 'Bzzz... Error!';
+      } else {
+        return num1 + num2;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
