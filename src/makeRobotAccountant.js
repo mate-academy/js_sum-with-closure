d90a2104963dbf77be2c3a4cd8de98fc1715a624
DefaultGitUser <usingforgit@gmail.com>
2023-04-26 13:30:32 +0300

    Sol

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,28 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const MAX_CALLS = 3;
+  let callsCount = 0;
+  let isEven = false;
+  const robotAccountant = (x = 0) => {
+    isEven = !isEven;
+
+    if (callsCount <= MAX_CALLS) {
+      callsCount++;
+    }
+
+    return function(y) {
+      if (callsCount <= MAX_CALLS) {
+        return x + y;
+      }
+
+      return isEven
+        ? x + y
+        : 'Bzzz... Error!';
+    };
+  };
+
+  return robotAccountant;
 }
 
 module.exports = makeRobotAccountant;
