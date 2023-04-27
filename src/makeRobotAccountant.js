3b6044a798fc99bb116cf7f3839293e2d0c0f3c4
Karyna Onyshchuk <onyshchukkarina@gmail.com>
2023-04-27 19:05:44 +0300

    Solution2

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,16 +8,14 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  return (num1) => {
-    return (num2) => {
+  return (firstNumber) => {
+    return (secondNumber) => {
       count++;
 
-      if (count === 4) {
-        count = 0;
-
+      if (count >= 4 && count % 2 === 0) {
         return 'Bzzz... Error!';
       } else {
-        return num1 + num2;
+        return firstNumber + secondNumber;
       }
     };
   };
