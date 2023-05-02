1b79b50b65f4315ee357869d04bdd98478cc2714
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-05-03 01:21:28 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
+
+  return (x) => (y) => {
+    count++;
+
+    if (count === 4) {
+      return 'Bzzz... Error!';
+    }
+
+    return x + y;
+  };
+};
 
 module.exports = makeRobotAccountant;
