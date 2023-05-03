fe14d71759d98eb7bff474b37f8c871c53bcc73e
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-05-03 19:04:26 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (a) => {
+    return (b) => {
+      count++;
+
+      return count > 3 && count % 2 === 0
+        ? 'Bzzz... Error!'
+        : a + b;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
