d9cdee11bd73f6a0874f15556e2894244a0341c7
Leonid Krainik <rialleons@gmail.com>
2023-05-05 14:48:01 +0300

    Calculate sum with a closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let numberOfCalls = 0;
+
+  const robotAccountant = (number1) => {
+    return (number2) => {
+      numberOfCalls++;
+
+      if (numberOfCalls < 3 || numberOfCalls % 2 !== 0) {
+        return number1 + number2;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
+
+  return robotAccountant;
 }
 
 module.exports = makeRobotAccountant;
