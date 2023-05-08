f36b09566540c5be49b42d69d70b894313ded075
Ruslan Pashkov <nothingsfading@gmail.com>
2023-05-08 17:49:18 +0300

    Add solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const getSum = (firstNumber) => (secondNumber) => {
+    callCount++;
+
+    if (callCount > 3 && callCount % 2 === 0) {
+      return 'Bzzz... Error!';
+    }
+
+    return firstNumber + secondNumber;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
