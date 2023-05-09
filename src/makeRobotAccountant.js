ab45e66330ed5b74e674e15c23b78ead5cbafdd7
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-05-09 11:26:40 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  
+  const sum = (x) => {
+    const secondVal = (y) => {
+      count++;
+
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+
+    return secondVal;
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
