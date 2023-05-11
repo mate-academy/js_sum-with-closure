752d84067964233f0fabecda37eb3b161adad2b9
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-11 14:11:26 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,28 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+
+  const addNum = x => {
+    if (x === -Infinity) {
+      return Infinity;
+    }
+
+    const sumFunc = (y) => {
+      if (count > 3 && count % 2 === 0) {
+        count++;
+
+        return 'Bzzz... Error!';
+      }
+      count++;
+
+      return x + y;
+    };
+
+    return sumFunc;
+  };
+
+  return addNum;
 }
 
 module.exports = makeRobotAccountant;
