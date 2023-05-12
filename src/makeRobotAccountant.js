147b9e98c8b0c9cbbfc3ddae1f4f1ec2dd8d792e
Antonio Zuccon Ghiotto <antonio.zucconghiotto@gmail.com>
2023-05-12 16:01:29 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,29 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let times = 0;
+
+  return function getSum(a) {
+    function x(b) {
+      let sum = 0;
+
+      sum += a + b;
+
+      if (times <= 3) {
+        return sum;
+      }
+
+      if (times % 2 !== 0 && times >= 5) {
+        return sum;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+
+    times++;
+
+    return x;
+  };
 }
 
 module.exports = makeRobotAccountant;
