13f5a1909963c0be349acd01ff26e80802020a45
Maksym Nemera <nickmitchell@ukr.net>
2023-05-12 16:28:09 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,29 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(x) {
+    return function(y) {
+      const sum = x + y;
+
+      count++;
+
+      if (count === 4) {
+        return 'Bzzz... Error!';
+      }
+
+      if (count > 3) {
+        if (count % 2 === 0) {
+          return 'Bzzz... Error!';
+        }
+
+        return sum;
+      }
+
+      return sum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
