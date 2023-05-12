9e3e6b9af48722cd7f4c0cbc99d0ec134c791c8a
Andrii <zisuan.od@gmail.com>
2023-05-12 19:23:03 +0300

    task solved

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,25 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+
+  function getSum(a) {
+    return function(b) {
+      count++;
+
+      if (count > 2 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return a + b;
+      }
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
+
+// if (count > 3 && count % 2 === 0) {
+//   return 'Bzzz... Error!';
+// }
