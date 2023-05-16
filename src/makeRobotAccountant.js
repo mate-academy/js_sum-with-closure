a9b2307f9e17c20e04fa71e1b6d2ace38de30c77
Mykola Brodyuk <nick.brodyuk@gmail.com>
2023-05-16 17:57:40 +0300

    fix

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,18 +5,24 @@
  * @return {function}
  */
 
-function makeRobotAccountant(a) {
+function makeRobotAccountant() {
   let count = 0;
 
-  return firstNumber => secondNumber => {
-    count++;
+  const getSum = (firstNumber) => {
+    const sum = (secondNumber) => {
+      count++;
 
-    if (count > 3 && !(count % 2)) {
-      return `Bzzz... Error!`;
-    }
+      if (count > 3 && !(count % 2)) {
+        return `Bzzz... Error!`;
+      }
 
-    return firstNumber + secondNumber;
+      return firstNumber + secondNumber;
+    };
+
+    return sum;
   };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
