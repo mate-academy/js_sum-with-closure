ceffe6a47a3fe5373417371418981e90bd0c82b8
AlexKherson <q0992358472@gmail.com>
2023-05-17 22:26:13 +0300

    added function makeRobotaccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,18 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const maxCountUninterruptedWork = 3;
+  let count = 0;
+
+  return (prev) => (curr) => {
+    count++;
+
+    if (count > maxCountUninterruptedWork && count % 2 === 0) {
+      return 'Bzzz... Error!';
+    }
+
+    return prev + curr;
+  };
 }
 
 module.exports = makeRobotAccountant;
