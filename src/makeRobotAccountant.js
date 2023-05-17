c34f4bfcd97662580a556f654a263ef430ee438a
Maksym Nemera <nickmitchell@ukr.net>
2023-05-17 12:21:06 +0300

    fixes variables and delete const sum

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,17 +8,15 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  return function(x) {
-    return function(y) {
-      const sum = x + y;
-
+  return function(firstNum) {
+    return function(secondNum) {
       count++;
 
       if (count > 3 && count % 2 === 0) {
         return 'Bzzz... Error!';
       }
 
-      return sum;
+      return firstNum + secondNum;
     };
   };
 }
