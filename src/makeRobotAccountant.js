3f05e9ab1e3e74102fd8ab4a0bda7fd644215f46
Maksym Shevelin <shevmax01@gmail.com>
2023-05-19 13:35:47 +0300

    done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,37 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let callCount = 0;
+  let resetCallCount = 0;
+
+  const getSum = (a) => {
+    return (b) => {
+      if (callCount === 3) {
+        callCount++;
+        resetCallCount++;
+
+        return 'Bzzz... Error!';
+      };
+
+      if (callCount > 3 && resetCallCount % 2 !== 0) {
+        resetCallCount++;
+
+        return a + b;
+      };
+
+      if (callCount > 3 && resetCallCount % 2 === 0) {
+        resetCallCount++;
+
+        return 'Bzzz... Error!';
+      }
+
+      callCount++;
+
+      return a + b;
+    };
+  };
+
+  return getSum;
+};
 
 module.exports = makeRobotAccountant;
