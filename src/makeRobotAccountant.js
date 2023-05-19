5efcdb9386df73eff31091483e13b1369ecd7437
Alla Nebozhenko <111634874+AllaNebozhenko@users.noreply.github.com>
2023-05-19 17:46:03 +0200

    Robot Accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let functionCall = 0;
+
+  return function(x) {
+    return function(y) {
+      functionCall++;
+
+      if (functionCall > 3 && functionCall % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
