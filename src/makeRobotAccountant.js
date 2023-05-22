a24650d35bdc27519577d24c33be4ec9d2bb77b8
Eugene Sherepa <evgenijserepa@gmail.com>
2023-05-22 18:36:43 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countOperations = 0;
+
+  return function(firstNumber) {
+    return function(secondNumber) {
+      countOperations++;
+
+      if (countOperations <= 3 || countOperations % 2 !== 0) {
+        const result = firstNumber + secondNumber;
+
+        return result;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
