9ffb8fba5de996a0a58e456ea17eedbcb4e6a514
Oleksii Lysenko <nano77@ukr.net>
2023-05-22 15:43:17 +0300

    add functionality to makeRobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 1;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      if (counter > 3 && counter % 2 === 0) {
+        counter++;
+
+        return 'Bzzz... Error!';
+      }
+
+      counter++;
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
