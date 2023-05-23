137382d63f6a2a80ec7e6400f8cca38ecfaf6eeb
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-05-23 23:18:07 +0300

    made task js sum-with-closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+
+  return (num1) => {
+    return (num2) => {
+      if (count < 4 || count % 2 !== 0) {
+        count++;
+
+        return num1 + num2;
+      }
+
+      count++;
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
