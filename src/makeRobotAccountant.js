ceab4f666185aa0d304d95d0bff969ffcee943b9
Kateryna Sokur <k.o.sokur@gmail.com>
2023-05-23 16:31:09 +0200

    add solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (num1) => {
+    return (num2) => {
+      count++;
+
+      if (count <= 3 || count % 2 !== 0) {
+        return num1 + num2;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
