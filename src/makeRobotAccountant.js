a5671c618fcfa648226e59d395a2ccb9d9a841a0
Iryna Balandiukh <balandiukh.i@gmail.com>
2023-05-23 13:55:34 +0300

    Add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (a) => {
+    counter++;
+
+    return (b) => {
+      return counter <= 3 || counter % 2 !== 0
+        ? a + b
+        : 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
