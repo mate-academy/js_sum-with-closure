a17553cbfd6bbb3fc585fbdf5950909856c1fc7e
Katya Zaremska <unnarummel@gmail.com>
2023-05-25 22:52:59 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const getSum = (firstNumber) => {
+    callCount++;
+
+    return (secondNumber) => {
+      const isEvenCall = callCount % 2 === 0;
+      const isInvalidCall = isEvenCall && callCount > 3;
+
+      return isInvalidCall ? 'Bzzz... Error!' : firstNumber + secondNumber;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
