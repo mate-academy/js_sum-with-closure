35c5a0dd17706276283615b0e8b8f836d6822edd
Liza Strykharchuk <ol220681vol@gmail.com>
2023-05-27 19:30:41 +0300

    code

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function getSum(firstVar) {
+    return (secondVar) => {
+      count++;
+
+      if (count <= 3 || count % 2 !== 0) {
+        return firstVar + secondVar;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
