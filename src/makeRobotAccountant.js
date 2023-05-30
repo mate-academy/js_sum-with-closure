39e587cdbc7e2ff59e2a301aefbeda1bd4825f5f
Evhen Ivchenko <ivchenkoevhen@gmail.com>
2023-05-30 23:00:19 +0300

    fixed version

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return (firstNumber) => {
+    callCount++;
+
+    return (secondNumber) => {
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
