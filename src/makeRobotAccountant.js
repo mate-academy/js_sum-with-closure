b8f3fe40c535858b9ff4fa14d8e151ef0e33ac84
iv4ek <ivaskovila@gmail.com>
2023-06-03 09:44:40 +0200

    Robot Accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let attemts = 0;
+  const errorMessage = 'Bzzz... Error!';
+
+  const getSum = (a) => b => {
+    const sum = a + b;
+
+    attemts++;
+
+    if (attemts > 3 && attemts % 2 === 0) {
+      return errorMessage;
+    };
+
+    return sum;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
