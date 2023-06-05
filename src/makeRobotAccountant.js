2daeca0bebf2e64f60ce4517ba2ac9495b526797
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2023-06-05 19:32:43 +0200

    Create sum calculator

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let additionsCounter = 0;
+
+  return function(number1) {
+    return function(number2) {
+      additionsCounter++;
+
+      return additionsCounter > 3 && additionsCounter % 2 === 0
+        ? 'Bzzz... Error!'
+        : number1 + number2;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
