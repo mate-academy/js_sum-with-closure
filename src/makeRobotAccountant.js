370c43bda4886a68adb90768ae9cf8d324d9cb13
Dmytro Fefelov <reseneweb@gmail.com>
2023-06-13 14:34:29 +0300

    Sum with a closure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -4,9 +4,20 @@
  *
  * @return {function}
  */
-
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  function sum(a) {
+    counter++;
+
+    if ((counter >= 4) && ((counter % 2) === 0)) {
+      return (b) => 'Bzzz... Error!';
+    }
+
+    return (b) => a + b;
+  }
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
