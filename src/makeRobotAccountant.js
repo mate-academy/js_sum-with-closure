6442081215e494f5d8dde159a024d732ca72a257
Alina <kozlovska.a.mail@gmail.com>
2023-06-18 02:56:13 +0200

    add solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,16 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+function makeRobotAccountant(number) {
+  let countOfCalls = 0;
+
+  return a => b => {
+    countOfCalls++;
+
+    return countOfCalls > 3 && countOfCalls % 2 === 0
+      ? 'Bzzz... Error!'
+      : a + b;
+  };
 }
 
 module.exports = makeRobotAccountant;
