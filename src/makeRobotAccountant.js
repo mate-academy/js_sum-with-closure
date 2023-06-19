07408f4f67c5bfe12a839a7e9bd4f0dbc878c159
artkas03 <zhonayer2017@gmail.com>
2023-06-19 21:05:57 +0300

    task finished

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let iterationCounter = 0;
+
+  const getSum = (number) => {
+    return (number2) => {
+      const result = number + number2;
+
+      if (iterationCounter >= 3 && iterationCounter % 2 === 1) {
+        iterationCounter++;
+
+        return 'Bzzz... Error!';
+      }
+
+      iterationCounter++;
+
+      return result;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
