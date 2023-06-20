25efb882857b9d85dcb4ec2fe7b078ccf8850439
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-06-20 22:37:00 +0300

    first version

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -4,9 +4,22 @@
  *
  * @return {function}
  */
-
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(a) {
+    count++;
+
+    return function(b) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
