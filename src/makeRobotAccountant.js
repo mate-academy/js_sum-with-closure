3046b685c10edd99d3c489fa139d5756e12f2483
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-06-21 17:19:28 +0300

    Add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let operationsCount = 0;
+
+  const robotAccountant = (a) => {
+    operationsCount++;
+
+    if (operationsCount > 3 && operationsCount % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return (b) => a + b;
+  };
+
+  return robotAccountant;
 }
 
 module.exports = makeRobotAccountant;
