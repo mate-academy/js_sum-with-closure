1770c6388cb02c06349325080d03ecf4315c4c6a
Nadiia Kovalchuk <nadiaaaa1256@gmail.com>
2023-06-21 21:13:56 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function curry(curriedFunction) {
+    return function(function1) {
+      return function(function2) {
+        return curriedFunction(function1, function2);
+      };
+    };
+  }
+
+  function getSum(num1, num2) {
+    count++;
+
+    if (count < 4 || count % 2 === 1) {
+      return num1 + num2;
+    }
+
+    return 'Bzzz... Error!';
+  }
+
+  return curry(getSum);
 }
 
 module.exports = makeRobotAccountant;
