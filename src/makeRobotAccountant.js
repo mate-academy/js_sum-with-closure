7ae7c849fb3157ea2390ef33d983b175146e55cd
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-06-21 18:56:20 +0300

    sum-with-closure solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 1;
+
+  return (num1) => {
+    return (num2) => {
+      const evenCount = (count > 3 && count % 2 === 0);
+      const errorMessage = 'Bzzz... Error!';
+      const sum = num1 + num2;
+
+      count++;
+
+      return evenCount ? errorMessage : sum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
