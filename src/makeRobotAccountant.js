6205d9f8fc09f571c19217b1e9aa5dd1f2f56566
Iryna <irinabolbat98@gmail.com>
2023-06-22 13:58:35 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function getSum(a) {
+    return (b) => {
+      count++;
+
+      if (count >= 4 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return a + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
