894430eac9390d2247044bf053e9109629f4a796
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-06-22 14:48:08 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(a) {
+    return function(b) {
+      count++;
+
+      const isEven = count % 2 === 0;
+
+      if (count > 3 && isEven) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
