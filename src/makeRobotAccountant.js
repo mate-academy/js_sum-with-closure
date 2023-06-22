15a7c584a9a0803fd8f676120c18166a9cf838b2
Valentyna <vabo1108@gmail.com>
2023-06-22 20:33:18 +0200

    siolution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let amount = 0;
+
+  return (num) => {
+    amount++;
+
+    return (num1) => {
+      if (amount > 3 && amount % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return num + num1;
+      };
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
