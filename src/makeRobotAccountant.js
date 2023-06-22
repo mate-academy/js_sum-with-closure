b7db7694c3aa77773214d6d599f5e9414ecd6f83
outofs <rtranscendental@gmail.com>
2023-06-22 16:06:03 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsLeft = 3;
+  let isEvenCall = true;
+
+  return (firstNum) => {
+    const sumFunc = (secondNum) => firstNum + secondNum;
+
+    if (callsLeft) {
+      callsLeft -= 1;
+
+      return (secondNum) => sumFunc(secondNum);
+    }
+
+    if (isEvenCall) {
+      isEvenCall = false;
+
+      return () => 'Bzzz... Error!';
+    }
+    isEvenCall = true;
+
+    return (secondNum) => sumFunc(secondNum);
+  };
 }
 
 module.exports = makeRobotAccountant;
