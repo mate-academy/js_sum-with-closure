67ebb06ef685023ed79836401ecdc9b3e2beffb0
Yevhen <ko4etov.evgeniy@gmail.com>
2023-06-23 01:01:43 +0300

    create makeRobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unmodified-loop-condition */
 'use strict';
 
 /**
@@ -6,7 +7,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return function(a) {
+    counter += 1;
+
+    return function(b) {
+      while (counter < 3) {
+        return a + b;
+      }
+
+      if (counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
