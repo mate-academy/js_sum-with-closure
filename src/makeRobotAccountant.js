a92fa5e10651aa8720e4e3d2816d1ce2448e5d1d
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-06-22 13:39:53 +0300

    Finished task

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,26 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  function getSum(a) {
+    return function(b) {
+      counter++;
+
+      switch (true) {
+        case (counter < 3):
+          return a + b;
+
+        case (counter >= 3 && counter % 2 === 0):
+          return 'Bzzz... Error!';
+
+        default:
+          return a + b;
+      }
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
