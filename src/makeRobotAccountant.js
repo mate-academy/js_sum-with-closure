1059eb8557ec17677684fd6dd323d02f6d2c530d
valet2102 <valikyanyk@gmail.com>
2023-06-22 17:03:00 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,21 @@
 
 function makeRobotAccountant() {
   // write code here
+  let firstNumber = 1;
+
+  return (a) => {
+    const currentNumber = firstNumber;
+
+    return (b) => {
+      firstNumber++;
+
+      if (currentNumber <= 3 || currentNumber % 2 === 1) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
