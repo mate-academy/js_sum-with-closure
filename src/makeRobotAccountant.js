f3ffda49426476b5c6f5ce60cd17d0b018cf4049
Nadiia Kovalchuk <nadiaaaa1256@gmail.com>
2023-06-22 13:40:43 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,25 +8,17 @@
 function makeRobotAccountant() {
   let count = 0;
 
-  function curry(curriedFunction) {
-    return function(function1) {
-      return function(function2) {
-        return curriedFunction(function1, function2);
-      };
+  return function(num1) {
+    return function(num2) {
+      count++;
+
+      if (count < 4 || count % 2 === 1) {
+        return num1 + num2;
+      }
+
+      return 'Bzzz... Error!';
     };
-  }
-
-  function getSum(num1, num2) {
-    count++;
-
-    if (count < 4 || count % 2 === 1) {
-      return num1 + num2;
-    }
-
-    return 'Bzzz... Error!';
-  }
-
-  return curry(getSum);
+  };
 }
 
 module.exports = makeRobotAccountant;
