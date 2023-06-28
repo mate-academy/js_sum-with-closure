b7edb1d757e241315ee949d9d89c434faf1ec653
Valera Viachalo <valeravyachalo@gmail.com>
2023-06-28 12:21:33 +0300

    Robot Accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,18 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let call = 0;
+  const WORK_FINE_CALLS = 3;
+
+  function getSum(firstOperator) {
+    call++;
+
+    return (secondOperator) => (call > WORK_FINE_CALLS && call % 2 === 0)
+      ? 'Bzzz... Error!'
+      : firstOperator + secondOperator;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
