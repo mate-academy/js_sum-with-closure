c55acf7a17705c80720516a3f7e9dc0cb17c23b9
Anton <tony.tarovyk@gmail.com>
2023-07-08 11:38:11 +0300

    add solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return function getSum(a) {
+    counter += 1;
+
+    if (counter % 2 === 0 && counter > 3) {
+      return function getError() {
+        return 'Bzzz... Error!';
+      };
+    }
+
+    return function getSum2(b) {
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
