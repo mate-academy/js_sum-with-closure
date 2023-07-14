cf6baf86cc0b0bf7ee9174b4eb2c04467caa0b09
Daniel Hladkyi <87299182+Hladkyi03@users.noreply.github.com>
2023-07-14 13:53:42 +0300

    Resolved makeRobotAccountant.js

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/**
- *
- * @return {function}
- */
-
 function makeRobotAccountant() {
   let callsCount = 0;
 
@@ -18,20 +13,5 @@ function makeRobotAccountant() {
     return () => 'Bzzz... Error!';
   };
 };
-
-  let count = 0;
-
-  return function(a) {
-    return function(b) {
-      count++;
-
-      if (count <= 3 || count % 2 === 1) {
-        return a + b;
-      } else {
-        return 'Bzzz... Error!';
-      }
-    };
-  };
-}
 
 module.exports = makeRobotAccountant;
