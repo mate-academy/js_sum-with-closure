4b7626d4f7e0e541bb4ab1947a7742b1bd0d39c9
unknown <fanton81@gmail.com>
2023-07-15 15:39:11 +0300

    mySolution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const result = (x) => {
+    return (y) => {
+      count++;
+
+      const evenOccurrence = (count >= 3 && count % 2 === 0);
+
+      return evenOccurrence ? 'Bzzz... Error!' : x + y;
+    };
+  };
+
+  return result;
 }
 
 module.exports = makeRobotAccountant;
