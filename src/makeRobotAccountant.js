7a9d2d4aa88c0189a6ff845349d0d54e636e4e58
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-07-18 16:15:09 +0200

    Added task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = -1;
+
+  return (a) => {
+    return (b) => {
+      counter++;
+
+      if (counter < 3 || (counter % 2 === 0 && counter > 3)) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
