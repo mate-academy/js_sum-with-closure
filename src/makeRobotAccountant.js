1de42e878a68701fb364031705e0ef792d4f8570
Nataliia Nikolaienko <tasinka.nn@gmail.com>
2023-07-24 13:50:47 +0200

    make Robot Accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const getSum = (firstNumber) => {
+    return (secondNumber) => {
+      count++;
+
+      if (count <= 3 || count % 2 !== 0) {
+        return firstNumber + secondNumber;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
