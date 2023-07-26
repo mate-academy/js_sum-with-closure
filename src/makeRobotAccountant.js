fb4b2dcbf0bf036717e8109ca269aaafff1b84a4
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2023-07-26 11:30:52 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCount = 0;
+
+  const sum = (a) => {
+    callsCount++;
+
+    return (b) => {
+      if (callsCount > 3 && callsCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return a + b;
+      }
+    };
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
