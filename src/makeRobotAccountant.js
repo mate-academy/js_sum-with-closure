9f8b518df49a8994301ae1492abb8288ff57933a
Anastasiia Burkovska <burkovskaya.anastasiya@gmail.com>
2023-07-27 16:34:08 +0300

    solutionSumClosure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(a) {
+    count++;
+
+    return (b) => {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
