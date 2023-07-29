a7eaf6db86f4d2ca8f915bdbb261940c350f6cb7
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-07-29 10:39:34 +0200

    task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,29 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  function getSumInner(num1) {
+    counter++;
+
+    if (counter < 4) {
+      return function(num2) {
+        return num1 + num2;
+      };
+    } else {
+      if (counter % 2 === 1) {
+        return function(num2) {
+          return num1 + num2;
+        };
+      } else {
+        return function() {
+          return 'Bzzz... Error!';
+        };
+      }
+    }
+  }
+
+  return getSumInner;
 }
 
 module.exports = makeRobotAccountant;
