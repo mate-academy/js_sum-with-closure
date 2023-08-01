d693e6db6747d4a80e8f8320520809f71c40a680
Andrzej Pec <pimatstudio@gmail.com>
2023-08-01 15:46:55 +0200

    Soluzione per il Robot Contabile

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,35 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function errorMessage() {
+    return 'Bzzz... Error!';
+  }
+
+  return function getSum(a) {
+    if (callCount < 3) {
+      return function(b) {
+        callCount++;
+
+        return add(a, b);
+      };
+    } else if (callCount % 2 === 0) {
+      callCount++;
+
+      return function(b) {
+        return add(a, b);
+      };
+    } else {
+      callCount++;
+      
+      return errorMessage;
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
