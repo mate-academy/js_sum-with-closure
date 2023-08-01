8a8ec75657931f90fd3ce9f804b8ac60614f8c36
Andrzej Pec <pimatstudio@gmail.com>
2023-08-01 21:25:40 +0200

    did I commet these changes, idk

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -16,21 +16,19 @@ function makeRobotAccountant() {
     return 'Bzzz... Error!';
   }
 
-  return function getSum(a) {
+  function getSum(a) {
     callCount++;
 
-    if (callCount < 3) {
-      return function(b) {
-        return sum(a, b);
-      };
-    } else if (callCount % 2 === 0) {
-      return function(b) {
-        return sum(a, b);
-      };
-    } else {
+    if (callCount > 3 && callCount % 2 === 0) {
       return error;
+    } else {
+      return function(b) {
+        return sum(a, b);
+      };
     }
-  };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
