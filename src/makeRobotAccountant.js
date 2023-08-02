a4e2b55c0e7f50011a029c2397a8056b16dd2338
Ivan Krylo <ivan.poltaven@gmail.com>
2023-08-02 12:41:30 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,47 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 1;
+  let summer = 0;
+
+  const getSum = function(a) {
+    if (counter <= 3 && summer !== 0) {
+      const b = summer;
+
+      counter++;
+      summer = 0;
+
+      return b + a;
+    }
+
+    if (counter % 2 !== 0 && summer !== 0) {
+      const b = summer;
+
+      counter++;
+      summer = 0;
+
+      return b + a;
+    }
+
+    if (counter <= 3 || counter % 2 !== 0) {
+      summer = a;
+
+      return getSum;
+    }
+
+    if (summer === 0) {
+      summer = a;
+
+      return getSum;
+    }
+
+    counter++;
+    summer = 0;
+
+    return 'Bzzz... Error!';
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
