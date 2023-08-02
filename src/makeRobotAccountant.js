bf79c936cf5da5f781c40b99817634be8cc53273
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-08-02 12:44:10 +0200

    fix

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 1;
+
+  return function(a) {
+    return function(b) {
+      if (counter > 3 && counter % 2 === 0) {
+        counter++;
+
+        return 'Bzzz... Error!';
+      } else {
+        counter++;
+
+        return a + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
