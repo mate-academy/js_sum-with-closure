efaf9f450b160061faab1e9eae64e62ced361140
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-08-03 21:38:40 +0200

    task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 3;
+
+  const result = function add(num1) {
+    return function(num2) {
+      if (count === 0) {
+        count = 1;
+
+        return 'Bzzz... Error!';
+      }
+
+      count--;
+
+      return num1 + num2;
+    };
+  };
+
+  return result;
 }
 
 module.exports = makeRobotAccountant;
