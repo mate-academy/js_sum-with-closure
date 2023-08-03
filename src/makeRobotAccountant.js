f569f92959b886b3bbe2b1a970b04f992e9ca4fa
Anna Shevtsova <aniashevtsova@gmail.com>
2023-08-03 18:38:38 +0200

    all tests pass

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  function getSum(a) {
+    counter++;
+
+    return (b) => {
+      if (counter > 3 && (counter % 2 === 0)) {
+        return `Bzzz... Error!`;
+      } else {
+        return a + b;
+      }
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
