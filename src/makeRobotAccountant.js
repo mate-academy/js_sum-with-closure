623788ab654a975ef6bb201975a40a451b29b21a
Hubert Klamann <hubert.klamann1@gmail.com>
2023-08-04 15:42:57 +0200

    task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function add(firstNumber) {
+    count++;
+
+    return function(secondNumber) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  }
+
+  return add;
 }
 
 module.exports = makeRobotAccountant;
