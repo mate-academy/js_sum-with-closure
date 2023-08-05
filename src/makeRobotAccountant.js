15c97feb4367c520208401e49524676f2cfe712a
kabaktech <kabacznikmobile@gmail.com>
2023-08-05 19:37:35 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -4,9 +4,20 @@
  *
  * @return {function}
  */
-
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return function(x) {
+    return function(y) {
+      callCount += 1;
+
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
