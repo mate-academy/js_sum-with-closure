403df191ba1b73de7084abf914f9066b11fe3d1e
Max Hyrych <gggirich@gmail.com>
2023-08-06 00:26:34 +0300

    First commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counterFunction = 0;
+
+  function getSum(firtsNumb) {
+    counterFunction++;
+
+    return function(secondNumb) {
+      if ((counterFunction > 3) && counterFunction % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return firtsNumb + secondNumb;
+      }
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
