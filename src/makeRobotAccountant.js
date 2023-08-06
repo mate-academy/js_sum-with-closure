4f3c5f2c9132b28a2b6b64b602bf9f62eb2be75e
Patryk Bąboń <patryk.babon@gmail.com>
2023-08-06 15:51:52 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return function getSum(a) {
+    callCount++;
+
+    return function(b) {
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
