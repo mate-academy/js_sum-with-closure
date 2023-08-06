aa6bd7acd0b0d131c43fdca9678b586680e09a6d
Dolnys <thedolnys@gmail.com>
2023-08-06 13:33:48 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  *
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+  const errorMessage = "Bzzz... Error!";
+
+  function getSum(a) {
+    return function (b) {
+      callCount++;
+
+      if (callCount <= 3) {
+        return a + b;
+      } else if (callCount % 2 === 0) {
+        return errorMessage;
+      } else {
+        return a + b;
+      }
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
