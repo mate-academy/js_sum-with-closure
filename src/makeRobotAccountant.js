445e2c6f255a2e6d11ea4610ca03b51e3d705c8e
patryk177m <patrykmajewski177@gmail.com>
2023-08-06 13:34:49 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const errorMessage = 'Bzzz... Error!';
+
+  const getSum = a => b => {
+    count++;
+
+    const sum = a + b;
+
+    const limit = count <= 3;
+
+    return limit || count % 2 !== 0 ? sum : errorMessage;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
