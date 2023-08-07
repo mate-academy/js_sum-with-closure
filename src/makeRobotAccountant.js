59675f02237cddc79f542c61cec577e4bff74d31
Emil Owczarek <owczarekemil@gmail.com>
2023-08-07 09:59:06 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(x) {
+    return function(y) {
+      count++;
+
+      if (count < 4 || count % 2 !== 0) {
+        return x + y;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
