1f87cef2e0cebf3c85a2f58af2e714b2343fb9b1
Wave <www.dandelion.ru@gmail.com>
2023-08-07 14:16:14 +0300

    Add solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -5,8 +5,21 @@
  * @return {function}
  */
 
-function makeRobotAccountant() {
-  // write code here
+function makeRobotAccountant(a) {
+  let callCount = 0;
+  return function (b) {
+    callCount++;
+
+    if (callCount < 3) {
+      return a + b;
+    }
+
+    if (callCount % 2 !== 0) {
+      return a + b;
+    }
+
+    return 'Bzzz... Error!';
+  };
 }
 
 module.exports = makeRobotAccountant;
