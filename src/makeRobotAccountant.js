31f709d3a729540d75abe04835f74d9fa83cee84
MateuszSlezal <M4teks93@gmail.com>
2023-08-07 02:05:38 +0200

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = function(firstNumber) {
+    count++;
+
+    return function(secondNumber) {
+      if (count > 3) {
+        return (count - 3) % 2 === 0
+          ? firstNumber + secondNumber
+          : 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
