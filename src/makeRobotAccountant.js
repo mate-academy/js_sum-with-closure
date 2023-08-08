4bd70ee17c333fe95e762e97ec3821127d1e766d
NataliMax <maximovs@1876gmail.com>
2023-08-08 13:22:07 +0200

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const getSum = (a) => {
+    count++;
+
+    return (b) => {
+      if (count > 2 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return a + b;
+      }
+    };
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
