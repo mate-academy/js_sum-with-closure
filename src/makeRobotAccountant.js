58c2d991e086dd7bb6d544d5a5f9a1af250e30cb
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-08-09 17:31:37 +0200

    make robot count

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,26 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  let countAfterThree = 0;
+
+  return function getSum(a) {
+    if (counter !== 3) {
+      counter++;
+
+      return (b) => a + b;
+    }
+
+    if (countAfterThree === 0) {
+      countAfterThree++;
+
+      return () => 'Bzzz... Error!';
+    } else {
+      countAfterThree = 0;
+
+      return b => a + b;
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
