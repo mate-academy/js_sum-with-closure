fa2f73dd263b62ee3f53cdfd2f4706e3a28bc8b1
Taras Voievoda <voevoda.taras@gmail.com>
2023-08-09 17:30:04 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countIteration = 1;
+
+  return a => {
+    return b => {
+      if (countIteration > 3 && countIteration % 2 === 0) {
+        countIteration++;
+
+        return 'Bzzz... Error!';
+      }
+      countIteration++;
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
