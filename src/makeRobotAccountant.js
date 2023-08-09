9e1155a39e3f41cc6f494088871cc118a9ec820a
davinnchii <zaikoivan99@gmail.com>
2023-08-09 20:05:07 +0200

    Add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCount = 0;
+  const errorMessage = 'Bzzz... Error!';
+  const sum = (x) => {
+    return (y) => {
+      callsCount++;
+
+      if (callsCount <= 3 || callsCount % 2 !== 0) {
+        return x + y;
+      }
+
+      return errorMessage;
+    };
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
