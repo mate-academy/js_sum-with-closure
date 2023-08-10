9aa0a8e47f026df9252baed05fc32f1a2c497f9f
Anton Tsurkan <wesses1999@gmail.com>
2023-08-10 21:23:55 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,16 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let robotAccountantCreated = 0;
+
+  return (number1 = 0) =>
+    (number2 = 0) => {
+      robotAccountantCreated++;
+
+      return (robotAccountantCreated > 3 && robotAccountantCreated % 2 === 0)
+        ? 'Bzzz... Error!'
+        : number1 + number2;
+    };
 }
 
 module.exports = makeRobotAccountant;
