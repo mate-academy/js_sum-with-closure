dbda196b47cd8ca4f133039491da798f1b108d5e
Victor Sich <vitalsich@gmail.com>
2023-08-11 09:13:15 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let caunt = 0;
+
+  return function(a) {
+    return function(b) {
+      caunt++;
+
+      if (caunt > 3) {
+        return caunt % 2 === 0 ? 'Bzzz... Error!' : a + b;
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
