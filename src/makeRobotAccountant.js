31b557e1ba424a9aa6f1709cf3cc731ef33be6d5
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-08-11 17:59:57 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,24 @@
 'use strict';
 
 /**
+
  *
  * @return {function}
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let counter = 0;
+
+  return function getSum(num1) {
+    counter++;
+
+    const isEvenCall = counter % 2 === 0;
+    const isGreaterThen3 = counter > 3;
+
+    return (num2) => (isEvenCall && isGreaterThen3)
+      ? 'Bzzz... Error!'
+      : num1 + num2;
+  };
+};
 
 module.exports = makeRobotAccountant;
