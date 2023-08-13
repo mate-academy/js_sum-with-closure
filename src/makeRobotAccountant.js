f2e84e17d285a7e7fd3e39c3840c2f156a89ff39
Denys Poltorak <itwfrontend@gmail.com>
2023-08-13 11:12:54 +0300

    feat: add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const CORRECT_COUNT_WORKINGS = 3;
+  let callCount = 1;
+
+  return (firstNum) => (secondNum) => {
+    const isEven = callCount % 2 === 0;
+    const isBroken = callCount > CORRECT_COUNT_WORKINGS && isEven;
+
+    if (isBroken) {
+      callCount++;
+
+      return 'Bzzz... Error!';
+    }
+    callCount++;
+
+    return firstNum + secondNum;
+  };
 }
 
 module.exports = makeRobotAccountant;
