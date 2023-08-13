68b48aa48a24d16aa1387b86d0505942e1eec70a
Viktoriia Roik <viktoriaroik2103@gmail.com>
2023-08-13 15:50:30 +0200

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let CALLS_COUNT = 0;
+  const MAX_CALLS_COUNT = 3;
+
+  const ERROR_MESSAGE = 'Bzzz... Error!';
+
+  function getSum(num1) {
+    CALLS_COUNT++;
+
+    return function(num2) {
+      if ((CALLS_COUNT > MAX_CALLS_COUNT) && (CALLS_COUNT % 2 === 0)) {
+        return ERROR_MESSAGE;
+      }
+
+      return num1 + num2;
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
