a0670b4408c6db8e3fb5325afe9576bb9ea058be
Dmytro Hrynchyi <desevoker@gmail.com>
2023-08-13 13:40:27 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,35 @@
 'use strict';
 
 /**
- *
  * @return {function}
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const FINE_CALLS = 3;
+  const ERROR_MSG = 'Bzzz... Error!';
+
+  let numberOfCalls = 0;
+
+  return (firstOperand) => {
+    return (secondOperand) => {
+      numberOfCalls += 1;
+
+      if (numberOfCalls > FINE_CALLS && isEven(numberOfCalls)) {
+        return ERROR_MSG;
+      }
+
+      return firstOperand + secondOperand;
+    };
+  };
+
+  /**
+   * @param {number} number
+   *
+   * @return {boolean}
+   */
+  function isEven(number) {
+    return number % 2 === 0;
+  }
 }
 
 module.exports = makeRobotAccountant;
