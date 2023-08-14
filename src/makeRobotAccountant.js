f40d80e01316cc0c77220fcd06a40ed2a361ca8a
Nikita <Nehrim24@gmail.com>
2023-08-15 00:46:32 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,19 @@
 
 function makeRobotAccountant() {
   // write code here
+  let callCount = 0;
+
+  return (x) => {
+    return (y) => {
+      callCount++;
+
+      if (callCount <= 3 || callCount % 2 === 1) {
+        return x + y;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
