b90e26c1f0c16c1afb19246b14b42b2b8c759556
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-08-14 15:47:40 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,22 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const NUMBER_OF_ATTEMPS = 3;
+  let counter = 0;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      counter++;
+
+      if (counter > NUMBER_OF_ATTEMPS) {
+        counter -= 2;
+
+        return 'Bzzz... Error!';
+      }
+
+      return firstNumber + secondNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
