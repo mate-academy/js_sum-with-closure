712ca7d43c6be4794ecd6f4b2cfa3b5fabbef9d4
Kava Bohdan <bogdan.kava.99@gmail.com>
2023-08-14 17:21:08 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (firstCallNumber) => {
+    counter++;
+
+    if (counter > 3 && counter % 2 === 0) {
+      return () => 'Bzzz... Error!';
+    }
+
+    return (secondCallNumber) => {
+      return firstCallNumber + secondCallNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
