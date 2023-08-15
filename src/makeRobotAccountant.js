2a149b0ef5a53b3a33a8f3880364b96e82781b48
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-08-15 15:47:41 +0300

    fix

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,17 +6,16 @@
  */
 
 function makeRobotAccountant() {
-  const NUMBER_OF_ATTEMPS = 3;
+  const NUMBER_OF_ATTEMPTS = 3;
+  const ERROR_MESSAGE = 'Bzzz... Error!';
   let counter = 0;
 
   return (firstNumber) => {
     return (secondNumber) => {
       counter++;
 
-      if (counter > NUMBER_OF_ATTEMPS) {
-        counter -= 2;
-
-        return 'Bzzz... Error!';
+      if (counter > NUMBER_OF_ATTEMPTS && counter % 2 === 0) {
+        return ERROR_MESSAGE;
       }
 
       return firstNumber + secondNumber;
