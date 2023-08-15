185fc54a878e16ee817b092bdbbfa3aabac83f0d
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-08-15 21:30:38 +0300

    make robot accountant solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const ERROR_MESSAGE = 'Bzzz... Error!';
+  const MAX_DIGIT_CALLS = 3;
+
+  return (firstNumCall) => {
+    count++;
+
+    return (secondNumCall) => {
+      const isCountAreEven = count % 2 === 0;
+
+      if (count > MAX_DIGIT_CALLS && isCountAreEven) {
+        return ERROR_MESSAGE;
+      }
+
+      return firstNumCall + secondNumCall;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
