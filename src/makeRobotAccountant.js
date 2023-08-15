e6a579822f54c321e3df37c428019929e292fd86
Rostyslav Kravchuk <mr.shadov.05@gmail.com>
2023-08-15 21:44:09 +0300

    add make robod accountant func

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const MAX_ATTEMPTS_NUMB = 3;
+  let callsCount = 0;
+
+  return (firstNumber) => {
+    return (secondNumber) => {
+      callsCount++;
+
+      if (callsCount > MAX_ATTEMPTS_NUMB && callsCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return secondNumber + firstNumber;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
