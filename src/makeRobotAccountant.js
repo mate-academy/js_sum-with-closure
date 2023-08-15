e18ec4278cadd6678bd87d403238b7abb1459d39
KatOlista^^! <olya.katyushina@gmail.com>
2023-08-15 15:41:01 +0200

    add solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,17 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 1;
+
+  return (num1) => {
+    const messageOrNum = counter > 3 && counter % 2 === 0
+      ? message => 'Bzzz... Error!'
+      : num2 => num1 + num2;
+
+    counter++;
+
+    return messageOrNum;
+  };
 }
 
 module.exports = makeRobotAccountant;
