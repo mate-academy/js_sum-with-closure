6aa7d2f4dc5975f8055d605ef8b6a5cb47844740
Oleh0293 <Oleh.kulyk.ik.2021@lpnu.ua>
2023-08-15 15:07:25 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,25 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return function(x) {
+    count++;
+
+    if (count <= 3) {
+      return function(y) {
+        return x + y;
+      };
+    } else if (count % 2 === 0) {
+      return function(y) {
+        return 'Bzzz... Error!';
+      };
+    } else {
+      return function(y) {
+        return x + y;
+      };
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
