88b314d346002306883bb28b8b121aa3beec80f5
Yevhenii <zheki2004@gmail.com>
2023-08-15 14:52:14 +0300

    task

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
-}
+  let count = 0;
+
+  return function getSum(a) {
+    count++;
+
+    return function(b) {
+      if (count >= 4 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
+};
 
 module.exports = makeRobotAccountant;
