28b592825f56819fcf2579850eb691446fd83e41
FurArt <iiionerart@gmail.com>
2023-08-15 09:30:56 +0300

    solution added

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const robot = (num1) => {
+    count++;
+
+    const secondRobot = (num2) => {
+      const oddCalls = !(count % 2);
+      const limitCalls = count > 3;
+      const errorMessage = 'Bzzz... Error!';
+      const summ = num1 + num2;
+
+      return limitCalls && oddCalls ? errorMessage : summ;
+    };
+
+    return secondRobot;
+  };
+
+  return robot;
 }
 
 module.exports = makeRobotAccountant;
