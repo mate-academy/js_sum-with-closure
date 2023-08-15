3e4eeedb1431661e5962712cabadc029d6a4e2d5
Taras Kusiy <taraskusiy@gmail.com>
2023-08-15 16:32:28 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,18 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const EROR_MESSAGE = 'Bzzz... Error!';
+  const maxCorectCountCall = 3;
+
+  return (x) => {
+    callCount++;
+
+    return (y) => callCount % 2 === 0 && callCount > maxCorectCountCall
+      ? EROR_MESSAGE
+      : x + y;
+  };
 }
 
 module.exports = makeRobotAccountant;
