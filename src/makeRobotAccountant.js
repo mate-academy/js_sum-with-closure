35e0bbf91b6fa2fb7229f51f2d0dde84fea902a2
Kava Bohdan <bogdan.kava.99@gmail.com>
2023-08-15 12:12:11 +0300

    change name

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,15 +9,15 @@ function makeRobotAccountant() {
   let counter = 0;
   const errorMessage = 'Bzzz... Error!';
 
-  return (firstCallNumber) => {
-    return (secondCallNumber) => {
+  return (firstArg) => {
+    return (secondArg) => {
       counter++;
 
       if (counter > 3 && counter % 2 === 0) {
         return errorMessage;
       }
 
-      return firstCallNumber + secondCallNumber;
+      return firstArg + secondArg;
     };
   };
 }
