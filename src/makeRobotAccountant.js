32b9be5744551d6ef25345b104c82aaf3861de75
Vitalii Serhiichuk <vitalya.sergiychuk19999@gmail.com>
2023-08-15 17:30:03 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let sum = 0;
+  const history = [];
+
+  return (a) => {
+    return (b) => {
+      sum = a + b;
+      history.push(sum);
+
+      if (history.length > 3 && history.length % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return sum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
