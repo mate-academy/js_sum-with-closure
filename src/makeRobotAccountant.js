304b52047e6c5723976177ea649fdf44916d1f55
Taras Kusiy <taraskusiy@gmail.com>
2023-08-17 16:37:11 +0300

    fixed solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -9,12 +9,16 @@ function makeRobotAccountant() {
   let callCount = 0;
 
   const EROR_MESSAGE = 'Bzzz... Error!';
-  const maxCorectCountCall = 3;
+  const MAX_CORRECT_COUNT_CALL = 3;
+  const IS_EVEN_CALL = (count) => count % 2 === 0;
+  const IS_GRETTER_THEN_MAX_CORRECT_COUNT_CALL = (count) => (
+    count > MAX_CORRECT_COUNT_CALL);
 
   return (x) => {
     callCount++;
 
-    return (y) => callCount % 2 === 0 && callCount > maxCorectCountCall
+    return (y) => IS_EVEN_CALL(callCount)
+    && IS_GRETTER_THEN_MAX_CORRECT_COUNT_CALL(callCount)
       ? EROR_MESSAGE
       : x + y;
   };
