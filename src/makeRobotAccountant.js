90e403ac191ea837e807a494525c1a90b690d235
Nazar Pastuschak <kns19pna@gmail.com>
2023-08-17 16:00:17 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(a) {
+    count++;
+
+    return function(b) {
+      if (count > 3 && count % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      const sum = a + b;
+
+      return sum;
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
