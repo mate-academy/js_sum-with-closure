e562dd53a583ac70db613b774629e866bf4041a7
Olha Momot <helgammt@gmail.com>
2023-08-19 14:27:52 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const LIMIT = 3;
+  let counter = 0;
+
+  return function(a) {
+    return function(b) {
+      counter++;
+
+      if (counter > LIMIT && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
