04e6200fee0e2d943d10ee4cfd9296dc223b4695
Vladyslav Prokhasko <vlad56445@gmail.com>
2023-08-19 18:55:25 +0300

    fix solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,12 +7,13 @@
 
 function makeRobotAccountant() {
   let attempts = 0;
+  const ERROR_MESSAGE = 'Bzzz... Error!';
 
   return a => b => {
     attempts++;
 
     if (attempts > 3 && attempts % 2 === 0) {
-      return 'Bzzz... Error!';
+      return ERROR_MESSAGE;
     }
 
     return a + b;
