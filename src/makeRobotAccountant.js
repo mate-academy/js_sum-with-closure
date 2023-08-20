f9b0a59e951df1f2f960f9875c648bac64286aa9
Valentyn Bilyk <valentynbilyk22@gmail.com>
2023-08-20 09:57:55 +0300

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,16 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  const LIMIT = 3;
+  let usageCount = 0;
+
+  return (a) => {
+    usageCount++;
+
+    const isBroken = usageCount > LIMIT && usageCount % 2 === 0;
+
+    return (b) => isBroken ? 'Bzzz... Error!' : a + b;
+  };
 }
 
 module.exports = makeRobotAccountant;
