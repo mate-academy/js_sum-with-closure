e7aece3f17d29c9b0ea42fa59a616bf0d17f41e5
Krzysztof Janiewicz <spojrzenie@gmail.com>
2023-08-24 22:52:36 +0200

    Add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,19 @@
 'use strict';
 
-/**
- *
- * @return {function}
- */
-
 function makeRobotAccountant() {
-  // write code here
+  let callsCounter = 0;
+
+  return (a) => {
+    return (b) => {
+      callsCounter++;
+
+      if (callsCounter < 3 || callsCounter % 2 !== 0) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
