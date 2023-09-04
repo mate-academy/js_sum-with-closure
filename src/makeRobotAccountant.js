a26cac344dec932019486742c870ee57aedb394f
Valeriia Fomenko <valeriia.fomenko@ukr.net>
2023-09-04 10:43:51 +0300

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const robot = function(x) {
+    return function(y) {
+      callCount++;
+
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return x + y;
+    };
+  };
+
+  return robot;
 }
 
 module.exports = makeRobotAccountant;
