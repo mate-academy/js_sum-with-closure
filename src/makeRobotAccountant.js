d0f4fbf022ff762868653c8b8544e900b8090415
Tommaso Giacopini <giacopini@bluewin.ch>
2023-09-07 18:09:15 +0200

    task solved

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -2,11 +2,34 @@
 
 /**
  *
+ * const getSum = makeRobotAccountant();
+getSum(5)(6) === 11;
+getSum(1)(3) === 4;
+getSum(6)(6) === 12;
+getSum(55)(16) === 'Bzzz... Error!';
+getSum(55)(16) === 71;
+getSum(55)(17) === 'Bzzz... Error!';
+getSum(5)(17) === 22;
+getSum(1)(1) === 'Bzzz... Error!';
+ *
  * @return {function}
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const errorMessage = 'Bzzz... Error!';
+
+  return (x) => {
+    return (y) => {
+      count++;
+
+      if (count % 2 === 0 && count > 3) {
+        return errorMessage;
+      }
+
+      return x + y;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
