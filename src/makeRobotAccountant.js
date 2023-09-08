63b50f237cabea4475febae001d2e2a26a41eb5c
Nataliia Kukushkina <n.kukushkina@gmail.com>
2023-09-08 14:04:59 +0300

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callsCount = 0;
+
+  const sum = (a) => {
+    return (b) => {
+      callsCount++;
+
+      if (callsCount >= 4 && callsCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
