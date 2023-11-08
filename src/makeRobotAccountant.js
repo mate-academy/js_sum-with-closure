7bdd3f115e84e76d52e83c92dcdf1a8519ce0e44
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-11-08 20:46:03 +0100

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,19 @@
-'use strict';
-
-/**
- *
- * @return {function}
- */
+"use strict";
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return function getSum(a) {
+    return function inner(b) {
+      counter++;
+
+      if (counter % 2 === 0 && counter > 3) {
+        return "Bzzz... Error!";
+      } else {
+        return a + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
