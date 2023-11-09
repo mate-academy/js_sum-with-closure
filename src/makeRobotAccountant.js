26255d42b44f7b888f6e129360e5a8e91241bdc2
Mirosław Wandyk <barosz1111@gmail.com>
2023-11-09 15:39:25 +0100

    initial commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,25 @@
 
 function makeRobotAccountant() {
   // write code here
+  let counter = 0;
+
+  function getSum(a) {
+    function bullshitNesting(b) {
+      if (counter > 2 && counter % 2 === 1) {
+        counter++;
+
+        return `Bzzz... Error!`;
+      } else {
+        counter++;
+
+        return a + b;
+      }
+    }
+
+    return bullshitNesting;
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
