c31fe92aaecca361da9b710ca71c49cbd3e2094c
Marcin Ligmann <marcinligmann2@gmail.com>
2023-11-09 20:54:38 +0100

    soluton

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,20 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  const error = 'Bzzz... Error!';
+
+  return (a) => {
+    count++;
+
+    return (b) => {
+      if (count > 3 & count % 2 === 0) {
+        return error;
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
