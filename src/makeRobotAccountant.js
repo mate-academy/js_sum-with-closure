0e7b9f2815893790d5843f445b8b4e47ed965a9b
jacekkosiba <jac.kosiba@gmail.com>
2023-11-10 14:46:51 +0100

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  return function (a) {
+    return function (b) {
+      counter++;
+
+      if (counter > 3 && counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+      
+      return a + b;
+    }
+  }
 }
+
 
 module.exports = makeRobotAccountant;
