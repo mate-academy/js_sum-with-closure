b213d3546ba5f25052dbd01c044839a06d675cbc
Mirosław Wandyk <barosz1111@gmail.com>
2023-11-10 15:00:04 +0100

    cut like half of code

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,26 +7,15 @@
 
 function makeRobotAccountant() {
   // write code here
-  // random comment cuz i forgot to switch branch and need to make changes :P
   let counter = 0;
 
-  function getSum(a) {
-    function bullshitNesting(b) {
-      if (counter > 2 && counter % 2 === 1) {
-        counter++;
+  return (a) => (b) => {
+    counter++;
 
-        return `Bzzz... Error!`;
-      } else {
-        counter++;
-
-        return a + b;
-      }
-    }
-
-    return bullshitNesting;
-  }
-
-  return getSum;
-}
+    return counter > 2 && counter % 2 === 0
+      ? `Bzzz... Error!`
+      : a + b;
+  };
+};
 
 module.exports = makeRobotAccountant;
