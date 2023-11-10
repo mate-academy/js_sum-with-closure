6d036887bfa6887e6d09fa1ffb01634a18184cba
Damian <damian.adamczewski92@gmail.com>
2023-11-10 12:21:00 +0100

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,16 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let i = 0;
+
+  return (first) => {
+    return (second) => {
+      i++;
+
+      return (i > 3 && i % 2 === 0)
+        ? 'Bzzz... Error!' : first + second;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
