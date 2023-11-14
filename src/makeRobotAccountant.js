165a5e3b84eb5cb80ffd61c6ed74c9d6d9156225
Marcin Ligmann <marcinligmann2@gmail.com>
2023-11-14 16:54:43 +0100

    solution - final version

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -12,7 +12,7 @@ function makeRobotAccountant() {
     return (b) => {
       count++;
 
-      if (count > 3 & count % 2 === 0) {
+      if (count > 3 && count % 2 === 0) {
         return 'Bzzz... Error!';
       }
 
