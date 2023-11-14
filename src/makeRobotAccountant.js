59d7acd8bcb67bab3e3846886db6d120c45d882e
web3-sorcerer <formyriadofsetups@gmail.com>
2023-11-14 16:00:57 +0100

    Solution 2

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,25 +6,20 @@
  */
 
 function makeRobotAccountant() {
+  let result = 0;
   let noOfExecutions = 0;
 
   return function firstNum(num1) {
-    let result = 0;
-
-    result += num1;
-
     return function secondNum(num2) {
-      result += num2;
-      noOfExecutions += 1;
+      result = num1 + num2;
+      noOfExecutions++;
 
-      if (noOfExecutions < 3) {
+      if (noOfExecutions <= 3) {
         return result;
+      } else if (noOfExecutions > 3 && noOfExecutions % 2 === 0) {
+        return 'Bzzz... Error!';
       } else {
-        if (noOfExecutions % 2 === 0) {
-          return 'Bzzz... Error!';
-        } else {
-          return result;
-        }
+        return result;
       }
     };
   };
