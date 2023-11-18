c6e2d7d683519e0a6a59517e61f123b84e1d0474
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-18 13:07:07 +0100

    add task soulution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function sum(a) {
+    return function(b) {
+      count++;
+
+      if (count <= 3) {
+        return a + b;
+      } else if (count % 2 === 1) {
+        return a + b;
+      } else {
+        return 'Bzzz... Error!';
+      }
+    };
+  }
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
