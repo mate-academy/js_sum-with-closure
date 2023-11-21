8b98fa63df24d60656a2acaa25bc6fb9c14e777f
Wiktoria Bielec <wiktoriabielec3@gmail.com>
2023-11-21 11:49:32 +0100

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,20 @@
 
 function makeRobotAccountant() {
   // write code here
+  let i = 1;
+
+  return function(a) {
+    return function(b) {
+      if (i > 3 && i % 2 === 0) {
+        i--;
+
+        return 'Bzzz... Error!';
+      }
+      i++;
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
