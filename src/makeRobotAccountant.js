15aa70d5452c014484a3caf1bf5a6b661c5610a5
Anton Chornobai <99268928+anton-chornobai@users.noreply.github.com>
2023-11-29 15:09:40 +0100

    task was done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+  let errEveryEven = false;
+
+  return function(num1) {
+    return function(num2) {
+      if (count >= 3) {
+        errEveryEven = !errEveryEven;
+      } else {
+        count++;
+      }
+
+      if (errEveryEven) {
+        return 'Bzzz... Error!';
+      }
+
+      return num1 + num2;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
