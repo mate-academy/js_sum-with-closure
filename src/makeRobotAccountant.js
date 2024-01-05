9f210f6b47e13eafc7067e6cd366c962f2a3f3d6
Krzychu92 <krzysztof.cichy1@gmail.com>
2024-01-05 12:08:40 +0100

    task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,24 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+  let error = false;
+
+  return (a) => {
+    return (b) => {
+      if (counter >= 3) {
+        error = !error;
+      } else {
+        counter++;
+      }
+
+      if (error) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
