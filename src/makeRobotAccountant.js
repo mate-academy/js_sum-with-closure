15739f3d85480c87c2ebaee8e676e6a3e96409d8
monikaprochniewicz <monika.prochniewicz99@gmail.com>
2024-02-04 11:10:29 +0100

    add task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  const getSum = (num1) => (num2) => {
+    callCount++;
+
+    return callCount <= 3
+      ? num1 + num2
+      : callCount % 2 === 0
+        ? 'Bzzz... Error!'
+        : num1 + num2;
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
