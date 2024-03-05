0b3dc5484998fa1a8000f52bd874b59bd24a28f0
Maryna Karpova <mari.artarea@gmail.com>
2024-03-05 15:21:10 +0100

    result

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (a) => {
+    return (b) => {
+      count++;
+
+      if (count % 2 === 0 && count > 3) {
+        return `Bzzz... Error!`;
+      } else {
+        return a + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
