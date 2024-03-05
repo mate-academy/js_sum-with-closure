cab12e675267f22338bfd5e1f07a5cabef1acc7e
Konrad Wasilewski <konradwasilewski444@gmail.com>
2024-03-05 16:18:44 +0100

    final version

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,29 +1,34 @@
+'use strict';
+
 /**
  *
  * @return {function}
  */
 
-"use strict";
-
-function makeRobotAccountant(num1) {
+function makeRobotAccountant() {
   let counter = 0;
 
-  function getSum(...nums) {
-    const first = Number(nums[0]);
+  return function getSum(num1) {
+    return function(num2) {
+      {
+        const first = Number(num1);
 
-    const second = Number(nums[1]);
+        const second = Number(num2);
 
-    counter++;
+        counter++;
 
-    if (counter % 2 === 0) {
-      return "Bzzz... Error!";
-    } else {
-      const result = first + second;
-      return result;
-    }
-  }
+        if (counter % 2 === 0 && counter > 3) {
+          return 'Bzzz... Error!';
+        } else {
+          const result = first + second;
 
-  return getSum;
+          return result;
+        }
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
+
+// V2
