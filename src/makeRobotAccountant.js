df10d01c60a260e3e25d9ff3ecdcea7276a7cf3d
Konrad Wasilewski <konradwasilewski444@gmail.com>
2024-03-06 10:31:03 +0100

    corrected version

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -11,19 +11,17 @@ function makeRobotAccountant() {
   return function getSum(num1) {
     return function(num2) {
       {
-        const first = Number(num1);
+        const first = num1;
 
-        const second = Number(num2);
+        const second = num2;
 
         counter++;
 
         if (counter % 2 === 0 && counter > 3) {
           return 'Bzzz... Error!';
-        } else {
-          const result = first + second;
-
-          return result;
         }
+
+        return first + second;
       }
     };
   };
