3c342977317306baf5d70e767ae25081b8db87e8
Mateusz Oślizło <amti4osl@gmail.com>
2024-03-21 10:55:45 +0100

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,13 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return (firstNumber) =>
+    (secondNumber) =>
+      (++counter >= 4 && counter % 2 === 0)
+        ? 'Bzzz... Error!'
+        : firstNumber + secondNumber;
 }
 
 module.exports = makeRobotAccountant;
