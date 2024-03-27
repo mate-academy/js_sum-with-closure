1c107190da7936becf8c4eb4082c85b30c610d0b
Krzysztof <krzysiek.gugala@gmail.com>
2024-03-27 13:15:31 +0100

    solutionV1

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,31 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 3;
+  let isOdd = '';
+  const error = 'Bzzz... Error!';
+
+  function getSum(a) {
+    return function(b) {
+      if (counter > 0) {
+        counter--;
+
+        return a + b;
+      } else {
+        if (isOdd === '' || isOdd === 'yes') {
+          isOdd = 'no';
+
+          return error;
+        } else {
+          isOdd = 'no';
+
+          return a + b;
+        }
+      }
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
